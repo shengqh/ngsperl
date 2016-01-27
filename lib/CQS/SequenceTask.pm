7d511e002b2d622233a111a562d4ea0bec217788
Quanhu Sheng <shengqh@gmail.com>
2016-01-27 09:49:35 -0600

    add summary pbs

--- lib/CQS/SequenceTask.pm
+++ lib/CQS/SequenceTask.pm
@@ -30,72 +30,85 @@ sub perform {
 
   my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct ) = get_parameter( $config, $section );
 
-  my %fqFiles = %{ get_raw_files( $config, $section ) };
+  my %step_map = %{ get_raw_files( $config, $section ) };
 
-  #print Dumper(\%fqFiles);
+  #print Dumper(\%step_map);
 
   my $cluster = get_cluster( $config, $section );
 
-  my $finalName    = $task_name . "_pipeline";
-  my $finalpbs     = $self->get_pbs_filename( $pbs_dir, $finalName );
-  my $finallog     = $self->get_log_filename( $log_dir, $finalName );
-  my $finallogdesp = $cluster->get_log_description($finallog);
+  my $final_name     = $task_name . "_pipeline";
+  my $final_pbs      = $self->get_pbs_filename( $pbs_dir, $final_name );
+  my $final_log      = $self->get_log_filename( $log_dir, $final_name );
+  my $final_log_desp = $cluster->get_log_description($final_log);
 
-  my $final = $self->open_pbs( $finalpbs, $pbs_desc, $finallogdesp, $path_file, $result_dir );
+  my $summary_name     = $task_name . "_summary";
+  my $summary_pbs      = $self->get_pbs_filename( $pbs_dir, $summary_name );
+  my $summary_log      = $self->get_log_filename( $log_dir, $summary_name );
+  my $summary_log_desp = $cluster->get_log_description($summary_log);
 
-  for my $taskName ( sort keys %fqFiles ) {
-    my $shfile = $self->get_task_filename( $pbs_dir, $taskName );
+  my $final   = $self->open_pbs( $final_pbs,   $pbs_desc, $final_log_desp,   $path_file, $result_dir );
+  my $summary = $self->open_pbs( $summary_pbs, $pbs_desc, $summary_log_desp, $path_file, $result_dir );
+
+  my $result_list_file = $self->get_file( $result_dir, $task_name, "expect_result.tsv" );
+  open( my $result_list, ">$result_list_file" ) or die $!;
+  print $result_list "StepName\tTaskName\tSampleName\tFileList\n";
+
+  for my $step_name ( sort keys %step_map ) {
+    my $shfile = $self->get_task_filename( $pbs_dir, $step_name );
     open( my $sh, ">$shfile" ) or die "Cannot create $shfile";
     print $sh get_run_command($sh_direct);
 
-    my @tasks = @{ $fqFiles{$taskName} };
+    my @tasks = @{ $step_map{$step_name} };
 
     my $samples = {};
     my $taskpbs = {};
-    for my $tasksection (@tasks) {
-
-      #print "task " . $tasksection . " ...\n";
-      my $get_pbs_files = getget_pbs_files( $config, $tasksection );
-      for my $sample ( sort keys %{$get_pbs_files} ) {
-
-        #print "\t", $sample, " => ", $get_pbs_files->{$sample}, "\n";
-        $samples->{$sample} = 1;
+    for my $task_section (@tasks) {
+      if ( !defined $config->{$task_section}{class} ) {
+        die "$section is not a valid task section.";
+      }
+      my $myclass         = instantiate( $config->{$task_section}{class} );
+      my %expect_file_map = %{ $myclass->result( $config, $task_section ) };
+      my $pbs_file_map    = $myclass->get_pbs_files( $config, $task_section );
+      for my $expect_name ( sort keys %expect_file_map ) {
+        my $expect_files = $expect_file_map{$expect_name};
+        my $expect_file_list = join( ",", @{$expect_files} );
+        print $result_list $step_name, "\t", $task_section, "\t", $expect_name, "\t", $expect_file_list, "\n";
       }
 
-      $taskpbs->{$tasksection} = $get_pbs_files;
-      for my $sample ( sort keys %{$get_pbs_files} ) {
+      #print "task " . $task_section . " ...\n";
+      $taskpbs->{$task_section} = $pbs_file_map;
+
+      for my $sample ( sort keys %{$pbs_file_map} ) {
         $samples->{$sample} = 1;
       }
     }
 
     for my $sample ( sort keys %{$samples} ) {
-      my $taskSample = $sample . "_" . $taskName;
-
-      my $pbs_file = $self->get_pbs_filename( $pbs_dir, $taskSample );
+      my $pbs_file = $self->get_step_sample_pbs( $pbs_dir, $step_name, $sample );
       my $pbs_name = basename($pbs_file);
-      my $log      = $self->get_log_filename( $log_dir, $taskSample );
+      my $log      = $self->get_step_sample_log( $log_dir, $step_name, $sample );
       my $logdesp  = $cluster->get_log_description($log);
 
       my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $logdesp, $path_file, $result_dir );
 
-      for my $tasksection (@tasks) {
+      for my $task_section (@tasks) {
 
-        #print "task " . $tasksection . " ...\n";
-        my $get_pbs_files = $taskpbs->{$tasksection};
+        #print "task " . $task_section . " ...\n";
+        my $pbs_files = $taskpbs->{$task_section};
 
-        if ( exists $get_pbs_files->{$sample} ) {
-          my $samplepbs = $get_pbs_files->{$sample};
+        if ( exists $pbs_files->{$sample} ) {
+          my $samplepbs = $pbs_files->{$sample};
           if ( ref($samplepbs) eq 'ARRAY' ) {
             for my $pbs ( @{$samplepbs} ) {
               if ( !-e $pbs ) {
-                die "Task " . $tasksection . ", file not exists " . $pbs . "\n";
+                die "Task " . $task_section . ", file not exists " . $pbs . "\n";
               }
               print $pbs "bash " . $pbs . "\n";
             }
           }
           else {
             if ( !-e $samplepbs ) {
-              die "Task " . $tasksection . ", file not exists " . $samplepbs . "\n";
+              die "Task " . $task_section . ", file not exists " . $samplepbs . "\n";
             }
             print $pbs "bash " . $samplepbs . "\n";
           }
@@ -116,8 +129,26 @@ sub perform {
     }
   }
 
-  $self->close_pbs( $final, $finalpbs );
+  $self->close_pbs( $final, $final_pbs );
+  close($result_list);
+  $self->close_pbs( $summary, $summary_pbs );
+}
 
+sub get_step_sample_name {
+  my ( $self, $step_name, $sample ) = @_;
+  return $sample . "_" . $step_name;
+}
+
+sub get_step_sample_pbs {
+  my ( $self, $pbs_dir, $step_name, $sample ) = @_;
+  my $task_sample = get_step_sample_name( $step_name, $sample );
+  return $self->get_pbs_filename( $pbs_dir, $task_sample );
+}
+
+sub get_step_sample_log {
+  my ( $self, $log_dir, $step_name, $sample ) = @_;
+  my $task_sample = get_step_sample_name( $step_name, $sample );
+  return $self->get_log_filename( $log_dir, $task_sample );
 }
 
 sub get_pbs_files {
@@ -127,35 +158,30 @@ sub get_pbs_files {
 
   my $result = {};
 
-  my %fqFiles = %{ get_raw_files( $config, $section ) };
+  my %step_map = %{ get_raw_files( $config, $section ) };
 
-  for my $taskName ( sort keys %fqFiles ) {
-    my @tasks = @{ $fqFiles{$taskName} };
+  for my $step_name ( sort keys %step_map ) {
+    my @tasks = @{ $step_map{$step_name} };
 
     my $samples = {};
     my $taskpbs = {};
-    for my $tasksection (@tasks) {
-
-      #print "task " . $tasksection . " ...\n";
-      my $get_pbs_files = getget_pbs_files( $config, $tasksection );
-      for my $sample ( sort keys %{$get_pbs_files} ) {
-
-        #print "\t", $sample, " => ", $get_pbs_files->{$sample}, "\n";
-        $samples->{$sample} = 1;
+    for my $task_section (@tasks) {
+      if ( !defined $config->{$task_section}{class} ) {
+        die "$section is not a valid task section.";
       }
-
-      $taskpbs->{$tasksection} = $get_pbs_files;
-      for my $sample ( sort keys %{$get_pbs_files} ) {
+      my $myclass = instantiate( $config->{$task_section}{class} );
+      my $pbs_file_map = $myclass->get_pbs_files( $config, $task_section );
+      for my $sample ( sort keys %{$pbs_file_map} ) {
         $samples->{$sample} = 1;
       }
     }
 
     for my $sample ( sort keys %{$samples} ) {
-      my $taskSample = $sample . "_" . $taskName;
-      $result->{$taskSample} = $self->get_pbs_filename( $pbs_dir, $taskSample );
+      my $step_sample = $self->get_step_sample_name( $step_name, $sample );
+      $result->{$step_sample} = $self->get_step_sample_pbs( $pbs_dir, $step_name, $sample );
     }
   }
-
   return $result;
 }
+
 1;
