8fb461a2c14304d4f99a532d28edb31fbf2847dd
Shilin Zhao <zhaoshilin@gmail.com>
2016-02-22 22:47:39 -0600

    perl check extence

--- lib/CQS/Perl.pm
+++ lib/CQS/Perl.pm
@@ -109,7 +109,12 @@ sub perform {
     my $log_desc = $cluster->get_log_description($log);
 
     my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir );
-    print $pbs "perl $perlFile $sample_name$output_ext $option $samples $parameterFile2 $parameterFile3 $parameterFile4 $parameterFile5";
+      print $pbs "
+if [ ! -s $sample_name$output_ext ]; then
+  perl $perlFile $sample_name$output_ext $option $samples $parameterFile2 $parameterFile3 $parameterFile4 $parameterFile5
+fi
+";
+#    print $pbs "perl $perlFile $sample_name$output_ext $option $samples $parameterFile2 $parameterFile3 $parameterFile4 $parameterFile5";
     $self->close_pbs( $pbs, $pbs_file );
   }
 
