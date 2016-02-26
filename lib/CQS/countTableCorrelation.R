7f82c92c4600bf9b3dbce26ca830c754944870c1
slzhao <zhaoshilin@gmail.com>
2016-02-26 15:21:24 -0600

    count table correlation

--- lib/CQS/countTableCorrelation.R
+++ lib/CQS/countTableCorrelation.R
@@ -1,32 +1,39 @@
-resultFile<-outFile
-countTableFile<-parFile1
-
-#resultFile<-"test"
-#countTableFile<-"/scratch/cqs/zhaos/vickers/20160211_smallRNA_3018-KCV-52_53_54_human/bowtie1_genome_1mm_NTA_smallRNA_table/result/smallRNA_1mm_3018-KCV-52_53_54.count"
+countTableFileList<-parSampleFile1
 
 library(heatmap3)
 library(DESeq2)  
 
-if (grepl(".csv$",countTableFile)) {
-	count<-read.csv(countTableFile,header=T,row.names=1)
-} else {
-	count<-read.delim(countTableFile,header=T,row.names=1)
+countTableFileAll<-read.delim(countTableFileList,header=F,as.is=T)
+for (i in 1:nrow(countTableFileAll)) {
+	countTableFile<-countTableFileAll[i,1]
+	
+	if (grepl(".csv$",countTableFile)) {
+		count<-read.csv(countTableFile,header=T,row.names=1,as.is=T)
+	} else {
+		count<-read.delim(countTableFile,header=T,row.names=1,as.is=T)
+	}
+	
+	colClass<-sapply(count, class)
+	countNum<-count[,which(colClass=="numeric" | colClass=="integer")]
+	countNum<-round(countNum,0)
+	
+	dds=DESeqDataSetFromMatrix(countData = countNum, colData = as.data.frame(rep(1,ncol(countNum))),design = ~1)
+	temp<-DESeq2::varianceStabilizingTransformation(dds, blind = TRUE)
+	countNumVsd<-assay(temp)
+	colnames(countNumVsd)<-colnames(countNum)
+	
+	countNumCor<-cor(countNumVsd,use="pa")
+	if (min(countNumCor,na.rm=T)<=0) {
+		col<-colorRampPalette(c("navy", "white","firebrick3"))(1024)
+	} else {
+		col<-colorRampPalette(c("white","firebrick3"))(1024)
+	}
+	
+	png(paste0(countTableFile,".Correlation.png"),width=2000,height=2000,res=300)
+	heatmap3(countNumCor,scale="none",balanceColor=T,margin=c(8,8),Rowv=NA,Colv=NA,col=col)
+	dev.off()
+	png(paste0(countTableFile,".Correlation.Cluster.png"),width=2000,height=2000,res=300)
+	heatmap3(countNumCor,scale="none",balanceColor=T,margin=c(8,8),col=col)
+	dev.off()
 }
 
-colClass<-sapply(count, class)
-countNum<-count[,which(colClass=="numeric" | colClass=="integer")]
-countNum<-round(countNum,0)
-
-dds=DESeqDataSetFromMatrix(countData = countNum, colData = as.data.frame(rep(1,ncol(countNum))),design = ~1)
-temp<-DESeq2::varianceStabilizingTransformation(dds, blind = TRUE)
-countNumVsd<-assay(temp)
-colnames(countNumVsd)<-colnames(countNum)
-
-countNumCor<-cor(countNumVsd,use="pa")
-
-png(paste0(resultFile,".Correlation.png"),width=2000,height=2000,res=300)
-heatmap3(countNumCor,scale="none",balanceColor=T,margin=c(8,8),Rowv=NA,Colv=NA)
-dev.off()
-png(paste0(resultFile,".Correlation.Cluster.png"),width=2000,height=2000,res=300)
-heatmap3(countNumCor,scale="none",balanceColor=T,margin=c(8,8))
-dev.off()
