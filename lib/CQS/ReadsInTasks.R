28b46e2547895c315c961f05cd1f1a8cf338a232
Shilin Zhao <zhaoshilin@gmail.com>
2016-03-18 00:07:33 -0500

    bugfix

--- lib/CQS/ReadsInTasks.R
+++ lib/CQS/ReadsInTasks.R
@@ -18,9 +18,14 @@ for (countFile in countFiles[,1]) {
 	resultTable<-rbind(resultTable,colSums(countTable))
 }
 
-
 row.names(resultTable)<-basename(countFiles[,1])
+write.csv(resultTable,paste0(resultFile,".TaskReads.csv"))
 
 tableBarplotToFile(dat=resultTable,fileName=paste0(resultFile,".TaskReads.Barplot.png"),
 		totalCountFile="",maxCategory=NA,textSize=textSize,
 		fill=NA,facet="Category")
+tableBarplotToFile(dat=resultTable,fileName=paste0(resultFile,".TaskReads.Barplot2.png"),
+		totalCountFile="",maxCategory=NA,textSize=textSize,
+		fill=NA,facet="Sample",x="Category",y="Reads",varName=c("Category","Sample","Reads"))
+
+
