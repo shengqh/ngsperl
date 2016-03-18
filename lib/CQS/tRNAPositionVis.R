09f1287855c6ad35674d48eb3efc6e8ff662d836
Quanhu Sheng <shengqh@gmail.com>
2016-03-18 10:33:05 -0500

    adjusted the display of all tRNA graph

--- lib/CQS/tRNAPositionVis.R
+++ lib/CQS/tRNAPositionVis.R
@@ -90,15 +90,18 @@ temp<-grep("^[A-Z][a-z][a-z]",positionRawAllSamplestRNAMeanSample$tRNA)
 positionRawAllSamplestRNAMeanSample3<-positionRawAllSamplestRNAMeanSample[temp,]
 write.csv(positionRawAllSamplestRNAMeanSample3,paste0(resultFile,".all3tRNAPosition.csv"))
 
+#positionRawAllSamplestRNAMeanSample3<-read.csv(paste0(resultFile,".all3tRNAPosition.csv"), row.names = 1)
 positionRawAllSamplestRNAMeanSample3<-positionRawAllSamplestRNAMeanSample3[which(positionRawAllSamplestRNAMeanSample3$Position<=maxPos),]
-m <- ggplot(positionRawAllSamplestRNAMeanSample3, aes(x = Position,y=CountPercentage,fill=tRNA))
-png(paste0(resultFile,".alltRNAPosition.png"),width=3000,height=3000,res=300)
-m + geom_bar(stat="identity")+facet_grid(Group ~ .) +
-		theme(legend.key.size = unit(0.4, "cm"))+
-		ylab("cumulative read fraction (read counts/total reads)")+
-		theme(text = element_text(size=20))+theme(legend.text = element_text(size=22))+
-		guides(fill= guide_legend(ncol = 1,keywidth=1.2))+
-		scale_fill_manual(values=colorRampPalette(brewer.pal(9, "Set1"))(length(unique(positionRawAllSamplestRNAMeanSample3$tRNA))))
+png(paste0(resultFile,".alltRNAPosition.png"),width=3000,height=max(2000, 1000 * length(unique(positionRawAllSamplestRNAMeanSample3$Group))),res=300)
+m <- ggplot(positionRawAllSamplestRNAMeanSample3, aes(x = Position,y=CountPercentage,fill=tRNA)) +
+  geom_bar(stat="identity")+facet_grid(Group ~ .) +
+  theme(legend.key.size = unit(0.4, "cm"))+
+  ylab("cumulative read fraction (read counts/total reads)")+
+  theme(text = element_text(size=20))+theme(legend.text = element_text(size=16))+
+  guides(fill= guide_legend(ncol = 1,keywidth=1, keyheight=1.5))+
+  scale_fill_manual(values=colorRampPalette(brewer.pal(9, "Set1"))(length(unique(positionRawAllSamplestRNAMeanSample3$tRNA)))) + 
+  xlim(-10, maxPos+5)
+print(m)
 dev.off()
 
 #significant tRNA position distribution
