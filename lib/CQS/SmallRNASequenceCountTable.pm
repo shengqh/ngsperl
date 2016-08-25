8fef87612f7edca8b17024873be54eec13f0277e
Quanhu Sheng <shengqh@gmail.com>
2016-08-25 15:20:40 -0500

    compatible with pairs/groups/files mode

--- lib/CQS/SmallRNASequenceCountTable.pm
+++ lib/CQS/SmallRNASequenceCountTable.pm
@@ -23,6 +23,18 @@ sub new {
   return $self;
 }
 
+sub get_sample_map {
+  my ( $groups, $rawfiles, $group_names ) = @_;
+  my $result = {};
+  for my $group_name (@$group_names) {
+    my @sample_names = @{ $groups->{$group_name} };
+    for my $sample_name (@sample_names) {
+      $result->{$sample_name} = $rawfiles->{$sample_name};
+    }
+  }
+  return $result;
+}
+
 sub perform {
   my ( $self, $config, $section ) = @_;
 
@@ -41,51 +53,51 @@ sub perform {
   $self->{_task_prefix} = get_option( $config, $section, "prefix", "" );
   $self->{_task_suffix} = get_option( $config, $section, "suffix", "" );
 
+  my $name_files_map = {};
+
+  if ( has_raw_files("pairs") ) {
+    my $comparisons      = get_raw_files( $config, $section, "pairs" );
+    my @comparison_names = sort keys %{$comparisons};
+    my $groups           = get_raw_files( $config, $section, "groups" );
+
+    for my $comparison_name (@comparison_names) {
+      my $gNames = $comparisons->{$comparison_name};
+      my @group_names;
+
+      if ( ref $gNames eq ref {} ) {
+        @group_names = @{ $gNames->{groups} };
+      }
+      else {
+        @group_names = @{$gNames};
+      }
+
+      $name_files_map->{$comparison_name} = get_sample_map( $groups, \%raw_files, \@group_names );
+    }
+  }
+  elsif ( has_raw_files("groups") ) {
+    my $groups = get_raw_files( $config, $section, "groups" );
+    for my $group_name ( sort keys %{$groups} ) {
+      my @group_names = [$group_name];
+      $name_files_map->{$group_name} = get_sample_map( $groups, \%raw_files, \@group_names );
+    }
+  }
+
+  $name_files_map->{$task_name} = \%raw_files;
+
   my $pbs_file = $self->get_pbs_filename( $pbs_dir, $task_name );
   my $pbs_name = basename($pbs_file);
   my $log      = $self->get_log_filename( $log_dir, $task_name );
-
   my $log_desc = $cluster->get_log_description($log);
-
-  if ( defined $config->{$section}{groups} || defined $config->{$section}{groups_ref} ) {
-    my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir );
-    my $groups = get_raw_files( $config, $section, "groups" );
-    for my $group_name ( sort keys %{$groups} ) {
-      my $filelist   = $self->get_file( $pbs_dir,    "${task_name}_${group_name}", ".filelist", 0 );
-      my $outputfile = $self->get_file( $result_dir, "${task_name}_${group_name}", ".count",    0 );
-      my $outputname = basename($outputfile);
-      
-      my @samples = @{ $groups->{$group_name} };
-      open( my $fl, ">$filelist" ) or die "Cannot create $filelist";
-      for my $sample_name ( sort @samples ) {
-        my @count_files = @{ $raw_files{$sample_name} };
-        my $countFile   = $count_files[0];
-
-        print $fl $sample_name, "\t", $countFile;
-        if ( defined $fastq_files ) {
-          print $fl $fastq_files->{$sample_name}->[0];
-        }
-        print $fl "\n";
-      }
-      close($fl);
-
-      print $pbs "
-if [ ! -s $outputname ]; then
-  mono $cqstools smallrna_sequence_count_table $option -o $outputname -l $filelist
-fi
-";
-    }
-    $self->close_pbs( $pbs, $pbs_file );
-  }
-  else {
-    my $filelist   = $self->get_file( $pbs_dir,    ${task_name}, ".filelist", 0 );
-    my $outputfile = $self->get_file( $result_dir, ${task_name}, ".count",    0 );
+  my $pbs      = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir );
+  for my $name ( sort keys %{$name_files_map} ) {
+    my $filelist   = $self->get_file( $pbs_dir,    $name, ".filelist", 0 );
+    my $outputfile = $self->get_file( $result_dir, $name, ".count",    0 );
     my $outputname = basename($outputfile);
-    my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $outputname );
 
     open( my $fl, ">$filelist" ) or die "Cannot create $filelist";
-    for my $sample_name ( sort keys %raw_files ) {
-      my @count_files = @{ $raw_files{$sample_name} };
+    my $samples = $name_files_map->{$name};
+    for my $sample_name ( sort keys %$samples ) {
+      my @count_files = @{ $samples->{$sample_name} };
       my $countFile   = $count_files[0];
       print $fl $sample_name, "\t", $countFile;
       if ( defined $fastq_files ) {
@@ -98,8 +110,8 @@ fi
     print $pbs "
 mono $cqstools smallrna_sequence_count_table $option -o $outputname -l $filelist
 ";
-    $self->close_pbs( $pbs, $pbs_file );
   }
+  $self->close_pbs( $pbs, $pbs_file );
 }
 
 sub result {
@@ -111,28 +123,26 @@ sub result {
 
   my $result = {};
 
-  my @result_files = ();
-  if ( defined $config->{$section}{groups} || defined $config->{$section}{groups_ref} ) {
-    my $groups = get_raw_files( $config, $section, "groups" );
-    for my $group_name ( sort keys %{$groups} ) {
-      my $outputfile = $self->get_file( $result_dir, "${task_name}_${group_name}", ".count",    0 );
-      my $filelist   = $self->get_file( $pbs_dir,    "${task_name}_${group_name}", ".filelist", 0 );
-      push( @result_files, $outputfile );
-      push( @result_files, $outputfile . ".fasta" );
-      push( @result_files, $filelist );
-    }
+  my @names = [];
+  if ( has_raw_files("pairs") ) {
+    my $comparisons = get_raw_files( $config, $section, "pairs" );
+    push( @names, keys %$comparisons );
   }
-  else {
-    my $outputfile = $self->get_file( $result_dir, ${task_name}, ".count",    0 );
-    my $read_outputfile = $self->get_file( $result_dir, ${task_name}, ".read.count",    0 );
-    my $filelist   = $self->get_file( $pbs_dir,    ${task_name}, ".filelist", 0 );
+  elsif ( has_raw_files("groups") ) {
+    my $groups = get_raw_files( $config, $section, "groups" );
+    push( @names, keys %$groups );
+  }
+  push( @names, $task_name );
+
+  for my $name (@names) {
+    my @result_files = ();
+    my $outputfile   = $self->get_file( $result_dir, $name, ".count", 0 );
+    my $filelist     = $self->get_file( $pbs_dir, $name, ".filelist", 0 );
     push( @result_files, $outputfile );
     push( @result_files, $outputfile . ".fasta" );
-    push( @result_files, $read_outputfile );
-    push( @result_files, $read_outputfile . ".fasta" );
     push( @result_files, $filelist );
+    $result->{$name} = filter_array( \@result_files, $pattern );
   }
-  $result->{$task_name} = filter_array( \@result_files, $pattern );
 
   return $result;
 }
