a56efc48e16ad2732384ec7281b81812393e067d
Shilin Zhao <zhaoshilin@gmail.com>
2016-08-28 23:50:34 -0500

    Non Host Overlap

--- lib/CQS/NonHostOverlap.R
+++ lib/CQS/NonHostOverlap.R
@@ -9,51 +9,55 @@ groupFileList<-parSampleFile2
 groupVisLayoutFileList<-parSampleFile3
 totalCountFile<-parFile3
 
-readsMappingResult<-list()
-for (i in 1:nrow(readFileList)) {
-	readsMappingResult[[i]]<-read.delim(readFileList[i,1],header=T,row.names=1)
-}
-readsMappingResultTable<-sapply(readsMappingResult,function(x) x)
-
-#Group1 and Group2 reads mapping table overlap
-temp<-table(c(row.names(readTable1),row.names(readTable2)))
-readsOverlapTime<-list()
-for (i in 1:max(temp)) {
-	readsOverlapTime[[i]]<-names(temp)[which(temp==i)]
+readsMappingNames<-list()
+readsMappingTable<-NULL
+readFiles<-read.delim(readFileList,header=F,as.is=T)
+for (i in 1:nrow(readFiles)) {
+	temp<-read.delim(readFiles[i,1],header=T,row.names=1,as.is=T)
+	readsMappingNames[[i]]<-row.names(temp)
+	readsMappingTable<-rbind(readsMappingTable,temp)
 }
 
-
-overlapCount<-colSums(readTable1[readsOverlap,])
-Genome1OnlyCount<-colSums(readTable1[setdiff(row.names(readTable1),readsOverlap),])
-Genome2OnlyCount<-colSums(readTable2[setdiff(row.names(readTable2),readsOverlap),])
-
-dataForPlot<-rbind(MicrobiomeOnly=Genome1OnlyCount,EnvironmentOnly=Genome2OnlyCount,InBoth=overlapCount)
+#Reads Overlap: Reads were found in how many categories?
+readsMappingNamesTable<-table(unlist(readsMappingNames))
+dataForPlot<-NULL
+maxReadCategory<-max(readsMappingNamesTable)
+for (i in 1:maxReadCategory) {
+	temp<-colSums(readsMappingTable[names(readsMappingNamesTable)[which(readsMappingNamesTable==i)],])
+	dataForPlot<-rbind(dataForPlot,temp)
+}
+row.names(dataForPlot)<-paste0("Reads Mapped to ",1:maxReadCategory," Categories")
 
 #Pie chart for all samples
-resultFile<-"Test"
-totalCountFile<-""
 ggpieToFile(dataForPlot,fileName=paste0(resultFile,".Piechart.png"),maxCategory=maxCategory,textSize=textSize)
 
 #Barplot for all samples
-tableBarplotToFile(dat=dataForPlot,fileName=paste0(resultFile,".Barplot.png"),totalCountFile=totalCountFile,maxCategory=maxCategory,textSize=textSize)
+tableBarplotToFile(dataForPlot,fileName=paste0(resultFile,".Barplot.png"),totalCountFile=totalCountFile,maxCategory=maxCategory,textSize=textSize)
 
 #Group Pie chart
-ggpieGroupToFile(dat=mappingResult2Species,fileName=paste0(resultFile,".Group.Piechart.png"),groupFileList=groupFileList,
+ggpieGroupToFile(dataForPlot,fileName=paste0(resultFile,".Group.Piechart.png"),groupFileList=groupFileList,
 		outFileName=paste0(resultFile,".PercentGroups.csv"),maxCategory=maxCategory,textSize=groupTextSize,visLayoutFileList=groupVisLayoutFileList)
 
+###################################################
+#Group1 and Group2 reads mapping table overlap
+###################################################
+temp1<-intersect(readsMappingNames[[1]],readsMappingNames[[2]])
+temp2<-setdiff(readsMappingNames[[1]],temp1)
+temp3<-setdiff(readsMappingNames[[2]],temp1)
 
-mappingResult<-read.delim(mappingResultFile,header=T,row.names=1, check.names=F)
-mappingResult2Species<-countTableToSpecies(dat=mappingResult,databaseLogFile=databaseLogFile,outFileName=paste0(resultFile,".Species.csv"))
+temp1<-colSums(readsMappingTable[temp1,])
+temp2<-colSums(readsMappingTable[temp2,])
+temp3<-colSums(readsMappingTable[temp3,])
+dataForPlot<-rbind(BothCategories=temp1,MicrobiomeOnly=temp2,EnvironmentOnly=temp3)
 
 #Pie chart for all samples
-ggpieToFile(mappingResult2Species,fileName=paste0(resultFile,".Piechart.png"),maxCategory=maxCategory,textSize=textSize)
+ggpieToFile(dataForPlot,fileName=paste0(resultFile,".MicrobiomeVsEnvironment.Piechart.png"),maxCategory=maxCategory,textSize=textSize)
 
 #Barplot for all samples
-tableBarplotToFile(dat=mappingResult2Species,fileName=paste0(resultFile,".Barplot.png"),totalCountFile=totalCountFile,maxCategory=maxCategory,textSize=textSize)
+tableBarplotToFile(dataForPlot,fileName=paste0(resultFile,".MicrobiomeVsEnvironment.Barplot.png"),totalCountFile=totalCountFile,maxCategory=maxCategory,textSize=textSize)
 
 #Group Pie chart
-ggpieGroupToFile(dat=mappingResult2Species,fileName=paste0(resultFile,".Group.Piechart.png"),groupFileList=groupFileList,
+ggpieGroupToFile(dataForPlot,fileName=paste0(resultFile,".MicrobiomeVsEnvironment.Group.Piechart.png"),groupFileList=groupFileList,
 		outFileName=paste0(resultFile,".PercentGroups.csv"),maxCategory=maxCategory,textSize=groupTextSize,visLayoutFileList=groupVisLayoutFileList)
-
 
 
