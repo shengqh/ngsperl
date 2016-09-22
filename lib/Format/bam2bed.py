e1f4aede6bfd533d2207fd5a667f4c3ef0fa2074
Quanhu Sheng <shengqh@gmail.com>
2016-09-22 10:58:26 -0500

    filter paired-end data by criteria used in MACS2

--- lib/Format/bam2bed.py
+++ lib/Format/bam2bed.py
@@ -3,8 +3,10 @@ import argparse
 import sys
 import logging
 import os
+from asyncore import read
 
-#this bam2bed is specific designed for DNASeq without structure vairantion
+#this bam2bed is specific designed for DNASeq without structure variation
+#filter paired-end data by criteria used in MACS2, https://groups.google.com/forum/#!topic/macs-announcement/Vh916L3tOOE
 parser = argparse.ArgumentParser(description="Convert SAM/BAM to bed. \nFor paired end data, only the fragment length between min-fragment-length and max-fragment-length will be exported. The reference start will be the minimum start of read1 and read2 and the reference end will be reference start plus fragment length. The strand will be set as same as read1.",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -37,33 +39,44 @@ samfile = pysam.Samfile(args.input, openmode)
 try:
   processed = 0
   accepted = 0
+  saved_read = {}
   for read in samfile.fetch(until_eof=True):
     processed += 1
     if processed % 1000000 == 0:
-      logger.info("processed %d, accepted %d" % (processed, accepted))
+      logger.info("processed %d, accepted %d, cache %d" % (processed, accepted, len(saved_read)))
       
-    if read.is_unmapped or read.mapq < args.min_mapq:
-      continue
+    if read.is_unmapped or read.mapq < args.min_mapq or read.is_secondary or read.is_qcfail or read.is_duplicate or read.is_supplementary:
+      continue;
     
     if read.is_paired:
+      if not read.is_proper_pair:
+        continue;
+      
       if read.reference_name != read.next_reference_name:
+        continue;
+      
+      if not saved_read.has_key(read.qname):
+        saved_read[read.qname] = read
         continue
       
-      if read.next_reference_start > read.reference_start:
-        continue
-      
-      if read.next_reference_start == read.reference_start and read.is_read2:
-        continue
-      
-      fragment_length = read.reference_end - read.next_reference_start
-      if fragment_length < args.min_fragment_length or fragment_length > args.max_fragment_length:
-        continue
-      
-      is_reverse = (read.is_read1 and read.is_reverse) or (read.is_read2 and not read.is_reverse)
-      reference_start = read.next_reference_start
+      paired_read = saved_read[read.qname]
+      try:
+        reference_start = min(read.reference_start, paired_read.reference_start)
+        reference_end = max(read.reference_end, paired_read.reference_end)
+        
+        fragment_length = reference_end - reference_start
+        if fragment_length < args.min_fragment_length or fragment_length > args.max_fragment_length:
+          continue
+        
+        mapq = min(read.mapq, paired_read.mapq)
+        is_reverse = (read.is_read1 and read.is_reverse) or (read.is_read2 and not read.is_reverse)
+      finally:
+        del saved_read[read.qname]
     else:
       is_reverse = read.is_reverse
       reference_start = read.reference_start
+      reference_end = read.reference_end
+      mapq = read.mapq
   
     if is_reverse:    
       strand = "-"
@@ -71,7 +84,7 @@ try:
     else:
       strand = "+"
       shift = args.shift_forward
-    output.write("%s\t%d\t%d\t%s\t%d\t%s\n" % (read.reference_name, reference_start + shift, read.reference_end + shift, read.qname, read.mapq, strand))
+    output.write("%s\t%d\t%d\t%s\t%d\t%s\n" % (read.reference_name, reference_start + shift, reference_end + shift, read.qname, mapq, strand))
     output.flush()
     accepted += 1
   
