3c29c2c62cd24fd4fb2e68d0d0a2d540bbaaf919
slzhao <zhaoshilin@gmail.com>
2016-09-23 11:29:53 -0500

    bugfix

--- lib/Pipeline/SmallRNA.pm
+++ lib/Pipeline/SmallRNA.pm
@@ -1129,8 +1129,8 @@ sub getSmallRNAConfig {
       );
     push @table_for_countSum,
       (
-      "bowtie1_tRNA_pm_table",            ".category.count\$", "bowtie1_rRNA_pm_table",          "^(?!.*?read).*\.count\$", "bowtie1_bacteria_group1_pm_table", "^(?!.*?read).*\.count\$",
-      "bowtie1_bacteria_group2_pm_table", "^(?!.*?read).*\.count\$",          "bowtie1_fungus_group4_pm_table", "^(?!.*?read).*\.count\$"
+      "bowtie1_tRNA_pm_table",            ".category.count\$", "bowtie1_rRNA_pm_table",          ".category.count\$", "bowtie1_bacteria_group1_pm_table", ".category.count\$",
+      "bowtie1_bacteria_group2_pm_table", ".category.count\$",          "bowtie1_fungus_group4_pm_table", ".category.count\$"
       );
     push @table_for_readSummary,
       (
