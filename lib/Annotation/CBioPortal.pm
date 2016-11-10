8383fe1d949929c43cbceeb4e374686b991ac3fe
Quanhu Sheng <shengqh@gmail.com>
2016-11-10 13:57:55 -0600

    oncoprinter and mutationmapper data preparation from FilterAnnovar result

---
+++ lib/Annotation/CBioPortal.pm
@@ -0,0 +1,86 @@
+#!/usr/bin/perl
+package Annotation::CBioPortal;
+
+use strict;
+use warnings;
+use File::Basename;
+use CQS::PBS;
+use CQS::ConfigUtils;
+use CQS::SystemUtils;
+use CQS::FileUtils;
+use CQS::NGSCommon;
+use CQS::StringUtils;
+use CQS::UniqueTask;
+
+our @ISA = qw(CQS::UniqueTask);
+
+sub new {
+  my ($class) = @_;
+  my $self = $class->SUPER::new();
+  $self->{_name}   = __PACKAGE__;
+  $self->{_suffix} = "_cbp";
+  bless $self, $class;
+  return $self;
+}
+
+sub perform {
+  my ( $self, $config, $section ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct, $cluster ) = get_parameter( $config, $section );
+
+  my %raw_files = %{ get_raw_files( $config, $section ) };
+  my $sampleNamePattern = get_option( $config, $section, "sample_name_pattern" );
+  my $sampleNameSuffix = get_option( $config, $section, "sample_name_suffix", "");
+  my $geneNames         = get_option( $config, $section, "gene_names" );
+
+  my $script = dirname(__FILE__) . "/CBioPortal.r";
+  if ( !-e $script ) {
+    die "File not found : " . $script;
+  }
+
+  my $pbs_file = $self->get_pbs_filename( $pbs_dir, $task_name );
+  my $log = $self->get_log_filename( $log_dir, $task_name );
+  my $log_desc = $cluster->get_log_description($log);
+
+  my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir );
+
+  for my $sample_name ( sort keys %raw_files ) {
+    my @inputFiles = @{ $raw_files{$sample_name} };
+    for my $inputFile (@inputFiles) {
+      my $filename = basename($inputFile);
+      my $prefix = change_extension($filename, "${sampleNameSuffix}.cBioPortal");
+      my $finalFile = $prefix . ".oncoprinter.txt";
+      print $pbs "if [ ! -e $finalFile ]; then 
+  R --vanilla -f $script --args $inputFile $prefix $sampleNamePattern $geneNames
+fi
+";
+    }
+  }
+  $self->close_pbs( $pbs, $pbs_file );
+}
+
+sub result {
+  my ( $self, $config, $section, $pattern ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct ) = get_parameter( $config, $section, 0 );
+
+  my %raw_files = %{ get_raw_files( $config, $section ) };
+  my $sampleNameSuffix = get_option( $config, $section, "sample_name_suffix" );
+
+  my $result = {};
+
+  for my $sample_name ( sort keys %raw_files ) {
+    my @result_files = ();
+    my @inputFiles   = @{ $raw_files{$sample_name} };
+    for my $inputFile (@inputFiles) {
+      my $filename = basename($inputFile);
+      my $prefix = change_extension($filename, "${sampleNameSuffix}.cBioPortal");
+      push( @result_files, "$result_dir/${prefix}.oncoprinter.txt" );
+      push( @result_files, "$result_dir/${prefix}.mutationmapper.txt" );
+    }
+    $result->{$sample_name} = filter_array( \@result_files, $pattern );
+  }
+  return $result;
+}
+
+1;
---
+++ lib/Annotation/CBioPortal.r
@@ -0,0 +1,99 @@
+library(ComplexHeatmap)
+
+args = commandArgs(trailingOnly=TRUE)
+inputFile = args[1]
+outputPrefix = args[2]
+sampleNamePattern=args[3]
+genelist=args[4:length(args)]
+
+#setwd("/scratch/cqs/shengq1/dnaseq/20161013_liuqi_gene_panel/bwa_refine_hc_gvcf_vqsr_annovar_filter/result/")
+#inputFile = "Adenoma.exac0.001.tsv"
+#outputPrefix = "Adenoma.exac0.001"
+#sampleNamePattern="cur_"
+#genelist=c("APC", "ROBO2", "KRAS", "NRAS", "BRAF", "TP53")
+
+cat("inputFile=", inputFile, "\n")
+cat("outputPrefix=", outputPrefix, "\n")
+cat("sampleNamePattern=", sampleNamePattern, "\n")
+cat("genelist=", genelist, "\n")
+
+oncoFile = paste0(outputPrefix, ".oncoprinter.txt")
+mutationmapperFile = paste0(outputPrefix, ".mutationmapper.txt")
+
+mutdata<-read.table(inputFile,as.is=T,header=TRUE,sep="\t")
+cnames=colnames(mutdata)
+
+samples<-cnames[grep(sampleNamePattern,cnames)]
+if(length(samples) == 0){
+  stop(paste0("No sample matches the pattern ", sampleNamePattern))
+}
+
+geneind<-mutdata$Gene.refGene %in% genelist
+
+mutdata_gene_samples<-mutdata[geneind,c("Func.refGene", "Gene.refGene", "ExonicFunc.refGene", "AAChange.refGene", samples)]
+sample_start<-5
+sample_end<-ncol(mutdata_gene_samples)
+
+##oncoprint
+oncoprint<-NULL
+i=69
+for (i in sample_start:sample_end){
+	sample<-colnames(mutdata_gene_samples)[i]
+	mut_sample<-mutdata_gene_samples[,i]
+	if (sum(mut_sample)>0) {
+		for (j in 1: length(mut_sample)) {
+			if (mut_sample[j]>0) {
+				gene=mutdata_gene_samples$Gene.refGene[j]
+				
+				info<-mutdata_gene_samples$AAChange.refGene[j]
+				info<-unlist(strsplit(info,":"))
+				pchange<-info[length(info)]
+				
+				type<-mutdata_gene_samples$ExonicFunc.refGene[j]
+				if (type=="frameshift deletion" | type=="frameshift insertion" | type=="frameshift substitution" | type=="stopgain"){ 
+					type="TRUNC"
+				}else if (type=="nonsynonymous SNV"){ 
+					type="MISSENSE"
+				}else if (mutdata_gene_samples$Func.refGene[j]=="splicing"){ 
+					type="TRUNC"
+					pchage="splice"
+				}else{
+					type="UNKNOWN"
+				}
+				
+				oncoprint<-rbind(oncoprint,c(sample,gene, pchange,type))
+			} 
+		}
+	}
+}
+colnames(oncoprint)=c("Sample","Gene","Alteration", "Type")
+write.table(oncoprint,file=oncoFile,quote=F,row.names=F,sep="\t")
+
+###mutationmapper
+mutationmapper<-NULL
+for (i in sample_start:sample_end){
+	sample<-colnames(mutdata_gene_samples)[i]
+	mut_sample<-mutdata_gene_samples[,i]
+	if (sum(mut_sample)>0) {
+		for (j in 1: length(mut_sample)) {
+			if (mut_sample[j]>0) {
+				if (mutdata_gene_samples$Func.refGene[j]!="splicing"){
+					gene=mutdata_gene_samples$Gene.refGene[j]
+					
+					info<-mutdata_gene_samples$AAChange.refGene[j]
+					info<-unlist(strsplit(info,":"))
+					pchange<-info[length(info)]
+					
+					type<-mutdata_gene_samples$ExonicFunc.refGene[j]
+					if (type=="frameshift deletion" | type=="frameshift substitution") type="Frame_Shift_Del"
+					else if (type=="frameshift insertion") type="Frame_Shift_Ins"
+					else if (type=="stopgain" | type=="nonsynonymous SNV") type="Nonsense_Mutation"
+					
+					mutationmapper<-rbind(mutationmapper,c(gene, sample,pchange, type))
+				} 
+			} 
+		}
+	}
+}
+colnames(mutationmapper)<-c("Hugo_Symbol","Sample_ID","Protein_Change","Mutation_Type")
+write.table(mutationmapper,file=mutationmapperFile,row.names=F,quote=F,sep="\t")
