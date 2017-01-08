e5e12ac25762efd4df3c0ed4c6b2093b2add26ba
Sheng, Quanhu <shengqh@gmail.com>
2017-01-07 19:41:56 -0600

    output fasta

--- lib/Blast/DESeq2SignificantReadToFasta.py
+++ lib/Blast/DESeq2SignificantReadToFasta.py
@@ -23,6 +23,10 @@ for inputfile in inputfiles:
     for row in spamreader:
       sequences.add(row[0])
 
+with open(outputfile, 'w') as fasta:
+  for seq in sorted(sequences):
+    fasta.write(">%s\n%s\n" %(seq, seq));
+    
 print("%d sequences prepared for blastn \n" % len(sequences))
 
 print("Transfer DEseq2 significant read to fasta for blastn done.")
