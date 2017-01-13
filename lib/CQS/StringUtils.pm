2e69d1f802b03aaa87338b375eac7a69c10c48ff
Quanhu Sheng <shengqh@gmail.com>
2017-01-13 10:41:28 -0600

    add string_combination string_repeat function and test

--- lib/CQS/StringUtils.pm
+++ lib/CQS/StringUtils.pm
@@ -7,11 +7,36 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-our %EXPORT_TAGS = ( 'all' => [qw( merge_string print_hash filter_array)] );
+our %EXPORT_TAGS = ( 'all' => [qw( merge_string print_hash filter_array string_combination string_repeat)] );
 
 our @EXPORT = ( @{ $EXPORT_TAGS{'all'} } );
 
 our $VERSION = '0.01';
+
+sub string_repeat {
+  my ( $array, $repeatTime ) = @_;
+  my $result = [];
+  for my $str (@$array) {
+    push @$result, ($str) x $repeatTime;
+  }
+  return $result;
+}
+
+sub string_combination {
+  my ( $left, $right, $delimiter ) = @_;
+  my $result = [];
+  for my $str1 (@$left) {
+    for my $str2 (@$right) {
+      if ( length($str1) == 0 || length($str2) == 0 ) {
+        push( @$result, $str1 . $str2 );
+      }
+      else {
+        push( @$result, $str1 . $delimiter . $str2 );
+      }
+    }
+  }
+  return $result;
+}
 
 sub merge_string {
   my ( $delimiter, @values ) = @_;
@@ -37,23 +62,25 @@ sub print_hash {
 }
 
 sub filter_array {
-  my ($sourceFiles, $pattern, $canReturnEmpty ) = @_;
+  my ( $sourceFiles, $pattern, $canReturnEmpty ) = @_;
 
-  if ( !defined $pattern || $pattern eq "") {
+  if ( !defined $pattern || $pattern eq "" ) {
     return $sourceFiles;
   }
 
   my @filteredFiles = ();
   for my $candidateFile ( @{$sourceFiles} ) {
+
     #print $candidateFile . "\n";
     if ( $candidateFile =~ m/$pattern/ ) {
+
       #print $candidateFile . " passed\n";
       push( @filteredFiles, $candidateFile );
     }
   }
 
-  if(scalar(@filteredFiles) == 0 && ! $canReturnEmpty){
-    my $str = join(', ', @{$sourceFiles});
+  if ( scalar(@filteredFiles) == 0 && !$canReturnEmpty ) {
+    my $str = join( ', ', @{$sourceFiles} );
     warn("No file in array [$str] matched with pattern $pattern, empty array returned.");
   }
   return \@filteredFiles;
---
+++ test/CQS/TestStringUtils.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Test::More tests => 2;
+use CQS::StringUtils;
+
+is_deeply( string_combination( [ 'a', 'b', '' ], [ '1', '2', '' ], ':' ), ["a:1", "a:2", "a", "b:1", "b:2", "b", "1", "2", "" ] );
+
+is_deeply( string_repeat( [ 'a', 'b' ], 2 ), ["a", "a", "b", "b" ] );
+
+1;
