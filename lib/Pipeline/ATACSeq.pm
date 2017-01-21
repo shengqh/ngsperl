d6130f261486dff36c4987cf87b5dc5e65033389
Quanhu Sheng <shengqh@gmail.com>
2017-01-21 08:44:11 -0600

    preprocessing by function

--- lib/Pipeline/ATACSeq.pm
+++ lib/Pipeline/ATACSeq.pm
@@ -23,28 +23,12 @@ our $VERSION = '0.01';
 sub initializeDefaultOptions {
   my $def = shift;
 
-  if ( !defined $def->{cluster} ) {
-    $def->{cluster} = 'slurm';
-  }
-
-  if ( !defined $def->{sra_to_fastq} ) {
-    $def->{sra_to_fastq} = 0;
-  }
-
-  if ( !defined $def->{fastq_remove_N} ) {
-    $def->{fastq_remove_N} = 0;
-  }
-
-  if ( !defined $def->{table_vis_group_text_size} ) {
-    $def->{table_vis_group_text_size} = "10";
-  }
-
-  if ( !defined $def->{max_thread} ) {
-    $def->{max_thread} = "8";
-  }
-  if ( !defined $def->{sequencetask_run_time} ) {
-    $def->{sequencetask_run_time} = "12";
-  }
+  initValue( $def, "cluster", "slurm" );
+  initValue( $def, "sra_to_fastq", 0 );
+  initValue( $def, "fastq_remove_N", 0 );
+  initValue( $def, "perform_cutadapt", 0 );
+  initValue( $def, "max_thread", 8 );
+  initValue( $def, "sequencetask_run_time", 12 );
 
   return $def;
 }
@@ -58,17 +42,12 @@ sub getConfig {
 
   $def = initializeDefaultOptions($def);
 
+  my ( $config, $individual, $summary, $source_ref, $preprocessing_dir ) = getPreprocessionConfig($def);
+
   my $cluster = $def->{cluster};
   my $task    = $def->{task_name};
 
-  my $sra_to_fastq     = getValue( $def, "sra_to_fastq",     0 );
-  my $fastq_remove_N   = getValue( $def, "fastq_remove_N",   0 );
   my $email            = getValue( $def, "email" );
-  my $cqstools         = getValue( $def, "cqstools" );
-  my $picard_jar       = getValue( $def, "picard_jar" );
-  my $bwa_fasta        = getValue( $def, "bwa_fasta" );
-  my $treatments       = getValue( $def, "treatments" );
-  my $pairend          = getValue( $def, "pairend" );
   my $macs2call_option = getValue( $def, "macs2call_option", "-f BEDPE --broad -g hs -B -q 0.01 --broad-cutoff 0.01 --nomodel --slocal 20000 --llocal 20000 --keep-dup all" );
 
   my $perform_rose = getValue( $def, "perform_rose" );
@@ -77,157 +56,17 @@ sub getConfig {
     $perform_coltron = getValue( $def, "perform_coltron" );
   }
 
-  my $config = {
-    general => {
-      task_name => $task,
-      cluster   => $cluster
-    },
-    files      => $def->{files},
-    treatments => $treatments
-  };
-
-  my $source_ref = "files";
-  my @individual;
-  my @summary;
-
-  if ($sra_to_fastq) {
-    $config->{"sra2fastq"} = {
-      class      => "SRA::FastqDump",
-      perform    => 1,
-      ispaired   => 0,
-      target_dir => "${target_dir}/sra2fastq",
-      option     => "",
-      source_ref => "files",
-      sh_direct  => 0,
-      pbs        => {
-        "email"    => $email,
-        "nodes"    => "1:ppn=1",
-        "walltime" => "10",
-        "mem"      => "10gb"
-      },
-    };
-    $source_ref = "sra2fastq";
-    push @individual, ("sra2fastq");
-  }
-
-  if ($fastq_remove_N) {
-    $config->{fastq_remove_N} = {
-      class      => "CQS::FastqTrimmer",
-      perform    => $fastq_remove_N,
-      target_dir => $target_dir . "/fastq_remove_N",
-      option     => "",
-      extension  => "_trim.fastq.gz",
-      source_ref => "files",
-      cluster    => $cluster,
-      sh_direct  => 1,
-      pbs        => {
-        "email"    => $email,
-        "nodes"    => "1:ppn=1",
-        "walltime" => "2",
-        "mem"      => "10gb"
-      }
-    };
-    $source_ref = "fastq_remove_N";
-    push @individual, ("fastq_remove_N");
-  }
+  $config->{treatments} = getValue( $def, "treatments" );
 
   my $processing = {
-    "fastqc_raw" => {
-      class      => "QC::FastQC",
-      perform    => 1,
-      target_dir => $target_dir . "/fastqc_raw",
-      option     => "",
-      source_ref => $source_ref,
-      cluster    => $cluster,
-      pbs        => {
-        "email"    => $email,
-        "nodes"    => "1:ppn=1",
-        "walltime" => "2",
-        "mem"      => "10gb"
-      },
-    },
-    "fastqc_raw_summary" => {
-      class      => "QC::FastQCSummary",
-      perform    => 1,
-      target_dir => $target_dir . "/fastqc_raw",
-      cqstools   => $cqstools,
-      option     => "",
-      cluster    => $cluster,
-      pbs        => {
-        "email"    => $email,
-        "nodes"    => "1:ppn=1",
-        "walltime" => "2",
-        "mem"      => "10gb"
-      },
-    },
-    "cutadapt" => {
-      class      => "Trimmer::Cutadapt",
-      perform    => 1,
-      target_dir => "${target_dir}/cutadapt",
-      option     => "-m 30 --trim-n",
-      source_ref => $source_ref,
-      adapter    => "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC",    #trueseq adapter
-      extension  => "_clipped.fastq",
-      pairend    => $pairend,
-      sh_direct  => 0,
-      pbs        => {
-        "email"    => $email,
-        "nodes"    => "1:ppn=1",
-        "walltime" => "24",
-        "mem"      => "20gb"
-      },
-    },
-    "fastqc_post_trim" => {
-      class      => "QC::FastQC",
-      perform    => 1,
-      target_dir => "${target_dir}/fastqc_post_trim",
-      option     => "",
-      sh_direct  => 1,
-      source_ref => [ "cutadapt", ".fastq.gz" ],
-      pbs        => {
-        "email"    => $email,
-        "nodes"    => "1:ppn=1",
-        "walltime" => "2",
-        "mem"      => "10gb"
-      },
-    },
-    "fastqc_post_trim_summary" => {
-      class      => "QC::FastQCSummary",
-      perform    => 1,
-      sh_direct  => 1,
-      target_dir => "${target_dir}/fastqc_post_trim",
-      cqstools   => $cqstools,
-      option     => "",
-      pbs        => {
-        "email"    => $email,
-        "nodes"    => "1:ppn=1",
-        "walltime" => "2",
-        "mem"      => "10gb"
-      },
-    },
-    "fastq_len" => {
-      class      => "CQS::FastqLen",
-      perform    => 1,
-      target_dir => "$target_dir/fastq_len",
-      option     => "",
-      source_ref => "cutadapt",
-      cqstools   => $cqstools,
-      sh_direct  => 1,
-      pbs        => {
-        "email"    => $email,
-        "nodes"    => "1:ppn=1",
-        "walltime" => "24",
-        "mem"      => "20gb"
-      },
-    },
     "bwa" => {
       class              => "Alignment::BWA",
       perform            => 1,
       target_dir         => "${target_dir}/bwa",
       option             => "",
-      bwa_index          => $bwa_fasta,
-      picard_jar         => $picard_jar,
-      source_ref         => [ "cutadapt", ".fastq.gz" ],
+      bwa_index          => getValue( $def, "bwa_fasta" ),
+      picard_jar         => getValue( $def, "picard_jar" ),
+      source_ref         => $source_ref,
       sort_by_coordinate => 1,
       sh_direct          => 0,
       pbs                => {
@@ -243,7 +82,7 @@ sub getConfig {
       target_dir              => "${target_dir}/bwa_cleanbam",
       option                  => "",
       source_ref              => "bwa",
-      picard_jar              => $picard_jar,
+      picard_jar              => getValue( $def, "picard_jar" ),
       remove_chromosome       => "M",
       keep_chromosome         => "chr",
       is_sorted_by_coordinate => 1,
@@ -274,7 +113,10 @@ sub getConfig {
         "mem"      => "40gb"
       },
     },
-    "bwa_macs2callpeak" => {
+  };
+  push @$individual, ( "bwa", "bwa_cleanbam", "bwa_bam2bed" );
+  if ( $perform_rose || ( !defined $def->{replicates} ) ) {
+    $config->{"bwa_macs2callpeak"} = {
       class      => "Chipseq::MACS2Callpeak",
       perform    => 1,
       target_dir => "${target_dir}/bwa_macs2callpeak",
@@ -287,10 +129,9 @@ sub getConfig {
         "walltime" => "72",
         "mem"      => "40gb"
       },
-    },
-  };
-  push @individual, ( "fastqc_raw", "cutadapt", "fastqc_post_trim", "fastq_len", "bwa", "bwa_cleanbam", "bwa_bam2bed" );
-  push @summary, ( "fastqc_raw_summary", "bwa_macs2callpeak" );
+    };
+    push @$summary, ("bwa_macs2callpeak");
+  }
 
   if ($perform_rose) {
     $config->{"bwa_macs2callpeak_bradner_rose"} = {
@@ -311,7 +152,7 @@ sub getConfig {
         "mem"      => "40gb"
       },
     };
-    push @summary, ("bwa_macs2callpeak_bradner_rose");
+    push @$summary, ("bwa_macs2callpeak_bradner_rose");
 
     if ($perform_coltron) {
       $config->{"bwa_macs2callpeak_bradner_rose_coltron"} = {
@@ -332,7 +173,7 @@ sub getConfig {
           "mem"      => "40gb"
         },
       };
-      push @summary, ("bwa_macs2callpeak_bradner_rose_coltron");
+      push @$summary, ("bwa_macs2callpeak_bradner_rose_coltron");
     }
   }
 
@@ -354,7 +195,7 @@ sub getConfig {
           "mem"      => "40gb"
         },
       };
-      push @summary, ("bwa_macs2callpeak_replicates");
+      push @$summary, ("bwa_macs2callpeak_replicates");
       $peakTask = "bwa_macs2callpeak_replicates";
     }
 
@@ -373,7 +214,7 @@ sub getConfig {
         "mem"      => "40gb"
       },
     };
-    push @summary, ("bwa_macs2diff");
+    push @$summary, ("bwa_macs2diff");
   }
 
   $config = merge( $config, $processing );
@@ -409,7 +250,7 @@ sub getConfig {
         "mem"      => "10gb"
       },
     };
-    push @summary, ("bamplot");
+    push @$summary, ("bamplot");
   }
 
   $config->{"sequencetask"} = {
@@ -418,8 +259,8 @@ sub getConfig {
     target_dir => "${target_dir}/sequencetask",
     option     => "",
     source     => {
-      step_1 => \@individual,
-      step_2 => \@summary,
+      step_1 => $individual,
+      step_2 => $summary,
     },
     sh_direct => 0,
     pbs       => {
