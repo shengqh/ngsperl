4452e6880469aee9935dccfba25058779139ca41
shengq1 <quanhu.sheng@vanderbilt.edu>
2017-02-10 11:35:06 -0600

    bugfix for remove samples after raw fastqc

--- lib/CQS/countInFastQcVis.R
+++ lib/CQS/countInFastQcVis.R
@@ -1,51 +1,67 @@
+
 resultFile<-outFile
 countInFastQcPreTrim<-parFile1
-countInFastQcPostTrim<-parFile2
-countInFastQcPostRemove<-parFile3
+countInFastQcTrim1<-parFile2
+countInFastQcTrim2<-parFile3
 #resultFile<-commandArgs()[7]
 #countInFastQcPreTrim<-commandArgs()[8]
-#countInFastQcPostTrim<-commandArgs()[9]
-#countInFastQcPostRemove<-commandArgs()[10]
+#countInFastQcTrim1<-commandArgs()[9]
+#countInFastQcTrim2<-commandArgs()[10]
 
 library(ggplot2)
 library(reshape2)
 library(cowplot)
 
 #countInFastQcPreTrim<-"/scratch/cqs/zhaos/vickers/20151017_3018-KCV-45-46/fastqc_pre_trim/result/3018-KCV-45-46.FastQC.summary.reads.tsv"
-#countInFastQcPostTrim<-"/scratch/cqs/zhaos/vickers/20151017_3018-KCV-45-46/fastqc_post_trim/result/3018-KCV-45-46.FastQC.summary.reads.tsv"
-#countInFastQcPostRemove<-"/scratch/cqs/zhaos/vickers/20151017_3018-KCV-45-46/fastqc_post_remove/result/3018-KCV-45-46.FastQC.summary.reads.tsv"
+#countInFastQcTrim1<-"/scratch/cqs/zhaos/vickers/20151017_3018-KCV-45-46/fastqc_post_trim/result/3018-KCV-45-46.FastQC.summary.reads.tsv"
+#countInFastQcTrim2<-"/scratch/cqs/zhaos/vickers/20151017_3018-KCV-45-46/fastqc_post_remove/result/3018-KCV-45-46.FastQC.summary.reads.tsv"
 #resultFile<-"test"
-
-if (countInFastQcPostRemove=="") {
-	count1<-read.delim(countInFastQcPreTrim,header=T)
-	count2<-read.delim(countInFastQcPostTrim,header=T)
-	
-	count1$Reads<-count1$Reads-count2$Reads
-	
-	count1$Label="Removed by Trimming or Removing Sequence"
-	count2$Label="Reads for Mapping"
-	
-	countForFigure<-rbind(count2,count1)
-	pdf(paste0(resultFile,".pdf"),width=14)
-	print(ggplot(countForFigure,aes(x=Sample,y=Reads,fill=Label))+geom_bar(stat="identity", width=0.5)+theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-	dev.off()
-} else {
-	count1<-read.delim(countInFastQcPreTrim,header=T)
-	count2<-read.delim(countInFastQcPostTrim,header=T)
-	count3<-read.delim(countInFastQcPostRemove,header=T)
-	
-	count1$Reads<-count1$Reads-count2$Reads
-	count2$Reads<-count2$Reads-count3$Reads
-	
-	count1$Label="Removed by Removing Sequence"
-	count2$Label="Removed by Trimming"
-	count3$Label="Reads for Mapping"
-	
-	countForFigure<-rbind(count3,count1,count2)
-	pdf(paste0(resultFile,".pdf"),width=14)
-	print(ggplot(countForFigure,aes(x=Sample,y=Reads,fill=Label))+geom_bar(stat="identity", width=0.5)+theme(axis.text.x = element_text(angle = 90, hjust = 1)))
-	dev.off()
+readCount<-function(fileName){
+  count1<-read.delim(fileName,header=T)
+  rownames(count1)<-count1$Sample
+  return(count1)
 }
+
+if (countInFastQcTrim2=="") {
+  count1<-readCount(countInFastQcPreTrim)
+  count2<-readCount(countInFastQcTrim1)
+  
+  count1<-count1[rownames(count2),]
+  count1$Reads<-count1$Reads-count2$Reads
+  
+  if(countInFastQcTrim1 =~ /post_remove/){
+    count1$Label="Removed by Removing Sequence"
+  } else if (countInFastQcTrim1 =~ /post_trim/){
+    count1$Label="Removed by Trimming"
+  } else {
+    count1$Label="Removed by Trimming or Removing Sequence"
+  }  
+  
+  count2$Label="Reads for Mapping"
+  
+  countForFigure<-rbind(count2,count1)
+} else {
+  count1<-readCount(countInFastQcPreTrim)
+  count2<-readCount(countInFastQcTrim1)
+  count3<-readCount(countInFastQcTrim2)
+  
+  count1<-count1[rownames(count3),]
+  count2<-count2[rownames(count3),]
+  
+  count1$Reads<-count1$Reads-count2$Reads
+  count2$Reads<-count2$Reads-count3$Reads
+  
+  count1$Label="Removed by Removing Sequence"
+  count2$Label="Removed by Trimming"
+  count3$Label="Reads for Mapping"
+  
+  countForFigure<-rbind(count3,count1,count2)
+}
+
+width<-max(14, length(unique(countForFigure$Sample)) * 0.15)
+pdf(paste0(resultFile,".pdf"),width=width)
+print(ggplot(countForFigure,aes(x=Sample,y=Reads,fill=Label))+geom_bar(stat="identity", width=0.5)+theme(axis.text.x = element_text(angle = 90, hjust = 1)))
+dev.off()
 
 countForFigureOut<-acast(countForFigure,Label~Sample,value.var="Reads")
 write.csv(countForFigureOut,paste0(resultFile,".Reads.csv"))
