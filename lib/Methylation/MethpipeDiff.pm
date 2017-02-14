dd4d2e42a494222eebc3d117e19c237560cbf611
zhaos <zhaos@cqs3>
2017-02-14 17:11:08 -0600

    diff

--- lib/Methylation/MethpipeDiff.pm
+++ lib/Methylation/MethpipeDiff.pm
@@ -33,8 +33,7 @@ sub perform {
   my @comparison_names = keys %{$comparisons};
 
   my $methfiles = get_raw_files( $config, $section, "methfile" );
-
-  #  my $hmrfiles = get_raw_files( $config, $section, "hmrfile" );
+  my $hmrfiles = get_raw_files( $config, $section, "hmrfile" );
 
   my $shfile = $self->get_task_filename( $pbs_dir, $task_name );
   open( my $sh, ">$shfile" ) or die "Cannot create $shfile";
@@ -51,10 +50,14 @@ sub perform {
 
     my $cur_dir = create_directory_or_die( $result_dir . "/$group_name" );
 
-    my $controlMethFile   = $methfiles->{ $sampleNames[0][1] };
-    my $treatmentMethFile = $methfiles->{ $sampleNames[1][1] };
+    my $controlMethFile   = ${$methfiles->{ $sampleNames[0] }}[0];
+    my $treatmentMethFile = ${$methfiles->{ $sampleNames[1] }}[0];
     my $methdiffFile      = "${group_name}.methdiff";
-
+    my $controlHmrFile   = ${$hmrfiles->{ $sampleNames[0] }}[0];
+    my $treatmentHmrFile = ${$hmrfiles->{ $sampleNames[1] }}[0];
+    my $dmrFile1      = "${controlHmrFile}.DMR";
+    my $dmrFile2      = "${treatmentHmrFile}.DMR";
+    
     my $pbs_file = $self->get_pbs_filename( $pbs_dir, $group_name );
     my $pbs_name = basename($pbs_file);
     my $log = $self->get_log_filename( $log_dir, $group_name );
@@ -69,7 +72,9 @@ sub perform {
 if [ ! -s $methdiffFile ]; then
   methdiff -o $methdiffFile $controlMethFile $treatmentMethFile
 fi
-
+if [[ ! -s $dmrFile1 && ! -s $dmrFile2 ]]; then
+  dmr $methdiffFile $controlHmrFile $treatmentHmrFile $dmrFile1 $dmrFile2
+fi
 ";
     $self->close_pbs( $pbs, $pbs_file );
   }
