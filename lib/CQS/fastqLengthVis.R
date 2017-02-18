0056071459320c2bdf0b72d86ff8872c2a2406c6
shengq1 <quanhu.sheng@vanderbilt.edu>
2017-02-18 17:45:24 -0600

    can ignore samples. can scale the y-axis

--- lib/CQS/fastqLengthVis.R
+++ lib/CQS/fastqLengthVis.R
@@ -14,18 +14,32 @@ lengthAll<-NULL
 for (i in 1:nrow(lengthFiles)) {
 	lengthFile<-lengthFiles[i,1]
 	sampleName<-lengthFiles[i,2]
+  if(exists("ignoreSamples") && sampleName %in% ignoreSamples){
+    cat("Sample ", sampleName, " ignored.\n")
+    next
+  }
 	lengthOne<-read.delim(lengthFile,as.is=T,header=T)
 	lengthOne$Sample<-sampleName
 	lengthAll<-rbind(lengthAll,lengthOne)
 }
 
 lengthAllTable<-acast(lengthAll,Len~Sample,value.var="Count")
-write.csv(lengthAllTable,paste0(resultFile,".csv"))
+write.csv(lengthAllTable,paste0(resultFile,".csv"), row.names=F)
 
-width=max(2000, 800 * (ceiling(sqrt(ncol(lengthAllTable)))+1))
+cellWidth=800
+scales="free_x"
+if(exists("free_y") && free_y){
+  cellWidth=1000
+  scales="free"
+}
+
+width=max(2000, cellWidth * (ceiling(sqrt(ncol(lengthAllTable)))+1))
 png(paste0(resultFile,".png"),width=width,height=width,res=300)
-p=ggplot(lengthAll, aes(x=Len, y=Count)) + geom_bar(stat="identity", width=.5) + facet_wrap(~Sample, scales = "free_x") +
-		xlab("Read length") + ylab("Read count")
+p=ggplot(lengthAll, aes(x=Len, y=Count)) + 
+  geom_bar(stat="identity", width=.5) + 
+  facet_wrap(~Sample, scales=scales) +
+	xlab("Read length") + 
+	ylab("Read count")
 print(p)
 dev.off()
 
@@ -37,8 +51,15 @@ if (groupFileList!="") {
 	datBySampleGroup<-mergeTableBySampleGroup(lengthAllTable,sampleToGroup)
 	temp<-melt(datBySampleGroup)
 	colnames(temp)<-c("Position","Group","Percent")
-	png(paste0(resultFile,".group.png"),width=2000,height=1500,res=300)
-	p<-ggplot(temp,aes(x=Position,y=Percent,colour= Group))+geom_line()
+  p<-ggplot(temp,aes(x=Position,y=Percent,colour= Group))+geom_line()
+
+  if(exists("facet") && facet){
+    width=max(2000, cellWidth * (ceiling(sqrt(ncol(temp)))+1))
+  	png(paste0(resultFile,".group.png"),width=width,height=width,res=300)
+    p<-p+facet_wrap(~Group, scales="free")
+  }else{
+    png(paste0(resultFile,".group.png"),width=2000,height=1500,res=300)
+  }  
 	print(p)
 	dev.off()
 }
