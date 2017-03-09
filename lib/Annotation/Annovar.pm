dd75fa7f5e8507c5717c37232f23b5606ca8c5f9
slzhao <zhaoshilin@gmail.com>
2017-03-09 13:41:07 -0600

    bugfix

--- lib/Annotation/Annovar.pm
+++ lib/Annotation/Annovar.pm
@@ -100,7 +100,7 @@ sub perform {
     table_annovar.pl $passinput $annovarDB $option --outfile $annovar --remove
   fi";
       } elsif ($isBed) {
-        $passinput = change_extension( $filename, ".avinput" );
+        $passinput = $filename.".avinput";
         $runcmd = "perl -lane 'my \$fileColNum=scalar(\@F);my \$fileColPart=join(\"\t\",\@F[3..(\$fileColNum-1)]);print \"\$F[0]\t\$F[1]\t\$F[2]\t0\t-\t\$fileColPart\"' $sampleFile > $passinput 
   if [ -s $passinput ]; then
     table_annovar.pl $passinput $annovarDB $option --outfile $annovar --remove
