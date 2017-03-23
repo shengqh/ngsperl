e973e696ad93b619bc6823965f9f52244af6f766
shengq1 <shengq1@cqs2>
2017-03-23 11:12:05 -0500

    can accept list_file as input. change filename to sampleName user defined or gsm from list_file

--- lib/SRA/FastqDump.pm
+++ lib/SRA/FastqDump.pm
@@ -10,6 +10,8 @@ use CQS::SystemUtils;
 use CQS::FileUtils;
 use CQS::Task;
 use CQS::StringUtils;
+use List::Util qw(first);
+use Utils::CollectionUtils;
 
 our @ISA = qw(CQS::Task);
 
@@ -22,6 +24,34 @@ sub new {
   return $self;
 }
 
+sub getGsmSrrMap {
+  my $listFile = shift;  
+  open my $list_handle, "<$listFile";
+  my $first_line = <$list_handle>;
+  close $list_handle;
+  my @columns = split( '\t', $first_line );
+  my $srrIndex = first { $columns[$_] eq 'Run_s' } 0 .. $#columns;
+  my $gsmIndex = first { $columns[$_] eq 'Sample_Name_s' } 0 .. $#columns;
+  my $taMap = readDictionaryByIndex( $listFile, $gsmIndex, $srrIndex, 1 );
+  return $taMap;
+}
+
+sub getSraFiles {
+  my ( $config, $section ) = @_;
+  my $result;
+
+  if ( defined $config->{$section}{"list_file"} ) {
+    my $taMap = getGsmSrrMap( $config->{$section}{"list_file"} );
+    for my $gsm ( keys %$taMap ) {
+      $result->{$gsm} = [ $taMap->{$gsm} ];
+    }
+  }
+  else {
+    $result = get_raw_files( $config, $section );
+  }
+  return $result;
+}
+
 sub perform {
   my ( $self, $config, $section ) = @_;
 
@@ -32,20 +62,14 @@ sub perform {
     $ispaired = 0;
   }
 
-  my %raw_files;
-  
-  if(defined $config->{$section}{"source"}){
-    my $raws = $config->{$section}{"source"};
-    
-  }
-  %raw_files = %{ get_raw_files( $config, $section ) };
+  my $raw_files = getSraFiles( $config, $section );
 
   my $shfile = $self->get_task_filename( $pbs_dir, $task_name );
   open( my $sh, ">$shfile" ) or die "Cannot create $shfile";
   print $sh get_run_command($sh_direct) . "\n";
 
-  for my $sample_name ( sort keys %raw_files ) {
-    my $sample_file = $raw_files{$sample_name}->[0];
+  for my $sample_name ( sort keys %$raw_files ) {
+    my $sample_file = $raw_files->{$sample_name}->[0];
 
     my $pbs_file = $self->get_pbs_filename( $pbs_dir, $sample_name );
     my $pbs_name = basename($pbs_file);
@@ -54,19 +78,20 @@ sub perform {
 
     print $sh "\$MYCMD ./$pbs_name \n";
 
-    my $isNameOnly = $sample_file !~ /\.sra/;
-    my $filePrefix = $isNameOnly ? $sample_file : $sample_name;
-
-    my $final_file = $ispaired ? $filePrefix . "_1.fastq.gz" : $filePrefix . ".fastq.gz";
+    my $final_file = $ispaired ? $sample_name . "_1.fastq.gz" : $sample_name . ".fastq.gz";
     my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $final_file );
-    if ($isNameOnly) {
-      print $pbs "fastq-dump --split-3 --gzip --origfmt --helicos $sample_file \n";
+
+    if ( $sample_file !~ /\.sra/ ) {
+      my $six = substr( $sample_file, 0, 6 );
+      print $pbs "wget ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/${six}/${sample_file}/${sample_file}.sra -O ${sample_name}.sra\n";
     }
     else {
-      print $pbs "ln -s $sample_file ${sample_name}.sra
-fastq-dump --split-3 --gzip --origfmt --helicos ${sample_name}.sra
-rm ${sample_name}.sra";
+      print $pbs "ln -s $sample_file ${sample_name}.sra \n";
     }
+    print $pbs "fastq-dump --split-3 --gzip --origfmt --helicos ${sample_name}.sra
+rm ${sample_name}.sra
+";
+
     $self->close_pbs( $pbs, $pbs_file );
   }
   close $sh;
@@ -75,7 +100,7 @@ rm ${sample_name}.sra";
     chmod 0755, $shfile;
   }
 
-  print "!!!shell file $shfile created, you can run this shell file to submit all Bam2Fastq tasks.\n";
+  print "!!!shell file $shfile created, you can run this shell file to submit all " . $self->{_name} . " tasks.\n";
 
   #`qsub $pbs_file`;
 }
@@ -90,28 +115,40 @@ sub result {
     $ispaired = 0;
   }
 
-  my %raw_files = %{ get_raw_files( $config, $section ) };
+  my $raw_files = getSraFiles( $config, $section );
 
   my $result = {};
-  for my $sample_name ( keys %raw_files ) {
-    my $sample_file = $raw_files{$sample_name}->[0];
+  for my $sample_name ( keys %$raw_files ) {
 
-    my $isNameOnly = $sample_file !~ /\.sra/;
-    my $filePrefix = $isNameOnly ? $sample_file : $sample_name;
-
-    my $final_file = $ispaired ? $filePrefix . "_1.fastq.gz" : $filePrefix . ".fastq.gz";
+    my $final_file = $ispaired ? $sample_name . "_1.fastq.gz" : $sample_name . ".fastq.gz";
 
     my @result_files = ();
     if ($ispaired) {
-      push( @result_files, $result_dir . "/" . $filePrefix . "_1.fastq.gz" );
-      push( @result_files, $result_dir . "/" . $filePrefix . "_2.fastq.gz" );
+      push( @result_files, $result_dir . "/" . $sample_name . "_1.fastq.gz" );
+      push( @result_files, $result_dir . "/" . $sample_name . "_2.fastq.gz" );
     }
     else {
-      push( @result_files, $result_dir . "/" . $filePrefix . ".fastq.gz" );
+      push( @result_files, $result_dir . "/" . $sample_name . ".fastq.gz" );
     }
 
     $result->{$sample_name} = filter_array( \@result_files, $pattern );
   }
+  return $result;
+}
+
+sub get_pbs_files {
+  my ( $self, $config, $section ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct ) = get_parameter( $config, $section );
+
+  my $raw_files = getSraFiles( $config, $section );
+
+  my $result = {};
+
+  for my $sample_name ( sort keys %$raw_files ) {
+    $result->{$sample_name} = $self->get_pbs_filename( $pbs_dir, $sample_name );
+  }
+
   return $result;
 }
 
