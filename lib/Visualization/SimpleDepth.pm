c38abd3b8ebba19ea601619b054585f4528ad6d1
shengq1 <shengq1@cqs3>
2017-04-18 14:39:50 -0500

    get depth file with header

---
+++ lib/Visualization/SimpleDepth.pm
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+package Visualization::SimpleDepth;
+
+use strict;
+use warnings;
+use Data::Dumper;
+use File::Basename;
+use CQS::PBS;
+use CQS::ConfigUtils;
+use CQS::SystemUtils;
+use CQS::FileUtils;
+use CQS::NGSCommon;
+use CQS::GroupTask;
+use CQS::StringUtils;
+
+our @ISA = qw(CQS::GroupTask);
+
+my $directory;
+
+sub new {
+  my ($class) = @_;
+  my $self = $class->SUPER::new();
+  $self->{_name}   = __PACKAGE__;
+  $self->{_suffix} = "_sd";
+  bless $self, $class;
+  return $self;
+}
+
+sub perform {
+  my ( $self, $config, $section ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct, $cluster ) = get_parameter( $config, $section );
+
+  my $bamFiles = get_raw_files( $config, $section );
+
+  my @bamNames = sort keys %$bamFiles;
+  my $bamNameString = join( '\t', @bamNames );
+
+  my $pbs_file = $self->get_pbs_filename( $pbs_dir, $task_name );
+  my $log = $self->get_log_filename( $log_dir, $task_name );
+  my $log_desc = $cluster->get_log_description($log);
+
+  my $final_file = $result_dir . "/" . $task_name . ".depth";
+
+  my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $final_file );
+  print $pbs "
+echo -e \"Chr\\tPosition\\t$bamNameString\" > ${task_name}.depth
+samtools depth $option ";
+  for my $bamName (@bamNames) {
+    print $pbs " " . $bamFiles->{$bamName}[0];
+  }
+  print $pbs " >> ${task_name}.depth \n";
+  $self->close_pbs( $pbs, $pbs_file );
+}
+
+sub result {
+  my ( $self, $config, $section, $pattern ) = @_;
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct ) = get_parameter( $config, $section, 0 );
+
+  my $final_file = $result_dir . "/" . $task_name . ".depth";
+  my $result = {};
+  $result->{$task_name} = filter_array( [$final_file], $pattern );
+
+  return $result;
+}
+
+1;
