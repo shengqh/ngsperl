955c8ad980c3a42991c10e0ebb3eb0d626cb2e0c
shengq1 <shengq1@cqs3>
2017-05-10 21:07:08 -0500

    remove flagstat

--- lib/Alignment/STAR.pm
+++ lib/Alignment/STAR.pm
@@ -84,7 +84,6 @@ STAR $option --outSAMattrRGline $rgline --runThreadN $thread --genomeDir $genome
 if [ -s $final ]; then
   samtools index $final
   $chromosome_grep_command
-  samtools flagstat $final > ${final}.stat
   rm -rf ${sample_name}__STARgenome ${sample_name}__STARpass1 ${sample_name}_SJ.out.tab ${sample_name}_Log.progress.out
 fi
 
