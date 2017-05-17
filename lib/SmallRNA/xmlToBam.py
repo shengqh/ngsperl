23e4606a62def293183136e5c2d5d5f25e97d313
shengq1 <shengq1@cqs3>
2017-05-16 20:36:56 -0500

    smallRNA xml to bam for IGV

--- lib/SmallRNA/xmlToBam.py
+++ lib/SmallRNA/xmlToBam.py
@@ -7,50 +7,77 @@ import argparse
 import xml.etree.ElementTree as ET
 
 DEBUG = 0
-NTA_TAG = ":CLIP_"
 
 if DEBUG:
-  inputFile="Z:/Shared/Labs/Vickers Lab/Tiger/projects/20161223_smallRNA_3018-KCV-77_78_79_86_v3.T/host_genome/bowtie1_genome_1mm_NTA_smallRNA_count/result/APOB_SRBIKO_86_01/APOB_SRBIKO_86_01.count.mapped.xml"
-  outputFile="H:/temp/APOB_SRBIKO_86_01.bam"
+  inputFile="/scratch/cqs/shengq1/vickers/20161223_smallRNA_3018-KCV-77_78_79_86_v3.T/host_genome/bowtie1_genome_1mm_NTA_smallRNA_count/result/APOB_SRBIKO_86_01/APOB_SRBIKO_86_01.count.mapped.xml"
+  oldbamFile = "/scratch/cqs/shengq1/vickers/20161223_smallRNA_3018-KCV-77_78_79_86_v3.T/host_genome/bowtie1_genome_1mm_NTA/result/APOB_SRBIKO_86_01/APOB_SRBIKO_86_01.bam"
+  outputFilePrefix="/scratch/cqs/shengq1/temp/APOB_SRBIKO_86_01"
 else:
   parser = argparse.ArgumentParser(description="Generate smallRNA BAM from mapped xml.",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-  parser.add_argument('-i', '--input', action='store', nargs='?', help='Input Fastq file')
-  parser.add_argument('-o', '--output', action='store', nargs='?', help="Output NTA Fastq file")
-  parser.set_defaults(ccaa=False)
+  parser.add_argument('-i', '--input', action='store', nargs='?', help='Input count xml file')
+  parser.add_argument('-b', '--oldbamfile', action='store', nargs='?', help="Original bam file")
+  parser.add_argument('-o', '--output', action='store', nargs='?', help="Output bam file")
 
   args = parser.parse_args()
   
   print(args)
   
   inputFile = args.input
-  outputFile = args.output
+  oldbamFile = args.oldbamfile
+  outputFilePrefix = args.output
 
-logger = logging.getLogger('fastqSmallRnaNTA')
+logger = logging.getLogger('xmlToBam')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)-8s - %(message)s')
 
+sam = pysam.Samfile(oldbamFile)
+header = sam.header
+chrMap = {}
+for idx, sq in enumerate(header["SQ"]):
+  chr = sq['SN']
+  chrMap[chr] = idx
+  if chr.startswith("chr"):
+    chrMap[chr[3:]] = idx
+  
 tree = ET.parse(inputFile)
 root = tree.getroot()
 
 queries = root.find('queries')
-for query in queries.findall('query'):
-  print(query.attrib)
-# 
-# with pysam.AlignmentFile(tmpfilename, "wb", header=header) as outf:
-#     a = pysam.AlignedSegment()
-#     a.query_name = "read_28833_29006_6945"
-#     a.query_sequence="AGCTTAGCTAGCTACCTATATCTTGGTCTTGGCCG"
-#     a.flag = 99
-#     a.reference_id = 0
-#     a.reference_start = 32
-#     a.mapping_quality = 20
-#     a.cigar = ((0,10), (2,1), (0,25))
-#     a.next_reference_id = 0
-#     a.next_reference_start=199
-#     a.template_length=167
-#     a.query_qualities = pysam.qualitystring_to_array("<<<<<<<<<<<<<<<<<<<<<:<9/,&,22;;<<<")
-#     a.tags = (("NM", 1),
-#               ("RG", "L1"))
-#     outf.write(a)
-    
+
+unsorted = outputFilePrefix + ".unsorted.bam"
+with pysam.AlignmentFile(unsorted, "wb", header=header) as outf:
+  for query in queries.findall('query'):
+    query_count = int(query.get("count")) + 1
+    query_name = query.get("name")
+    query_sequence=query.get("sequence")
+    for loc in query.findall("location"):
+      strand = loc.get("strand")
+
+      a = pysam.AlignedSegment()
+      a.query_sequence=query_sequence
+      
+      if(strand == '+'):
+        a.flag = 0
+      else:
+        a.flag = 16
+        
+      a.reference_id = chrMap[ loc.get("seqname")]
+      a.reference_start = int(loc.get("start"))
+      a.mapping_quality = 255
+      a.cigarstring = loc.get("cigar")
+      a.next_reference_id = -1
+      a.next_reference_start=-1
+      a.template_length=len(query_sequence)
+      #a.query_qualities = '*'
+      a.tags = (("XA", int(loc.get("score"))),
+                ("MD", loc.get("mdz")),
+                ("NM", int(loc.get("nmi"))))
+
+      for idx in range(1, query_count):
+        a.query_name = query_name + ":" + str(idx)
+        outf.write(a)
+   
+pysam.sort(unsorted, outputFilePrefix)
+pysam.index(outputFilePrefix + ".bam")
+os.remove(unsorted)
