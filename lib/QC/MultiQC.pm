3bf873c64543e0c3748412105df4e42f39485392
Quanhu Sheng <shengqh@gmail.com>
2017-06-10 17:50:41 -0500

    init MultiQC

---
+++ lib/QC/MultiQC.pm
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+package QC::MultiQC;
+
+use strict;
+use warnings;
+use File::Basename;
+use CQS::PBS;
+use CQS::ConfigUtils;
+use CQS::SystemUtils;
+use CQS::FileUtils;
+use CQS::NGSCommon;
+use CQS::StringUtils;
+use CQS::UniqueTask;
+use Pipeline::PipelineUtils;
+
+our @ISA = qw(CQS::UniqueTask);
+
+sub new {
+  my ($class) = @_;
+  my $self = $class->SUPER::new();
+  $self->{_name}   = __PACKAGE__;
+  $self->{_suffix} = "_mqc";
+  bless $self, $class;
+  return $self;
+}
+
+sub perform {
+  my ( $self, $config, $section ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct, $cluster, $thread ) = get_parameter( $config, $section );
+
+  my $root_dir = getOption( $config, $section, "root_dir" );
+
+  my $pbs_file = $self->get_pbs_filename( $pbs_dir, $task_name );
+  my $pbs_name = basename($pbs_file);
+  my $log      = $self->get_log_filename( $log_dir, $task_name );
+  my $log_desc = $cluster->get_log_description($log);
+  my $pbs      = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir );
+
+  print $pbs "cd $result_dir
+multiqc $option $root_dir
+";
+
+  $self->close_pbs( $pbs, $pbs_file );
+}
+
+sub result {
+  my ( $self, $config, $section, $pattern ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct ) = get_parameter( $config, $section, 0 );
+  my $result = {};
+
+  my @result_files = ();
+  $result->{$task_name} = filter_array( \@result_files, $pattern );
+  return $result;
+}
+
+1;
