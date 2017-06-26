4a4b3190aef48de5bacb99ee5ae6dbfa64178871
shengq1 <shengq1@cqs3>
2017-06-26 09:37:09 -0500

    bugfix

--- lib/Chipseq/Rose2.pm
+++ lib/Chipseq/Rose2.pm
@@ -140,7 +140,7 @@ sub result {
 
   my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct ) = get_parameter( $config, $section, 0 );
 
-  my %treatments_files = %{ $self->get_grouped_raw_files( $config, $section, "groups" ) };
+  my %treatments_files = %{ get_grouped_raw_files( $config, $section, "groups" ) };
 
   my %binding_site_beds = %{ get_raw_files( $config, $section, "binding_site_bed" ) };
 
