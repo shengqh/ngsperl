804108e922d03408d6c5eff51888395c4996656e
shengq2 <shengqh@gmail.com>
2017-09-18 11:03:00 -0500

    output count, fpkm and tpm

--- lib/TCGA/PrepareRnaseq.r
+++ lib/TCGA/PrepareRnaseq.r
@@ -1,21 +1,49 @@
-library(TCGAbiolinks)
 library(SummarizedExperiment)
 
-DEBUG=0
-
-writeFile<-function(dat, file){
-  newDat<-cbind(data.frame(ID=rownames(dat)), dat)
-  rownames(newDat)<-rownames(dat)
-  write.table(newDat, file=file, sep="\t", row.names=F, quote=F)
+counts_to_fpkm <- function(counts, featureLength) {
+  stopifnot(length(featureLength) == nrow(counts))
+  
+  # Process one column at a time.
+  result <- do.call(cbind, lapply(1:ncol(counts), function(i) {
+    countI<-counts[,i]
+    totalCount<-sum(countI)
+    res<-(10^9)*countI/(featureLength*totalCount)
+    return(res)
+  }))
+  
+  # Copy the row and column names from the original matrix.
+  colnames(result) <- colnames(counts)
+  rownames(result) <- rownames(counts)
+  return(result)
 }
+
+counts_to_tpm <- function(counts, featureLength) {
+  stopifnot(length(featureLength) == nrow(counts))
+  
+  logFeatureLength<-log(featureLength)
+  
+  # Process one column at a time.
+  tpm <- do.call(cbind, lapply(1:ncol(counts), function(i) {
+    rate = log(counts[,i]) - logFeatureLength[i]
+    denom = log(sum(exp(rate)))
+    exp(rate - denom + log(1e6))
+  }))
+  
+  # Copy the row and column names from the original matrix.
+  colnames(tpm) <- colnames(counts)
+  rownames(tpm) <- rownames(counts)
+  return(tpm)
+}
+
+DEBUG=0
 
 args = commandArgs(trailingOnly=TRUE)
 cancerName = args[1]
 outputFolder = args[2]
 
 if(DEBUG){
-  cancerName = "SKCM"
-  outputFolder = "/scratch/cqs/shengq1/guoyan/prepareRnaseq/result/SKCM"
+  cancerName = "COAD"
+  outputFolder = "/scratch/cqs/shengq2/guoyan/prepareRnaseq/result/COAD"
 }
 
 setwd(outputFolder)
@@ -24,6 +52,7 @@ project = paste0("TCGA-", cancerName)
 countFile<-paste0(cancerName, ".rnaseq2.count.rdata")
 
 if(!file.exists(countFile)){
+  library(TCGAbiolinks)
   query <- GDCquery(project = project, 
                     data.category = "Gene expression",
                     data.type = "Gene expression quantification",
@@ -49,93 +78,65 @@ if(!file.exists(countFile)){
 }
 
 pos<-rowRanges(rnaseq)
-posframe<-data.frame(pos)
-posframe<-posframe[,c("ensembl_gene_id", "seqnames", "start", "end")]
-colnames(posframe)<-c("geneid", "chr", "s1", "s2")
+pos<-data.frame(pos)
+pos<-pos[,c("ensembl_gene_id", "seqnames", "start", "end", "gene_id")]
 
 exp<-assay(rnaseq)
 rownames(exp)<-pos$ensembl_gene_id
 
-isdup<-duplicated(posframe$geneid)
-posframe<-posframe[!isdup,]
+isdup<-duplicated(pos$ensembl_gene_id)
+pos<-pos[!isdup,]
 exp<-exp[!isdup,]
 
 #sort pos by chromosome and start
-posframe$chr<-substring(posframe$chr, 4)
-posframe$chrX<-grepl('[XYZ]', posframe$chr)
-posframe<-posframe[with(posframe, order(chrX, chr, s1, s2)), c("geneid", "chr", "s1", "s2")]
-posframe$length<-posframe$s2-posframe$s1
+pos$length<-pos$end-pos$start+1
+pos$seqnames<-substring(pos$seqnames, 4)
+pos$seqnamesX<-grepl('[XYZ]', pos$seqnames)
+pos$seqnamesN<-as.numeric(pos$seqnames)
+pos<-pos[with(pos, order(seqnamesX, seqnamesN, seqnames, start, end)), c("ensembl_gene_id", "seqnames",    "start" ,     "end"      ,"length","gene_id")]
 
 #save position file
-write.table(posframe, file=paste0(cancerName, ".rnaseq2.gene.pos"), sep="\t", row.names=F, quote=F)
+genepos<-pos[,c("ensembl_gene_id", "seqnames",    "start" ,     "end" )]
+colnames(genepos)<-c("geneid", "chr", "s1", "s2")
+write.table(genepos, file=paste0(cancerName, ".rnaseq2.gene.pos"), sep="\t", row.names=F, quote=F)
 
-exp<-exp[as.character(posframe$geneid),]
+exp<-exp[as.character(pos$ensembl_gene_id),]
 colnames(exp)<-substring(colnames(exp), 1, 15)
+colnames(pos)<-c("Feature", "Feature_chr", "Feature_start", "Feature_end", "Feature_length", "Feature_gene_name")
 
-writeFile(exp, file=paste0(cancerName, ".rnaseq2.count.tsv"))
+fpkm<-counts_to_fpkm(exp, pos$Feature_length)
+tpm<-counts_to_tpm(exp, pos$Feature_length)
 
-normal<-exp[,grepl("11$", colnames(exp))]
-if(is.matrix(normal) && ncol(normal) > 0){
-  writeFile(normal, paste0(cancerName, ".rnaseq2.count.normal.tsv"))
-}
+normalNames<-colnames(exp)[grepl("11$", colnames(exp))]
+tumorNames<-colnames(exp)[grepl("01$", colnames(exp))]
 
-tumor<-exp[,grepl("01$", colnames(exp))]
-if(length(tumor) > 0){
-  writeFile(tumor, paste0(cancerName, ".rnaseq2.count.tumor.tsv"))
-}
+names(normalNames)<-gsub("-11", "", normalNames)
+names(tumorNames)<-gsub("-01", "", tumorNames)
 
-totalCounts<-apply(exp, 2, sum)
+pairShortNames<-intersect(names(normalNames), names(tumorNames))
+normalPairNames<-normalNames[pairShortNames]
+tumorPairNames<-tumorNames[pairShortNames]
 
-#per kilobases
-genelength<-(posframe$s2 - posframe$s1 + 1) / 1000 
+exportNames<-list()
+exportNames[["normal"]]<-normalNames
+exportNames[["tumor"]]<-tumorNames
+exportNames[["normal_paired"]]<-normalPairNames
+exportNames[["tumor_paired"]]<-tumorPairNames
 
-#by column
-fpkm<-exp / totalCounts
+dataMap<-list()
+dataMap[["count"]]<-exp
+dataMap[["fpkm"]]<-fpkm
+dataMap[["tpm"]]<-tpm
 
-#by row
-fpkm <-t(t(fpkm) / genelength)
-fpkm <- fpkm * 1000000
-
-writeFile(fpkm, file=paste0(cancerName, ".rnaseq2.fpkm.tsv"))
-
-normal<-fpkm[,grepl("11$", colnames(fpkm))]
-if(is.matrix(normal) && ncol(normal) > 0){
-  writeFile(normal, paste0(cancerName, ".rnaseq2.fpkm.normal.tsv"))
-}
-
-tumor<-fpkm[,grepl("01$", colnames(fpkm))]
-if(length(tumor) > 0){
-  writeFile(tumor, paste0(cancerName, ".rnaseq2.fpkm.tumor.tsv"))
-}
-
-
-counts_to_tpm <- function(counts, featureLength) {
-  stopifnot(length(featureLength) == nrow(counts))
+for(dataName in names(dataMap)){
+  data<-dataMap[[dataName]]  
+  write.table(cbind(pos, data), file=paste0(cancerName, ".rnaseq2.", dataName, ".tsv"), sep="\t", row.names=F, quote=F)
   
-  logFeatureLength<-log(featureLength)
-  
-  # Process one column at a time.
-  tpm <- do.call(cbind, lapply(1:ncol(counts), function(i) {
-    rate = log(counts[,i]) - logFeatureLength[i]
-    denom = log(sum(exp(rate)))
-    exp(rate - denom + log(1e6))
-  }))
-
-  # Copy the row and column names from the original matrix.
-  colnames(tpm) <- colnames(counts)
-  rownames(tpm) <- rownames(counts)
-  return(tpm)
-}
-
-tpms<-counts_to_tpm(exp, genelength)
-writeFile(tpms, file=paste0(cancerName, ".rnaseq2.tpm.tsv"))
-
-normal<-tpms[,grepl("11$", colnames(tpms))]
-if(is.matrix(normal) && ncol(normal) > 0){
-  writeFile(normal, paste0(cancerName, ".rnaseq2.tpm.normal.tsv"))
-}
-
-tumor<-tpms[,grepl("01$", colnames(tpms))]
-if(length(tumor) > 0){
-  writeFile(tumor, paste0(cancerName, ".rnaseq2.tpm.tumor.tsv"))
+  for(exportName in names(exportNames)){
+    fileNames<-exportNames[[exportName]]
+    subdata<-data[,fileNames]
+    if(is.matrix(subdata) && ncol(subdata) > 0){
+      write.table(cbind(pos, subdata), file=paste0(cancerName, ".rnaseq2.", dataName, "." , exportName, ".tsv"), sep="\t", row.names=F, quote=F)
+    }
+  }
 }
