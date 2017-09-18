5d54996643d1008f5f639369a67841bb78aee959
shengq2 <shengqh@gmail.com>
2017-09-18 11:04:15 -0500

    read gtex result

---
+++ lib/eQTL/GTExUtils.py
@@ -0,0 +1,21 @@
+import csv
+
+class GTExItem:
+  def __init__(self, row):
+    parts = row["snp"].split('_')
+    self.Locus = parts[0] + "_" + parts[1]
+    self.RefAllele = parts[2];
+    self.AltAllele = parts[3];
+    self.Gene = row["gene"].split('.')[0];
+    self.Beta = float(row["beta"]);
+    self.Key = self.Locus + ":" + self.Gene
+
+def readGTExResult(fileName):
+  result = list()
+  with open(fileName, "r") as f:
+    mycsv = csv.DictReader(f, delimiter="\t")
+    for row in mycsv:
+      item = GTExItem(row)
+      result.append(item)
+  return(result)
+  
