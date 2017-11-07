79ae3c1832833059c2a8a28e56c14b821e0ca267
shengq2 <shengqh@gmail.com>
2017-11-07 14:19:49 -0600

    adjust the output format

--- lib/eQTL/MatrixEQTLOverlap.py
+++ lib/eQTL/MatrixEQTLOverlap.py
@@ -26,55 +26,73 @@ def findOverlap(inputFile, inputBimFile, inputName, compareFile, compareBimFile,
   logger.info("input eQTL = %d" % len(inputResult))
 
   compareMap = {p.Name:p for p in readPlinkSNP(compareBimFile) if p.Chromosome != "0"}
-  compareResultMap = {p.SNP + ":" + p.Gene:p for p in readMatrixEQTLResult(compareFile)}
-  logger.info("compare eQTL = %d" % len(compareResultMap))
+  compareResult = readMatrixEQTLResult(compareFile)
+  logger.info("compare eQTL = %d" % len(compareResult))
 
-  matched = 0
-  unmatched = 0
-  tmpFile = outputFile + ".tmp"
-  with open(tmpFile, "w") as f:
-    f.write("Locus\tGene\t%s_Major\t%s_Minor\t%s_Beta\t%s_Major\t%s_Minor\t%s_Beta\tBetaMatch\n" %(inputName, inputName, inputName, compareName, compareName, compareName));
-    for i in inputResult:
-      iAllele = inputMap[i.SNP];
-      iKey = i.SNP + ":" + i.Gene;
-      if iKey in compareResultMap:
-        c = compareResultMap[iKey]
-        cAllele = compareMap[i.SNP]
-        betaMatch = False;
-        if (iAllele.MajorAllele == cAllele.MajorAllele) and (iAllele.MinorAllele == cAllele.MinorAllele):
-          betaMatch = i.Beta * c.Beta > 0
-        elif (iAllele.MajorAllele == cAllele.MinorAllele) and (iAllele.MinorAllele == cAllele.MajorAllele):
-          betaMatch = i.Beta * c.Beta < 0
-        else:
-          continue;
+  pvalues = [0.01, 0.001, 0.0001, 0.00001, 0.000001]
+  with open(outputFile + ".info", "w") as fi:
+    fi.write("pValue\t%s\t%s\tOverlap\tOverlapRate\tMatched\tConflicting_eQTL\teQTL_inconsistency\n" % (inputName, compareName))
+    for pvalue in pvalues:
+      logger.info("pvalue = %f" % pvalue)
+      matched = 0
+      unmatched = 0
+      curInputResult = [p for p in inputResult if p.Pvalue <= pvalue]
+      curCompareResultMap = {p.SNP + ":" + p.Gene:p for p in compareResult if p.Pvalue <= pvalue}
+      
+      if pvalue == pvalues[0]:
+        tmpFile = outputFile + ".tmp"
+        f = open(tmpFile, "w")
+        f.write("Locus\tGene\t%s_Major\t%s_Minor\t%s_Pvalue\t%s_Beta\t%s_Major\t%s_Minor\t%s_Pvalue\t%s_Beta\tBetaMatch\n" %(inputName, inputName, inputName, inputName, compareName, compareName, compareName, compareName));
         
-        if betaMatch:
-          matched = matched + 1
-        else:
-          unmatched = unmatched + 1
-        
-        f.write("%s\t%s\t%s\t%s\t%f\t%s\t%s\t%f\t%r\n" % (
-            iAllele.Locus,
-            i.Gene,
-            iAllele.MajorAllele,
-            iAllele.MinorAllele,
-            i.Beta,
-            cAllele.MajorAllele,
-            cAllele.MinorAllele,
-            c.Beta,
-            betaMatch));
+      try:
+        for i in curInputResult:
+          iAllele = inputMap[i.SNP];
+          iKey = i.SNP + ":" + i.Gene;
+          if iKey in curCompareResultMap:
+            c = curCompareResultMap[iKey]
+            cAllele = compareMap[i.SNP]
+            betaMatch = False;
+            if (iAllele.MajorAllele == cAllele.MajorAllele) and (iAllele.MinorAllele == cAllele.MinorAllele):
+              betaMatch = i.Beta * c.Beta > 0
+            elif (iAllele.MajorAllele == cAllele.MinorAllele) and (iAllele.MinorAllele == cAllele.MajorAllele):
+              betaMatch = i.Beta * c.Beta < 0
+            else:
+              continue;
             
-  if os.path.isfile(outputFile):
-    os.remove(outputFile)
-  os.rename(tmpFile, outputFile)
-  
-  with open(outputFile + ".info", "w") as f:
-    f.write("Type\tCount\n")
-    f.write("%s\t%d\n" % (inputName, len(inputResult)))
-    f.write("%s\t%d\n" % (compareName, len(compareResultMap)))
-    f.write("Matched\t%d\n" % matched)
-    f.write("Unmatched\t%d\n" % unmatched)
-    f.write("UnmatchedPercentage\t%4.2f%%\n" % (unmatched * 100.0 / (matched + unmatched)))
+            if betaMatch:
+              matched = matched + 1
+            else:
+              unmatched = unmatched + 1
+            
+            if pvalue == pvalues[0]:
+              f.write("%s\t%s\t%s\t%s\t%f\t%f\t%s\t%s\t%f\t%f\t%r\n" % (
+                iAllele.Locus,
+                i.Gene,
+                iAllele.MajorAllele,
+                iAllele.MinorAllele,
+                i.Pvalue,
+                i.Beta,
+                cAllele.MajorAllele,
+                cAllele.MinorAllele,
+                c.Pvalue,
+                c.Beta,
+                betaMatch));
+      finally:
+        if pvalue == pvalues[0]:
+          f.close()
+          if os.path.isfile(outputFile):
+            os.remove(outputFile)
+          os.rename(tmpFile, outputFile)
+      overlap = matched + unmatched
+      overlapRate = 1.0 * overlap / min(len(curInputResult), len(curCompareResultMap))
+      fi.write("%f\t%d\t%d\t%d\t%f\t%d\t%d\t%f\n" % (pvalue,
+                                                               len(curInputResult), 
+                                                               len(curCompareResultMap),
+                                                               overlap,
+                                                               overlapRate,
+                                                               matched,
+                                                               unmatched,
+                                                               (unmatched * 1.0 / (matched + unmatched))))
   
   logger.info("Done.")
         
