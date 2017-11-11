05b0ab1ff0d3af7a543eb04c49fdb06925d47990
shengq2 <shengqh@gmail.com>
2017-11-11 11:53:30 -0600

    fix error file path

--- examples/pipeline_exomeseq_simple.pl
+++ examples/pipeline_exomeseq_simple.pl
@@ -10,34 +10,34 @@ use Pipeline::ExomeSeq;
 
 my $def = {
   task_name  => "Adenoma",
-  target_dir => create_directory_or_die("/scratch/cqs/shengq1/test/20161013_liuqi_gene_panel"),
+  target_dir => create_directory_or_die("/scratch/cqs/shengq2/test/20161013_liuqi_gene_panel"),
   email      => "quanhu.sheng.1\@vanderbilt.edu",
-  cqstools   => "/home/shengq1/cqstools/cqstools.exe",
+  cqstools   => "/home/shengq2/cqstools/cqstools.exe",
 
   #mapping
   aligner   => "bwa",
-  bwa_fasta => "/scratch/cqs/shengq1/references/gatk/b37/bwa_index_0.7.12/human_g1k_v37.fasta",
+  bwa_fasta => "/scratch/cqs/shengq2/references/gatk/b37/bwa_index_0.7.12/human_g1k_v37.fasta",
 
   #call variants
   perform_gatk_callvariants   => 1,
   gatk_callvariants_vqsr_mode => 0,
-  gatk_jar                    => "/scratch/cqs/shengq1/local/bin/gatk/GenomeAnalysisTK.jar",
-  picard_jar                  => "/scratch/cqs/shengq1/local/bin/picard/picard.jar",
-  dbsnp                       => "/scratch/cqs/shengq1/references/gatk/b37/dbsnp_150.b37.vcf",
-  hapmap                      => "/scratch/cqs/shengq1/references/gatk/b37/hapmap_3.3.b37.vcf",
-  omni                        => "/scratch/cqs/shengq1/references/gatk/b37/1000G_omni2.5.b37.vcf",
-  g1000                       => "/scratch/cqs/shengq1/references/gatk/b37/1000G_phase1.snps.high_confidence.b37.vcf",
-  mills                       => "/scratch/cqs/shengq1/references/gatk/b37/Mills_and_1000G_gold_standard.indels.b37.vcf",
+  gatk_jar                    => "/scratch/cqs/shengq2/local/bin/gatk/GenomeAnalysisTK.jar",
+  picard_jar                  => "/scratch/cqs/shengq2/local/bin/picard/picard.jar",
+  dbsnp                       => "/scratch/cqs/shengq2/references/gatk/b37/dbsnp_150.b37.vcf",
+  hapmap                      => "/scratch/cqs/shengq2/references/gatk/b37/hapmap_3.3.b37.vcf",
+  omni                        => "/scratch/cqs/shengq2/references/gatk/b37/1000G_omni2.5.b37.vcf",
+  g1000                       => "/scratch/cqs/shengq2/references/gatk/b37/1000G_phase1.snps.high_confidence.b37.vcf",
+  mills                       => "/scratch/cqs/shengq2/references/gatk/b37/Mills_and_1000G_gold_standard.indels.b37.vcf",
 
   #somatic mutation
   perform_muTect      => 1,
   muTect_init_command => "setpkgs -a java",
   muTect_option       => "--min_qscore 20 --filter_reads_with_N_cigar",
-  muTect_jar          => "/home/shengq1/local/bin/mutect-1.1.7.jar",
+  muTect_jar          => "/home/shengq2/local/bin/mutect-1.1.7.jar",
 
   #annotation
   annovar_param    => "-protocol refGene,avsnp147,cosmic70,exac03 -operation g,f,f,f",
-  annovar_db       => "/scratch/cqs/shengq1/references/annovar/humandb/",
+  annovar_db       => "/scratch/cqs/shengq2/references/annovar/humandb/",
   annovar_buildver => "hg19",
 
   files => {
