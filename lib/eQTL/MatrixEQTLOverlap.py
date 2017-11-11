77394dddfbbd1e325a980be88a8dd7f622cbb2b0
shengq2 <shengqh@gmail.com>
2017-11-11 11:49:00 -0600

    export based on pvalue

--- lib/eQTL/MatrixEQTLOverlap.py
+++ lib/eQTL/MatrixEQTLOverlap.py
@@ -5,7 +5,7 @@ import os
 import re
 import csv
 from PlinkUtils import PlinkSNPItem, readPlinkSNP
-from MatrixEQTLUtils import MatrixEQTLItem, readMatrixEQTLResult
+from MatrixEQTLUtils import MatrixEQTLItem, readMatrixEQTLResult, fillMatrixEQTL
 from GTExUtils import GTExItem, readGTExResult
 from os import listdir
 from os.path import isfile, join
@@ -21,40 +21,43 @@ def findOverlap(inputFile, inputBimFile, inputName, compareFile, compareBimFile,
   checkFileExist(compareFile, logger)
   checkFileExist(compareBimFile, logger)
   
-  inputMap = {p.Name:p for p in readPlinkSNP(inputBimFile) if p.Chromosome != "0"}
   inputResult = readMatrixEQTLResult(inputFile)
+  fillMatrixEQTL(inputResult, inputBimFile)
   logger.info("input eQTL = %d" % len(inputResult))
 
-  compareMap = {p.Name:p for p in readPlinkSNP(compareBimFile) if p.Chromosome != "0"}
   compareResult = readMatrixEQTLResult(compareFile)
+  fillMatrixEQTL(compareResult, compareBimFile)
   logger.info("compare eQTL = %d" % len(compareResult))
 
-  pvalues = [0.01, 0.001, 0.0001, 0.00001, 0.000001]
-  with open(outputFile + ".info", "w") as fi:
-    fi.write("pValue\t%s\t%s\tOverlap\tOverlapRate\tMatched\tConflicting_eQTL\teQTL_inconsistency\n" % (inputName, compareName))
-    for pvalue in pvalues:
-      logger.info("pvalue = %f" % pvalue)
-      matched = 0
-      unmatched = 0
-      curInputResult = [p for p in inputResult if p.Pvalue <= pvalue]
-      curCompareResultMap = {p.SNP + ":" + p.Gene:p for p in compareResult if p.Pvalue <= pvalue}
+  pvalues = [0.01, 0.001, 0.0001, 0.00001, 0.000001, -0.05]
+  tmpFile = outputFile + ".tmp"
+  with open(tmpFile, "w") as f:
+    f.write("Locus\tGene\t%s_Major\t%s_Minor\t%s_Pvalue\t%s_FDR\t%s_Beta\t%s_Major\t%s_Minor\t%s_Pvalue\t%s_FDR\t%s_Beta\tBetaMatch\n" % (inputName, inputName, inputName, inputName, inputName, compareName, compareName, compareName, compareName, compareName));
+    
+    with open(outputFile + ".info", "w") as fi:
+      fi.write("pValue\t%s\t%s\tOverlap\tOverlapRate\tMatched\tConflicting_eQTL\teQTL_inconsistency\n" % (inputName, compareName))
       
-      if pvalue == pvalues[0]:
-        tmpFile = outputFile + ".tmp"
-        f = open(tmpFile, "w")
-        f.write("Locus\tGene\t%s_Major\t%s_Minor\t%s_Pvalue\t%s_Beta\t%s_Major\t%s_Minor\t%s_Pvalue\t%s_Beta\tBetaMatch\n" %(inputName, inputName, inputName, inputName, compareName, compareName, compareName, compareName));
-        
-      try:
+      for pvalue in pvalues:
+        matched = 0
+        unmatched = 0
+        if pvalue < 0:
+          fdr = -pvalue 
+          logger.info("fdr = %f" % fdr)
+          curInputResult = [p for p in inputResult if p.FDR <= fdr]
+          curCompareResultMap = {p.SNP + ":" + p.Gene:p for p in compareResult if p.FDR <= fdr}
+        else:
+          logger.info("pvalue = %f" % pvalue)
+          curInputResult = [p for p in inputResult if p.Pvalue <= pvalue]
+          curCompareResultMap = {p.SNP + ":" + p.Gene:p for p in compareResult if p.Pvalue <= pvalue}
+      
         for i in curInputResult:
-          iAllele = inputMap[i.SNP];
           iKey = i.SNP + ":" + i.Gene;
           if iKey in curCompareResultMap:
             c = curCompareResultMap[iKey]
-            cAllele = compareMap[i.SNP]
             betaMatch = False;
-            if (iAllele.MajorAllele == cAllele.MajorAllele) and (iAllele.MinorAllele == cAllele.MinorAllele):
+            if (i.MajorAllele == c.MajorAllele) and (i.MinorAllele == c.MinorAllele):
               betaMatch = i.Beta * c.Beta > 0
-            elif (iAllele.MajorAllele == cAllele.MinorAllele) and (iAllele.MinorAllele == cAllele.MajorAllele):
+            elif (i.MajorAllele == c.MinorAllele) and (i.MinorAllele == c.MajorAllele):
               betaMatch = i.Beta * c.Beta < 0
             else:
               continue;
@@ -65,34 +68,43 @@ def findOverlap(inputFile, inputBimFile, inputName, compareFile, compareBimFile,
               unmatched = unmatched + 1
             
             if pvalue == pvalues[0]:
-              f.write("%s\t%s\t%s\t%s\t%f\t%f\t%s\t%s\t%f\t%f\t%r\n" % (
-                iAllele.Locus,
+              f.write("%s\t%s\t%s\t%s\t%.2E\t%.2E\t%f\t%s\t%s\t%.2E\t%.2E\t%f\t%r\n" % (
+                i.Locus,
                 i.Gene,
-                iAllele.MajorAllele,
-                iAllele.MinorAllele,
+                i.MajorAllele,
+                i.MinorAllele,
                 i.Pvalue,
+                i.FDR,
                 i.Beta,
-                cAllele.MajorAllele,
-                cAllele.MinorAllele,
+                c.MajorAllele,
+                c.MinorAllele,
                 c.Pvalue,
+                c.FDR,
                 c.Beta,
                 betaMatch));
-      finally:
-        if pvalue == pvalues[0]:
-          f.close()
-          if os.path.isfile(outputFile):
-            os.remove(outputFile)
-          os.rename(tmpFile, outputFile)
-      overlap = matched + unmatched
-      overlapRate = 1.0 * overlap / min(len(curInputResult), len(curCompareResultMap))
-      fi.write("%f\t%d\t%d\t%d\t%f\t%d\t%d\t%f\n" % (pvalue,
-                                                               len(curInputResult), 
-                                                               len(curCompareResultMap),
-                                                               overlap,
-                                                               overlapRate,
-                                                               matched,
-                                                               unmatched,
-                                                               (unmatched * 1.0 / (matched + unmatched))))
+          
+        overlap = matched + unmatched
+        minEqtl = min(len(curInputResult), len(curCompareResultMap))
+        
+        if minEqtl == 0:
+          fi.write("%f\t%d\t%d\t%d\t%f\t%d\t%d\t%f\n" % (pvalue, len(curInputResult), len(curCompareResultMap),
+                                                                   0,
+                                                                   0,
+                                                                   0,
+                                                                   0,
+                                                                   0))
+        else:
+          overlapRate = 1.0 * overlap / min(len(curInputResult), len(curCompareResultMap))
+          fi.write("%f\t%d\t%d\t%d\t%f\t%d\t%d\t%f\n" % (pvalue, len(curInputResult), len(curCompareResultMap),
+                                                                   overlap,
+                                                                   overlapRate,
+                                                                   matched,
+                                                                   unmatched,
+                                                                   (unmatched * 1.0 / (matched + unmatched))))
+  
+  if os.path.isfile(outputFile):
+    os.remove(outputFile)
+  os.rename(tmpFile, outputFile)
   
   logger.info("Done.")
         
@@ -125,7 +137,7 @@ def main():
   logger = logging.getLogger('MatrixEQTL_Overlap')
   logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)-8s - %(message)s')
   
-  findOverlap(args.input, args.input_bim_file, args.input_name, args.compare_file, args.compare_bim_file,args.compare_name, args.output, logger)
+  findOverlap(args.input, args.input_bim_file, args.input_name, args.compare_file, args.compare_bim_file, args.compare_name, args.output, logger)
   
 if __name__ == "__main__":
     main()
--- lib/eQTL/MatrixEQTLOverlapGTEx.py
+++ lib/eQTL/MatrixEQTLOverlapGTEx.py
@@ -4,8 +4,7 @@ import logging
 import os
 import re
 import csv
-from PlinkUtils import PlinkSNPItem, readPlinkSNP
-from MatrixEQTLUtils import MatrixEQTLItem, readMatrixEQTLResult
+from MatrixEQTLUtils import MatrixEQTLItem, readMatrixEQTLResult, fillMatrixEQTL
 from GTExUtils import GTExItem, readGTExResult
 from os import listdir
 from os.path import isfile, join
@@ -20,14 +19,6 @@ def findOverlap(inputFile, bimFile, gtexFile, outputFile, logger):
   checkFileExist(bimFile, logger)
   checkFileExist(gtexFile, logger)
 
-  pSNPs = readPlinkSNP(bimFile)
-  pSNPs = [p for p in pSNPs if p.Chromosome != "0"]
-  logger.info("SNP = %d" % len(pSNPs))
-
-  snpMap = {};
-  for t in pSNPs:
-    snpMap[t.Name] = t
-
   gtex = readGTExResult(gtexFile)
   gtex = [g for g in gtex if len(g.RefAllele) == 1 and len(g.AltAllele) == 1]
   logger.info("GTEx = %d" % len(gtex))
@@ -38,51 +29,83 @@ def findOverlap(inputFile, bimFile, gtexFile, outputFile, logger):
       gtexMap[g.Key] = g;
 
   mResult = readMatrixEQTLResult(inputFile)
+  fillMatrixEQTL(mResult, bimFile)
   logger.info("eQTL = %d" % len(mResult))
 
-  matched = 0
-  unmatched = 0
+  pvalues = [0.01, 0.001, 0.0001, 0.00001, 0.000001, -0.05]
   tmpFile = outputFile + ".tmp"
   with open(tmpFile, "w") as f:
-    f.write("Locus\tGene\tTCGA_Major\tTCGA_Minor\tTCGA_Beta\tGTex_Ref\tGTex_Alt\tGTex_Beta\tBetaMatch\n");
-    for b in mResult:
-      allele = snpMap[b.SNP];
-      key = allele.Locus + ":" + b.Gene;
-      if key in gtexMap:
-        gtexItem = gtexMap[key]
-        betaMatch = False;
-        if (allele.MajorAllele == gtexItem.RefAllele) and (allele.MinorAllele == gtexItem.AltAllele):
-          betaMatch = b.Beta * gtexItem.Beta > 0
-        elif (allele.MajorAllele == gtexItem.AltAllele) and (allele.MinorAllele == gtexItem.RefAllele):
-          betaMatch = b.Beta * gtexItem.Beta < 0
+    f.write("Locus\tGene\tTCGA_Major\tTCGA_Minor\tTCGA_Pvalue\tTCGA_FDR\tTCGA_Beta\tGTex_Ref\tGTex_Alt\tGTex_Pvalue\tGTex_FDR\tGTex_Beta\tBetaMatch\n");
+    with open(outputFile + ".info", "w") as fi:
+      fi.write("pValue\tMatrixEQTL\tGTEx\tOverlap\tOverlapRate\tMatched\tConflicting_eQTL\teQTL_inconsistency\n")
+      for pvalue in pvalues:
+        matched = 0
+        unmatched = 0
+        if pvalue < 0:
+          fdr = -pvalue 
+          logger.info("fdr = %f" % fdr)
+          curInputResult = [p for p in mResult if p.FDR <= fdr]
+          curCompareResultMap = {g:v for g,v in gtexMap.iteritems() if v.FDR <= fdr}
         else:
-          continue;
+          logger.info("pvalue = %f" % pvalue)
+          matched = 0
+          unmatched = 0
+          curInputResult = [p for p in mResult if p.Pvalue <= pvalue]
+          curCompareResultMap = {g:v for g,v in gtexMap.iteritems() if v.Pvalue <= pvalue}
+      
+        for b in curInputResult:
+          key = b.Locus + ":" + b.Gene;
+          if key in curCompareResultMap:
+            gtexItem = curCompareResultMap[key]
+            betaMatch = False;
+            if (b.MajorAllele == gtexItem.RefAllele) and (b.MinorAllele == gtexItem.AltAllele):
+              betaMatch = b.Beta * gtexItem.Beta > 0
+            elif (b.MajorAllele == gtexItem.AltAllele) and (b.MinorAllele == gtexItem.RefAllele):
+              betaMatch = b.Beta * gtexItem.Beta < 0
+            else:
+              continue;
+            
+            if betaMatch:
+              matched = matched + 1
+            else:
+              unmatched = unmatched + 1
+            
+            if pvalue == pvalues[0]: 
+              f.write("%s\t%s\t%s\t%s\t%f\t%.2E\t%s\t%s\t%f\t%.2E\t%r\n" %(
+                  b.Locus,
+                  b.Gene,
+                  b.MajorAllele,
+                  b.MinorAllele,
+                  b.Beta,
+                  b.Pvalue,
+                  gtexItem.RefAllele,
+                  gtexItem.AltAllele,
+                  gtexItem.Beta,
+                  gtexItem.Pvalue,
+                  betaMatch));
+                  
+        overlap = matched + unmatched
+        minEqtl = min(len(curInputResult), len(curCompareResultMap))
         
-        if betaMatch:
-          matched = matched + 1
+        if minEqtl == 0:
+          fi.write("%f\t%d\t%d\t%d\t%f\t%d\t%d\t%f\n" % (pvalue, len(curInputResult), len(curCompareResultMap),
+                                                                   0,
+                                                                   0,
+                                                                   0,
+                                                                   0,
+                                                                   0))
         else:
-          unmatched = unmatched + 1
-        
-        f.write("%s\t%s\t%s\t%s\t%f\t%s\t%s\t%f\t%r\n" %(
-            allele.Locus,
-            gtexItem.Gene,
-            allele.MajorAllele,
-            allele.MinorAllele,
-            b.Beta,
-            gtexItem.RefAllele,
-            gtexItem.AltAllele,
-            gtexItem.Beta,
-            betaMatch));
+          overlapRate = 1.0 * overlap / min(len(curInputResult), len(curCompareResultMap))
+          fi.write("%f\t%d\t%d\t%d\t%f\t%d\t%d\t%f\n" % (pvalue, len(curInputResult), len(curCompareResultMap),
+                                                                   overlap,
+                                                                   overlapRate,
+                                                                   matched,
+                                                                   unmatched,
+                                                                   (unmatched * 1.0 / (matched + unmatched))))
             
   if os.path.isfile(outputFile):
     os.remove(outputFile)
   os.rename(tmpFile, outputFile)
-  
-  with open(outputFile + ".info", "w") as f:
-    f.write("Type\tCount\n");
-    f.write("Matched\t%d\n" % matched)
-    f.write("Unmatched\t%d\n" % unmatched)
-    f.write("UnmatchedPercentage\t%4.2f%%\n" % (unmatched * 100.0 / (matched + unmatched)))
   
   logger.info("Done.")
         
@@ -101,10 +124,10 @@ def main():
   args = parser.parse_args()
   
   if DEBUG:
-    args.input = "/scratch/cqs/shengq2/guoyan/matrixEqtlNormal/result/normal_BRCA.cis.txt"
-    args.bim_file = "/scratch/cqs/shengq2/guoyan/bim/normal_BRCA_common_filtered.bim"
+    args.input = "/scratch/cqs/shengq2/guoyan/matrixEqtl_rnaTP_snpTP/result/rnaTP_snpTP_BRCA.cis.txt"
+    args.bim_file = "/scratch/cqs/shengq2/guoyan/dataPreparation_rnaTP_snpTP/result/BRCA/rnaTP_snpTP_BRCA_snp.bim"
     args.gtex_file = "/scratch/cqs/shengq2/references/GTEx/Breast_Mammary_Tissue_Analysis.snpgenes"
-    args.output = "/scratch/cqs/shengq2/guoyan/matrixEqtlNormal/result/normal_BRCA.cis.GTEx.tsv"
+    args.output = "/scratch/cqs/shengq2/guoyan/testOverlap.tsv"
   
   logger = logging.getLogger('MatrixEQTL_GTEx')
   logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)-8s - %(message)s')
