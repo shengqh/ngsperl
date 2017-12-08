f9f38b6fd71724ada95d87a898eaf8c27b29a023
shengq2 <shengqh@gmail.com>
2017-12-08 13:16:15 -0600

    make reference of feature same in different samples

--- lib/SmallRNA/smallRNAEndPosition.py
+++ lib/SmallRNA/smallRNAEndPosition.py
@@ -4,6 +4,7 @@ import os
 import logging
 import argparse
 import re
+import operator
 from Bio import SeqIO
 from Bio.Seq import Seq
 from CountXmlUtils import readCountXmlFeatures
@@ -14,8 +15,8 @@ DEBUG = 1
 
 if DEBUG:
   #inputFile="/scratch/cqs/shengq2/vickers/20170628_smallRNA_3018-KCV-77_78_79_mouse_v3/temp/temp.list"
-  inputFile="/scratch/cqs/shengq2/vickers/20170628_smallRNA_3018-KCV-77_78_79_mouse_v3/temp/smallRNA_1mm_KCV_3018_77_78_79.filelist"
-  outputFile="/scratch/cqs/shengq2/vickers/20170628_smallRNA_3018-KCV-77_78_79_mouse_v3/temp/temp.endpoint.txt"
+  inputFile = "/scratch/cqs/shengq2/vickers/20170628_smallRNA_3018-KCV-77_78_79_mouse_v3/temp/smallRNA_1mm_KCV_3018_77_78_79.filelist"
+  outputFile = "/scratch/cqs/shengq2/vickers/20170628_smallRNA_3018-KCV-77_78_79_mouse_v3/temp/temp.endpoint.txt"
 else:
   parser = argparse.ArgumentParser(description="Generate smallRNA NTA read for Fastq file.",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -36,8 +37,6 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 with open(inputFile, "r") as ir:
   files = [line.rstrip().split('\t') for line in ir]
 
-logger.info("Write result ...")
-
 fileFeatures = []
 for file in files:
   logger.info("Reading feature-query in %s ..." % file[1])
@@ -48,7 +47,7 @@ for features in fileFeatures:
   for mf in features[1]:
     for f in mf.Features:
       groupNames.add(f.Name.split(':')[0])
-groupNames=sorted(groupNames)
+groupNames = sorted(groupNames)
 
 groupFeatureMap = {}
 for gname in groupNames:
@@ -59,19 +58,25 @@ for gname in groupNames:
     gFeatures = [fg for fg in mappedFeatures if fg.Features[0].Name.startswith(gname)]
     for idx in range(0, min(10, len(gFeatures))):
       gf = gFeatures[idx].Features[0]
-      gfCount = sum(ep[1] for ep in gf.EndPoints)
       if gf.Name in gmap:
-        gmap[gf.Name] = gmap[gf.Name] + gfCount
+        oldEndPoints = gmap[gf.Name];
+        for ep in gf.EndPoints:
+          if ep[0] in oldEndPoints:
+            oldEndPoints[ep[0]] = oldEndPoints[ep[0]] + ep[1]
+          else:
+            oldEndPoints[ep[0]] = ep[1]
       else:
-        gmap[gf.Name] = gfCount
+        gmap[gf.Name] = {ep[0]:ep[1] for ep in gf.EndPoints}
 
 with open(outputFile, "w") as sw:
-  sw.write("Sample\tGroup\tFeature\tSampleRank\tOverallRank\tTotalCount\tEndpointCount\tEndpoint\tRelativeEndpoint\tPercentage\n")
+  sw.write("Sample\tGroup\tFeature\tSampleRank\tOverallRank\tTotalCount\tEndpoint\tEndpointCount\tRelativeEndpoint\tPercentage\n")
   for groupName in groupNames:
     gmap = groupFeatureMap[groupName]
+    gCountMap = {k: sum(v.values()) for k,v in gmap.iteritems()}
+    gMaxCountIndexMap = {k: max(v.iteritems(), key=operator.itemgetter(1))[0] for k,v in gmap.iteritems()}
     featureRankMap = {}
     overallrank = 0
-    for fname, fcount in sorted(gmap.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+    for fname, fcount in sorted(gCountMap.iteritems(), key=lambda (k, v): (v, k), reverse=True):
       overallrank = overallrank + 1
       featureRankMap[fname] = overallrank
     
@@ -91,9 +96,8 @@ with open(outputFile, "w") as sw:
         endpoints = gf.EndPoints
         positions = sorted(set(ep[0] for ep in endpoints))
         totalCount = sum(ep[1] for ep in endpoints)
-        maxCount=max(ep[1] for ep in endpoints)
-        maxPosition = [ep[0] for ep in endpoints if ep[1] == maxCount][0]
+        maxPosition = gMaxCountIndexMap[featureName]
         for position in positions:
           positionCount = sum(ep[1] for ep in endpoints if ep[0] == position)
-          sw.write("%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%.2f\n" %(sampleName, groupName, featureName, (idx+1),  groupRank, totalCount, positionCount, position, position - maxPosition, (positionCount * 1.0) / totalCount))  
+          sw.write("%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%.2f\n" % (sampleName, groupName, featureName, (idx + 1), groupRank, totalCount, position, positionCount, position - maxPosition, (positionCount * 1.0) / totalCount))  
 logger.info("Result has been saved to %s" % outputFile)
