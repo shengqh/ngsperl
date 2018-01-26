8817c92380d375fb3c3f3dbe3a396a88f5402293
shengq2 <shengqh@gmail.com>
2018-01-26 15:59:44 -0600

    build sample file

--- lib/eQTL/BirdseedTable.py
+++ lib/eQTL/BirdseedTable.py
@@ -19,57 +19,60 @@ def buildBirdseedTable(listFile, outputFile):
   bseeds = []
   print("writing %s ..." % outputFile)
   with open(outputFile, "w") as os:
-    bFirst=True
-    fileIndex = 0
-    for filename in filenames:
-      fileIndex = fileIndex + 1
-      print("reading %d/%d:%s ..." % (fileIndex, len(filenames), filename))
-      file = fileMap[filename].split(',')[0]
-      seeds = readBirdseed(file)
-      
-      if bFirst:
-        os.write("Sample")
+    with open("%s.samples" % outputFile, "w") as osSample:
+      bFirst=True
+      fileIndex = 0
+      for filename in filenames:
+        fileIndex = fileIndex + 1
+        print("reading %d/%d:%s ..." % (fileIndex, len(filenames), filename))
+        file = fileMap[filename].split(',')[0]
+        seeds = readBirdseed(file)
+        
+        if bFirst:
+          os.write("Sample")
+          for seed in seeds:
+            os.write("\t%s" % seed.SNP)
+          os.write("\n")
+          bFirst = False
+          bseeds = seeds
+          bseedsIndecies = set()
+          for idx in range(0, len(bseeds)):
+            item = bseeds[idx]
+            if isBaseValid(item.BaseA) and isBaseValid(item.BaseB):
+              continue
+            else:
+              bseedsIndecies.add(idx)
+        
+        os.write(filename)
+        osSample.write("%s\n" % filename)
+        
         for seed in seeds:
-          os.write("\t%s" % seed.SNP)
+          os.write("\t%s" % ("%0.2f" % math.log(seed.RatioBA, 2)))
         os.write("\n")
-        bFirst = False
-        bseeds = seeds
-        bseedsIndecies = set()
-        for idx in range(0, len(bseeds)):
-          item = bseeds[idx]
-          if isBaseValid(item.BaseA) and isBaseValid(item.BaseB):
-            continue
-          else:
-            bseedsIndecies.add(idx)
+        
+        if bseeds == seeds:
+          continue
+        
+        keys = [idx for idx in bseedsIndecies]
+        for idx in keys:
+          item = seeds[idx]
+          bitem = bseeds[idx]
+          if isBaseValid(item.BaseA):
+            bitem.BaseA = item.BaseA
+          if isBaseValid(item.BaseB):
+            bitem.BaseB = item.BaseB
+          if isBaseValid(bitem.BaseA) and isBaseValid(bitem.BaseB):
+            bseedsIndecies.remove(idx)
       
-      os.write(filename)
-      for seed in seeds:
-        os.write("\t%s" % ("%0.2f" % math.log(seed.RatioBA, 2)))
-      os.write("\n")
+      os.write('BaseA')
+      for item in bseeds:
+        os.write("\t%s" % item.BaseA)
+      os.write('\n')
       
-      if bseeds == seeds:
-        continue
-      
-      keys = [idx for idx in bseedsIndecies]
-      for idx in keys:
-        item = seeds[idx]
-        bitem = bseeds[idx]
-        if isBaseValid(item.BaseA):
-          bitem.BaseA = item.BaseA
-        if isBaseValid(item.BaseB):
-          bitem.BaseB = item.BaseB
-        if isBaseValid(bitem.BaseA) and isBaseValid(bitem.BaseB):
-          bseedsIndecies.remove(idx)
-    
-    os.write('BaseA')
-    for item in bseeds:
-      os.write("\t%s" % item.BaseA)
-    os.write('\n')
-    
-    os.write('BaseB')
-    for item in bseeds:
-      os.write("\t%s" % item.BaseB)
-    os.write('\n')
+      os.write('BaseB')
+      for item in bseeds:
+        os.write("\t%s" % item.BaseB)
+      os.write('\n')
        
 def main():
   parser = argparse.ArgumentParser(description="build birdseed table",
@@ -84,8 +87,8 @@ def main():
   args = parser.parse_args()
 
   if DEBUG:
-    args.inputFile = "/scratch/cqs/shengq2/temp/brca.filelist"
-    args.outputFile = "/scratch/cqs/shengq2/temp/brca_intensity.tsv"
+    args.inputFile = "/scratch/cqs/shengq2/guoyan/20180125_TCGA_eqtl_ratio/brca.filelist"
+    args.outputFile = "/scratch/cqs/shengq2/guoyan/20180125_TCGA_eqtl_ratio/brca_intensity.tsv"
 
   print(args)
   
