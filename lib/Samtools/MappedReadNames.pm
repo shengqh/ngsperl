ddbbd020f49408a67861687e85727743a411b702
shengq2 <shengqh@gmail.com>
2018-04-11 23:44:48 -0500

    all mapped read names

---
+++ lib/Samtools/MappedReadNames.pm
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+package Samtools::MappedReadNames;
+
+use strict;
+use warnings;
+use File::Basename;
+use CQS::PBS;
+use CQS::ConfigUtils;
+use CQS::SystemUtils;
+use CQS::FileUtils;
+use CQS::Task;
+use CQS::NGSCommon;
+use CQS::StringUtils;
+
+our @ISA = qw(CQS::Task);
+
+sub new {
+  my ($class) = @_;
+  my $self = $class->SUPER::new();
+  $self->{_name}   = __PACKAGE__;
+  $self->{_suffix} = "_mrn";
+  bless $self, $class;
+  return $self;
+}
+
+sub perform {
+  my ( $self, $config, $section ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct, $cluster ) = get_parameter( $config, $section );
+
+  my %raw_files = %{ get_raw_files( $config, $section ) };
+
+  my $shfile = $self->get_task_filename( $pbs_dir, $task_name );
+  open( my $sh, ">$shfile" ) or die "Cannot create $shfile";
+  print $sh get_run_command($sh_direct) . "\n";
+
+  for my $sample_name ( sort keys %raw_files ) {
+    my @sample_files = @{ $raw_files{$sample_name} };
+
+    my $pbs_file = $self->get_pbs_filename( $pbs_dir, $sample_name );
+    my $pbs_name = basename($pbs_file);
+    my $log      = $self->get_log_filename( $log_dir, $sample_name );
+
+    print $sh "\$MYCMD ./$pbs_name \n";
+
+    my $log_desc = $cluster->get_log_description($log);
+
+    my $bam_file = $sample_files[0];
+    my $nameFile = $sample_name . ".mnames";
+
+    my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $nameFile );
+    print $pbs "samtools view -F 4 $bam_file | cut -f1 > $nameFile
+
+if [ -s ${bam_file}.max.txt ]; then
+  grep '\@' ${bam_file}.max.txt >> $nameFile
+fi    
+";
+    $self->close_pbs( $pbs, $pbs_file );
+  }
+
+  close $sh;
+
+  if ( is_linux() ) {
+    chmod 0755, $shfile;
+  }
+
+  print "!!!shell file $shfile created, you can run this shell file to submit all " . $self->{_name} . " tasks.\n";
+}
+
+sub result {
+  my ( $self, $config, $section, $pattern ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct ) = get_parameter( $config, $section, 0 );
+
+  my %raw_files = %{ get_raw_files( $config, $section ) };
+
+  my $result = {};
+  for my $sample_name ( sort keys %raw_files ) {
+    my $nameFile = $result_dir . "/" . $sample_name . ".mnames";
+
+    my @result_files = ();
+    push( @result_files, $nameFile );
+    $result->{$sample_name} = filter_array( \@result_files, $pattern );
+  }
+
+  return $result;
+}
+
+1;
