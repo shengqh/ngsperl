c22b2a7e6f35fb3f3b9bbb5c7f846361a19ffbbe
shengq2 <shengqh@gmail.com>
2018-12-07 12:25:46 -0600

    test get_pair_group_sample_map

--- test/CQS/TestConfigUtils.pl
+++ test/CQS/TestConfigUtils.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use Test::More tests => 3;
+use Test::More tests => 4;
 use CQS::ConfigUtils;
 
 my $def = {
@@ -39,6 +39,36 @@ is_deeply(
   {
     ".order" => [ "C",  "B",  "A" ],
     ".col"   => [ "CC", "BB", "AA" ]
+  }
+);
+
+my $def2 = {
+    groups => {
+      "g1" => [ "g1s1", "g1s2" ],
+      "g2" => [ "g2s1", "g2s2" ],
+      "g3" => [ "g3s1", "g3s2" ],
+    },
+    correlation_groups => {
+      "g1g2" => {
+        groups => [ "g1", "g2" ],
+        cov    => [ 1,    2, 1, 2 ],
+      },
+      "g1g3" => [ "g1", "g3" ],
+    },
+};
+
+my $map = get_pair_group_sample_map( $def2->{correlation_groups}, $def2->{groups} );
+is_deeply(
+  $map,
+  {
+    "g1g2" => {
+      "g1" => [ "g1s1", "g1s2" ],
+      "g2" => [ "g2s1", "g2s2" ],
+    },
+    "g1g3" => {
+      "g1" => [ "g1s1", "g1s2" ],
+      "g3" => [ "g3s1", "g3s2" ],
+    },
   }
 );
 
