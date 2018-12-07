6dd958834906d93afcc8d994800b781c88f749c6
Quanhu Sheng <shengqh@gmail.com>
2018-12-07 16:19:52 -0600

    filter vcf by allele frequency

--- lib/Annotation/filterVcf.py
+++ lib/Annotation/filterVcf.py
@@ -25,41 +25,66 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 
 
 with open(args.output, "w") as fout:
-  with open(args.input, "r") as fin:
-    while True:
-      line = fin.readline()
-      if "#CHROM" in line:
-        fout.write(line)
-        vcfheaders = line.rstrip().split("\t")
-        format_index = vcfheaders.index("FORMAT")
-        sample_index = format_index + 1
-        AD_index = -1
-        break
-      else:
-        fout.write(line)
+  with open(args.output + ".discard", "w") as fdiscard:
+    with open(args.input, "r") as fin:
+      while True:
+        line = fin.readline()
+        if "#CHROM" in line:
+          fout.write(line)
+          vcfheaders = line.rstrip().split("\t")
+          format_index = vcfheaders.index("FORMAT")
+          sample_index = format_index + 1
+          AD_index = -1
+          break
+        else:
+          fout.write(line)
+      
+      totalsnv = 0
+      svnzero = 0
+      for line in fin:
+        snv = line.split('\t')
         
-    for line in fin:
-      snv = line.split('\t')
-      gt1count = 0
-      gt1lessCount = 0
-      if AD_index == -1:
-        AD_index = snv[format_index].split(":").index("AD")
-      
-      for si in range(sample_index, len(vcfheaders)):
-        sampleData = snv[si]
-        if "0/1:" in sampleData:
-          gt1count = gt1count + 1
+        totalsnv = totalsnv + 1
+        
+        gt1count = 0
+        gt1lessCount = 0
+        if AD_index == -1:
+          AD_index = snv[format_index].split(":").index("AD")
+        
+        haszero = False
+        for si in range(sample_index, len(vcfheaders)):
+          sampleData = snv[si]
+          if "0/0:" in sampleData:
+            continue
           
-          parts = sampleData.split(":")
-          adList = parts[AD_index].split(",")
-          ad0 = float(adList[0])
-          ad1 = float(adList[1])
+          if "0/" in sampleData:
+            gt1count = gt1count + 1
+            
+            parts = sampleData.split(":")
+            gts = parts[0].split("/")
+            gt = int(gts[1])
+            
+            adList = parts[AD_index].split(",")
+            ad0 = float(adList[0])
+            ad1 = float(adList[gt])
+            
+            if ad0 + ad1 == 0:
+              haszero = True
+              gt1lessCount = gt1lessCount + 1
+            elif(ad1  / (ad0 + ad1) < args.frequency):
+              gt1lessCount = gt1lessCount + 1
+        
+        if haszero:
+          print("haszero: gt1count %d ~ failedCount %d : %s" %(gt1count, gt1lessCount, line.rstrip()))
+          svnzero = svnzero + 1
           
-          if(ad1  / (ad0 + ad1) < args.frequency):
-            gt1lessCount = gt1lessCount + 1
-      
-      gt1lessPercentage = gt1lessCount * 1.0 / gt1count
-      if gt1lessPercentage < args.percentage:
-        fout.write(line)
-      else:
-        print("Discard: " + line)
+        if gt1count == 0:
+          fout.write(line)
+          continue
+          
+        gt1lessPercentage = gt1lessCount * 1.0 / gt1count
+        if gt1lessPercentage < args.percentage:
+          fout.write(line)
+        else:
+          fdiscard.write("gt1count %d ~ failedCount %d\t%s" %(gt1count, gt1lessCount, line.rstrip()))
+      print("haszero: %d out of %d" % (svnzero, totalsnv))
