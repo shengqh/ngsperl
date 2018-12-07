d1624927522376d205bf892f2941467b6c5a4f5c
Quanhu Sheng <shengqh@gmail.com>
2018-12-07 16:19:35 -0600

    updated summary

--- lib/LSAM/Summary.r
+++ lib/LSAM/Summary.r
@@ -12,4 +12,24 @@ ctrFile<-parFile2
 load(ctrFile)
 options("stringsAsFactors"=FALSE)
 out1=OutputSummary(ParentDataFolder,ctr.opo)
+
 GenerateTables(out1,csv=T,tablename=tablename)
+
+getci=function(x)
+{
+  x=as.numeric(x)[2:11]
+  paste0(round(mean(x),1)," (",round(mean(x)-1.96*sd(x),1),", ",round(mean(x)+1.96*sd(x),1),")")
+}
+
+T2<-NULL
+for (idx in c(1:length(out1))){
+  T2.idx=data.frame(LocId=out1[[idx]]$Tx.Total.DSA$Table.Tx.DSA$PatLocId,CI=apply(out1[[idx]]$Tx.Total.DSA$Table.Tx.DSA,1,getci))
+  colnames(T2.idx)<-c("LocId", attr(out1[[idx]], "simName"))
+  if(is.null(T2)){
+    T2<-T2.idx
+  }else{
+    T2<-merge(T2,T2.idx,by="LocId")
+  }
+}
+
+write.csv(T2,file=paste0(tablename, "_transplantTo.csv"))
