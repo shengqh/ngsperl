88e10381e934808cb8f0a16c2b6a4cb4747c9186
Quanhu Sheng <shengqh@gmail.com>
2019-04-04 15:18:47 -0500

    fastQCSummary

--- lib/QC/fastQCSummary.py
+++ lib/QC/fastQCSummary.py
@@ -30,11 +30,25 @@ parser = argparse.ArgumentParser(description="Summerize the fastqc result.",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('-i', '--input', action='store', nargs='?', help='Input fastqc data file list', required=True)
-parser.add_argument('-o', '--output', action='store', nargs='?', help="Output mismatch file name", required=False)
+parser.add_argument('-o', '--output', action='store', nargs='?', help="Output summary prefix", required=False)
 
 args = parser.parse_args()
 
-logger = initialize_logger(args.output + ".log", 'bamMismatch', False)
+logger = initialize_logger(args.output + ".log", 'fastQCSummary', False)
+
+logger.info("Output overall summary file.")
+with open(args.output + ".summary.txt", "w") as fout:
+  fout.write("Status\tCategory\tRawFile\tSample\n")
+  with open(args.input, "r") as flistin:
+    for line in flistin:
+      parts = line.split('\t')
+      datafile = parts[0]
+      datafolder, datafilename = os.path.split(datafile)
+      summaryfile = datafolder + "/summary.txt"
+      with open(summaryfile, "r") as fsummary:
+        for sline in fsummary:
+          fout.write(sline.rstrip() + "\t" + parts[1].rstrip() + "\n")
+    
 
   
 
