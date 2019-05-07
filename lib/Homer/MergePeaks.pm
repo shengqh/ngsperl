a9f7d3b45f8195457e12c7c19cb65c02ce5e2c5a
Quanhu Sheng <shengqh@gmail.com>
2019-05-07 11:27:58 -0500

    apply group

--- lib/Homer/MergePeaks.pm
+++ lib/Homer/MergePeaks.pm
@@ -4,6 +4,7 @@ package Homer::MergePeaks;
 use strict;
 use warnings;
 use File::Basename;
+use Data::Dumper;
 use CQS::PBS;
 use CQS::ConfigUtils;
 use CQS::SystemUtils;
@@ -28,24 +29,45 @@ sub perform {
 
   my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct, $cluster, $thread ) = get_parameter( $config, $section );
 
-  my %raw_files = %{ get_raw_files( $config, $section ) };
+  if ( defined $config->{$section}->{groups} ) {
+    my %raw_files = %{ get_grouped_raw_files( $config, $section, "groups" ) };
 
-  my $pbs_file   = $self->get_pbs_filename( $pbs_dir, $task_name );
-  my $pbs_name   = basename($pbs_file);
-  my $log        = $self->get_log_filename( $log_dir, $task_name );
-  my $log_desc   = $cluster->get_log_description($log);
-  my $final_file = "${task_name}.merged.peaks";
+    for my $sample_name ( sort keys %raw_files ) {
+      my $pbs_file   = $self->get_pbs_filename( $pbs_dir, $sample_name );
+      my $pbs_name   = basename($pbs_file);
+      my $log        = $self->get_log_filename( $log_dir, $sample_name );
+      my $log_desc   = $cluster->get_log_description($log);
+      my $final_file = "${sample_name}.merged.peaks";
 
-  my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $final_file );
-  print $pbs "mergePeaks -prefix ${task_name}.merged";
-  for my $sample_name ( sort keys %raw_files ) {
-    my @peak_files = @{ $raw_files{$sample_name} };
-    my $peak_file  = $peak_files[0];
-    print $pbs " \"$peak_file\"";
+      my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $final_file );
+      print $pbs "mergePeaks -prefix ${sample_name}.merged ";
+      my @peak_files = @{ $raw_files{$sample_name} };
+      for my $peak_file (@peak_files) {
+        print $pbs "\\\n  \"$peak_file\" \n";
+      }
+      $self->close_pbs( $pbs, $pbs_file );
+    }
   }
+  else {
+    my %raw_files = %{ get_raw_files( $config, $section ) };
 
-  print $pbs "\n";
-  $self->close_pbs( $pbs, $pbs_file );
+    my $pbs_file   = $self->get_pbs_filename( $pbs_dir, $task_name );
+    my $pbs_name   = basename($pbs_file);
+    my $log        = $self->get_log_filename( $log_dir, $task_name );
+    my $log_desc   = $cluster->get_log_description($log);
+    my $final_file = "${task_name}.merged.peaks";
+
+    my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $final_file );
+    print $pbs "mergePeaks -prefix ${task_name}.merged";
+    for my $sample_name ( sort keys %raw_files ) {
+      my @peak_files = @{ $raw_files{$sample_name} };
+      my $peak_file  = $peak_files[0];
+      print $pbs " \"$peak_file\"";
+    }
+
+    print $pbs "\n";
+    $self->close_pbs( $pbs, $pbs_file );
+  }
 }
 
 sub result {
