2b859ae9b8fe9300781393581949bd880d249d26
Quanhu Sheng <shengqh@gmail.com>
2019-07-12 10:35:46 -0500

    explain more short reads

--- lib/SmallRNA/explainShortReads.py
+++ lib/SmallRNA/explainShortReads.py
@@ -4,13 +4,26 @@ import logging
 import os
 import csv
 
+class ReadItem:
+  def __init__(self, sequence, totalCount):
+    self.Sequence = sequence
+    self.TotalCount = totalCount
+    self.SampleMap = {}
+
+class AnnotationItem:
+  def __init__(self, sequence, totalCount, category, counts):
+    self.Sequence = sequence
+    self.TotalCount = totalCount
+    self.Categories = [category]
+    self.Counts = counts
+
 def getValue(value):
-  return int(value[1][0])
+  return value.TotalCount
 
 def getFilename(value):
   return value[1]
 
-def match(logger, input, names, annotated, maxMapped, maxNumber, outputPrefix):
+def match(logger, input, names, annotated, maxMapped, maxNumber, minReadCount, minSampleCount, outputPrefix):
   logger.info("Reading short reads:" + input + " ...")
   shortReadMap = {}
   shortReadFiles = []
@@ -23,22 +36,36 @@ def match(logger, input, names, annotated, maxMapped, maxNumber, outputPrefix):
   
   shortFileList = sorted(shortFileList, key=getFilename)
   for parts in shortFileList:
-    shortReadFiles.append(parts[1])
-    logger.info("  Reading " + parts[0] + " ...")
-    with open(parts[0], 'r') as fin:
+    sampleFile = parts[0]
+    sample = parts[1]
+    shortReadFiles.append(sample)
+    logger.info("  Reading " + sampleFile + " ...")
+    with open(sampleFile, 'r') as fin:
       fin.readline()
       for line in fin:
         reads = line.rstrip().split('\t')
+        count = int(reads[1])
         seq = reads[2].rstrip()
         if not seq in shortReadMap:
-          shortReadMap[seq] = [int(reads[1]), {parts[1]:reads[1]}]
+          ri = ReadItem(seq, count)
+          shortReadMap[seq] = ri
         else:
-          curArray = shortReadMap[seq]
-          curArray[0] = curArray[0] + int(reads[1])
-          curArray[1][parts[1]] = reads[1]
+          ri = shortReadMap[seq]
+          ri.TotalCount += count
+        ri.SampleMap[sample] = count
   
-  shortReads = sorted(shortReadMap.items(), key=getValue, reverse=True)
-  shortReads = shortReads[0:maxNumber]
+  if minSampleCount > 1 or minReadCount > 1:
+    shortReads = []
+    for read in shortReadMap.values():
+      validSampleCount = len([v for v in read.SampleMap.values() if v >= minReadCount])
+      if validSampleCount >= minSampleCount:
+        shortReads.append(read)
+  else:
+    shortReads = shortReadMap.values()
+  shortReads = sorted(shortReads, key=getValue, reverse=True)
+  
+  if len(shortReads) > maxNumber:
+    shortReads = shortReads[0:maxNumber]
 
   logger.info("Reading max mapped reads:" + maxMapped + " ...")
   maxmappedReads = {}
@@ -69,18 +96,21 @@ def match(logger, input, names, annotated, maxMapped, maxNumber, outputPrefix):
       parts = row.split('\t')
       annofile = parts[0]
       iIndex = iIndex + 1
+      category = cnames[iIndex]
       
       logger.info("  Reading " + annofile + " ...")
       with open(annofile, 'r') as sr:
         annotatedFiles = sr.readline().rstrip().split('\t')[1:]
         for line in sr:
           parts = line.rstrip().split('\t')
-          if parts[0] not in annotatedReadMap:
-            annotatedReadMap[parts[0]] = [sum(int(p) for p in parts[1:]), [cnames[iIndex]],  parts[1:]]
+          seq = parts[0]
+          if seq not in annotatedReadMap:
+            totalCount = sum(int(p) for p in parts[1:])
+            annotatedReadMap[seq] = AnnotationItem(seq, totalCount, category, parts[1:])
           else:
-            annotatedReadMap[parts[0]][1].append(cnames[iIndex])
+            annotatedReadMap[seq].Categories.append(category)
             
-  annotatedReads = sorted(annotatedReadMap.items(), key=getValue, reverse=True)
+  annotatedReads = sorted(annotatedReadMap.values(), key=getValue, reverse=True)
   
   output = outputPrefix + ".tsv"
   logger.info("Writing explain result:" + output + " ...")
@@ -88,9 +118,9 @@ def match(logger, input, names, annotated, maxMapped, maxNumber, outputPrefix):
     sw.write("ShortRead\tShortReadCount\tShortReadLength\t" + "\t".join(["SRS_" + f for f in shortReadFiles]) + "\tIsMaxMapped\tParentRead\tParentReadCount\tParentReadCategory\t" + "\t".join(["PRS_" + f for f in annotatedFiles]) + "\n")
     emptyAnnotation = "\t\t\t\t" + "\t".join(["" for af in annotatedFiles]) + "\n"
     for shortRead in shortReads:
-      shortSeq = shortRead[0]
-      shortSeqCount = shortRead[1][0]
-      seqMap = shortRead[1][1]
+      shortSeq = shortRead.Sequence
+      shortSeqCount = shortRead.TotalCount
+      seqMap = shortRead.SampleMap
       sw.write("%s\t%s\t%d" % (shortSeq, shortSeqCount, len(shortSeq)))
       for fname in shortReadFiles:
         if fname in seqMap:
@@ -102,11 +132,10 @@ def match(logger, input, names, annotated, maxMapped, maxNumber, outputPrefix):
           
       bFound = False
       for annotatedRead in annotatedReads:
-        annoSeq = annotatedRead[0]
+        annoSeq = annotatedRead.Sequence
         if shortSeq in annoSeq:
           bFound = True
-          annoInfo = annotatedRead[1]
-          sw.write("\t%s\t%s\t%s\t%s\n" % (annoSeq, annoInfo[0], annoInfo[1][0], "\t".join(annoInfo[2])))
+          sw.write("\t%s\t%s\t%s\t%s\n" % (annoSeq, annotatedRead.TotalCount, "/".join(annotatedRead.Categories[0]), "\t".join(annotatedRead.Counts)))
           break
       
       if not bFound:
@@ -125,7 +154,9 @@ def main():
   parser.add_argument('-m', '--maxMapped', action='store', nargs='?', help='Input reads exceed maximum mapping to genome', required=NOT_DEBUG)
   parser.add_argument('-a', '--annotated', action='store', nargs='?', help='Input annotated reads', required=NOT_DEBUG)
   parser.add_argument('-n', '--names', action='store', nargs='?', help='Input annotated reads categories, split by ''', required=NOT_DEBUG)
-  parser.add_argument('--maxNumber', action='store', default=500, nargs='?', help='Input number of top short reads for annotation')
+  parser.add_argument('--maxNumber', action='store', default=100, nargs='?', help='Input number of top short reads for annotation')
+  parser.add_argument('--minReadCount', action='store', default=3, nargs='?', help='Input minimum copy of short reads in sample for annotation')
+  parser.add_argument('--minSampleCount', action='store', default=2, nargs='?', help='Input minimum number of sample with valid read count')
   parser.add_argument('-o', '--output', action='store', nargs='?', default="-", help="Output prefix of matched reads file", required=NOT_DEBUG)
   
   if NOT_DEBUG and len(sys.argv)==1:
@@ -140,12 +171,12 @@ def main():
     args.annotated = "T:/Shared/Labs/Vickers Lab/Tiger/projects/20180809_smallRNA_269_933_2002_human/data_visualization/short_reads_source/result/match__fileList3.list"
     args.names = "Host miRNA,Host tRNA,Host snRNA,Host snoRNA,Host rRNA,Host other small RNA,Host Genome,Microbiome Bacteria,Environment Bacteria,Fungus,Non host tRNA,Non host rRNA"
     #args.names = "Host miRNA,Host tRNA"
-    args.output = "T:/Shared/Labs/Vickers Lab/Tiger/projects/20180809_smallRNA_269_933_2002_human/data_visualization/short_reads_source/result/match"
+    args.output = "T:/Shared/Labs/Vickers Lab/Tiger/projects/20180809_smallRNA_269_933_2002_human/data_visualization/short_reads_source/result/match2"
   
   logger = logging.getLogger('explainShortReads')
   logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)-8s - %(message)s')
 
-  match(logger, args.input, args.names, args.annotated, args.maxMapped, args.maxNumber, args.output)
+  match(logger, args.input, args.names, args.annotated, args.maxMapped, args.maxNumber, args.minReadCount, args.minSampleCount, args.output)
   
 if __name__ == "__main__":
     main()
