e15fb88f8d23dcf7207a62b41e77c9480e6f258c
Quanhu Sheng <shengqh@gmail.com>
2019-08-27 14:04:31 -0500

    bugfix and add exon result

--- lib/Annotation/getGeneLocus.r
+++ lib/Annotation/getGeneLocus.r
@@ -49,11 +49,12 @@ if(!file.exists(genesStr)){
 
 ensembl <- useMart("ensembl", host=host, dataset=dataset)
 
-geneLocus<-getBM(attributes=c("chromosome_name", "start_position", "end_position", symbolKey, "strand"),
+geneLocus<-getBM(attributes=c("chromosome_name", "start_position", "end_position", symbolKey, "strand", "ensembl_gene_id"),
                  filters=symbolKey, values=genes, mart=ensembl, uniqueRows=TRUE)
 
 geneLocus$score<-1000
-geneLocus<-geneLocus[,c("chromosome_name", "start_position", "end_position", "score", symbolKey, "strand")]
+
+geneLocus<-geneLocus[,c("chromosome_name", "start_position", "end_position", "score", symbolKey, "strand", "ensembl_gene_id")]
 geneLocus<-geneLocus[order(geneLocus$chromosome_name, geneLocus$start_position),]
 
 if(shift != 0){
@@ -64,11 +65,20 @@ if(shift != 0){
 geneLocus$strand[geneLocus$strand == 1]<-"+"
 geneLocus$strand[geneLocus$strand == -1]<-"-"
 
-if(addChr & (!any(grepl("chr", geneLocus$chromosome_name))){
+if(addChr & (!any(grepl("chr", geneLocus$chromosome_name)))){
   geneLocus$chromosome_name = paste0("chr", geneLocus$chromosome_name)
 }
 
-write.table(geneLocus, file=paste0(outFile, ".bed"), row.names=F, col.names = F, sep="\t", quote=F)
+
+bedFile<-paste0(outFile, ".bed")
+write.table(geneLocus, file=bedFile, row.names=F, col.names = F, sep="\t", quote=F)
 
 missing<-genes[!(genes %in% geneLocus[,symbolKey])]
 writeLines(missing, paste0(outFile, ".missing"))
+
+exonLocus<-getBM(attributes=c("exon_chrom_start", "exon_chrom_end", "ensembl_gene_id"),
+                 filters="ensembl_gene_id", values=geneLocus$ensembl_gene_id, mart=ensembl, uniqueRows=TRUE)
+
+exonLocus<-exonLocus[,c("ensembl_gene_id", "exon_chrom_start", "exon_chrom_end")]
+
+write.table(exonLocus, file=paste0(bedFile, ".exon"), row.names=F, col.names = F, sep="\t", quote=F)
