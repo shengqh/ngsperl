dcf4adc310a3677a8bcbb194941a0c18982f9ae4
Quanhu Sheng <shengqh@gmail.com>
2019-09-13 15:03:29 -0500

    single cell rna pipeline init

---
+++ lib/CQS/RmdUtils.pm
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+package CQS::RmdUtils;
+
+use strict;
+use warnings;
+use File::Basename;
+use File::Copy;
+use String::Util ':all';
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [qw( build_rmd_file )] );
+
+our @EXPORT = ( @{ $EXPORT_TAGS{'all'} } );
+
+our $VERSION = '0.01';
+
+sub build_rmd_file {
+  my ($config, $section, $result_dir, $result_file) = @_;
+
+  my $rtemplate = dirname(__FILE__) . "/" . $config->{$section}{report_rmd_file};
+  my $rfile     = $result_dir . "/" . $result_file;
+
+  if ( defined $config->{$section}{function_r_files} ) {
+    my @function_r_files = split( ';', $config->{$section}{function_r_files} );
+
+    open( my $rmd, ">$rfile" )     or die "Cannot create $rfile";
+    open( my $rin, "<$rtemplate" ) or die "Cannot open $rtemplate";
+    my $function_done = 0;
+    while (<$rin>) {
+      my $rinline = $_;
+      $rinline = s/\r|\n//g;
+      if ( $rinline =~ /^```\{/ ) {
+        if ( !$function_done ) {
+          print $rmd '```{r, include=FALSE} \n';
+          for my $func_file (@function_r_files) {
+            $func_file = dirname(__FILE__) . "/" . trim($func_file);
+            open( my $fin, "<$func_file" ) or die "Cannot open $func_file";
+            while (<$fin>) {
+              my $finline = $_;
+              $finline = s/\r|\n//g;
+              print $rmd $finline . "\n";
+            }
+          }
+          print $rmd '``` \n';
+          $function_done = 1;
+        }
+      }
+    }
+  }
+  else {
+    copy( $rtemplate, $rfile ) or die "Copy failed: $!";
+  }
+
+  return($rfile);
+}
+
+1;
---
+++ lib/CQS/UniqueRmd.pm
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+package CQS::UniqueRmd;
+
+use strict;
+use warnings;
+use File::Basename;
+use File::Copy;
+use Data::Dumper;
+use String::Util ':all';
+use CQS::UniqueTask;
+use CQS::PBS;
+use CQS::SystemUtils;
+use CQS::FileUtils;
+use CQS::NGSCommon;
+use CQS::StringUtils;
+use CQS::ConfigUtils;
+use CQS::ClassFactory;
+use CQS::RmdUtils;
+
+our @ISA = qw(CQS::UniqueTask);
+
+sub new {
+  my ($class) = @_;
+  my $self = $class->SUPER::new();
+  $self->{_name}   = __PACKAGE__;
+  $self->{_suffix} = "_ur";
+  bless $self, $class;
+  return $self;
+}
+
+sub perform {
+  my ( $self, $config, $section ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct, $cluster ) = get_parameter( $config, $section );
+
+  my $final_pbs = $self->get_pbs_filename( $pbs_dir, $task_name );
+  my $final_log = $self->get_log_filename( $log_dir, $task_name );
+  my $final_log_desp = $cluster->get_log_description($final_log);
+
+  my $rtemplate = dirname(__FILE__) . "/" . $config->{$section}{report_rmd_file};
+  my $rfilename = "${task_name}.Rmd";
+  my $rfile     = build_rmd_file($config, $section, $result_dir, $rfilename);
+
+  my $removeEmpty = get_option( $config, $section, "remove_empty_parameter", 0 );
+  my $parametersample_files1 = writeParameterSampleFile( $config, $section, $result_dir, 1, $removeEmpty );
+  my $parametersample_files2 = writeParameterSampleFile( $config, $section, $result_dir, 2, $removeEmpty );
+  my $parametersample_files3 = writeParameterSampleFile( $config, $section, $result_dir, 3, $removeEmpty );
+  my $parametersample_files4 = writeParameterSampleFile( $config, $section, $result_dir, 4, $removeEmpty );
+
+  my $final_file = "${task_name}.html";
+  my $final = $self->open_pbs( $final_pbs, $pbs_desc, $final_log_desp, $path_file, $result_dir, $final_file );
+  print $final "
+R -e \"library(knitr);rmarkdown::render('$rfilename');\"
+";
+
+  $self->close_pbs( $final, $final_pbs );
+}
+
+sub result {
+  my ( $self, $config, $section, $pattern ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct ) = get_parameter( $config, $section, 0 );
+
+  my $result       = {};
+  my @result_files = ();
+  push( @result_files, "${result_dir}/${task_name}.html" );
+  $result->{$task_name} = filter_array( \@result_files, $pattern );
+  return $result;
+}
+
+1;
