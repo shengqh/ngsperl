a6a2fba5c5ea1a6b68ece503d062398e435c4aa5
Shilin Zhao <zhaoshilin@gmail.com>
2020-06-03 09:45:57 -0500

    mafReport bugfix

--- lib/Annotation/mafReport.r
+++ lib/Annotation/mafReport.r
@@ -1,13 +1,20 @@
 library(mafreport)
 
 mafFileList = parSampleFile1
-reportOutDir = "."
+#reportOutDir = "."
+reportOutDir = getwd()
 
 if(parFile1 != ''){
   clinicalData = parFile1
+  #need also define clinicalFeatures in rCode
 }else{
   clinicalData = NULL
   clinicalFeatures = NULL
+}
+
+
+if (!exists("genome")) {
+  genome="hg19"
 }
 
 interestedGenes = NULL
@@ -23,6 +30,7 @@ for(mafFile in mafFiles$V1){
   dataForReport1=initialize_maf_report_parameter(mafFile,reportOutDir=reportOutDir,
     vc_nonSyn=c("Frame_Shift_Del", "Frame_Shift_Ins", "Splice_Site", "Translation_Start_Site",
                 "Nonsense_Mutation",      "Nonstop_Mutation", "Missense_Mutation"),
+    genome=genome,
     interestedGenes=interestedGenes,
     clinicalData=clinicalData,
     clinicalFeatures=clinicalFeatures
