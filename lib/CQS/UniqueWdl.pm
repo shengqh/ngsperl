1cd3089455454e3c9806dbbaa86290ba50922da0
Quanhu Sheng <shengqh@gmail.com>
2020-06-26 13:46:36 -0500

    refine uniquewdl

--- lib/CQS/UniqueWdl.pm
+++ lib/CQS/UniqueWdl.pm
@@ -23,6 +23,143 @@ sub new {
   return $self;
 }
 
+sub get_input_value {
+  my ($config, $section, $input_hash, $input_key) = @_;
+  my $input_name = $input_key;
+  my $result = $input_hash->{$input_name};
+  
+  my $sample_names = $config->{$section}{sample_names};
+  my $is_key_ref = substr($input_key, -4) eq "_ref";
+  
+  if ($is_key_ref){
+    die "Key-ref $input_key should point to array!" if ref($result) ne "ARRAY";
+    $input_name =~ s/_config_ref$//g;
+    $input_name =~ s/_ref$//g;
+    $config->{$section}{$input_key} = $result;
+    $result = get_raw_files( $config, $section, $input_name );
+    
+    if (defined $sample_names){
+      my $oldresult = $result;
+      $result = {};
+      for my $sample_name (@$sample_names){
+        $result->{$sample_name} = $oldresult->{$sample_name};
+      }
+    }
+    
+    delete $config->{$section}{$input_key};
+  }
+
+  return ($input_name, $result, $is_key_ref);
+}
+
+sub prepare_wdl_parameters {
+  my ($self, $config, $section, $task_name, $result_dir, $replace_dics, $replace_values) = @_;
+  
+  my $input_parameters = get_option($config, $section, "input_parameters");
+  die "input_parameters should be hash in section $section!" if(ref($input_parameters) ne 'HASH');
+
+  for my $input_key (keys %$input_parameters){
+    my ($input_name, $input_value, $is_key_ref) = get_input_value($config, $section, $input_parameters, $input_key);
+    
+    if ($is_key_ref) {
+      $replace_values->{$input_name} = $input_value->{$task_name};
+    }else{
+      $replace_values->{$input_name} = $input_value;
+    }
+  }
+}
+
+sub prepare_wdl_array {
+  my ($self, $config, $section, $task_name, $result_dir, $replace_dics, $replace_values) = @_;
+  
+  my $input_array = get_option($config, $section, "input_array", {});
+  die "$input_array should be hash" if(ref($input_array) ne 'HASH');
+
+  for my $input_key (keys %$input_array){
+    my ($input_name, $input_value, $is_key_ref) = get_input_value($config, $section, $input_array, $input_key);
+
+    if ((not $is_key_ref) and (ref($input_value) ne 'HASH')) {
+      die "Key $input_key in input_array of $section should point to HASH!";
+    }
+    
+    my $res_array = [];
+    for my $sample_name (keys %$input_value){
+      my $sample_values = $input_value->{$sample_name};
+      die "value for $sample_name in input_list should be array" if (ref($sample_values) ne "ARRAY");
+      push @$res_array, @$sample_values;
+    }
+    
+    $replace_values->{$input_name} = $res_array;
+  }
+}
+
+sub prepare_wdl_list {
+  my ($self, $config, $section, $task_name, $result_dir, $replace_dics, $replace_values) = @_;
+  
+  my $input_list = get_option($config, $section, "input_list", {});
+  die "$input_list should be hash" if(ref($input_list) ne 'HASH');
+
+  for my $input_key (keys %$input_list){
+    my ($input_name, $input_value, $is_key_ref) = get_input_value($config, $section, $input_list, $input_key);
+
+    if ((not $is_key_ref) and (ref($input_value) ne 'HASH')) {
+      die "Key $input_key in input_list of $section should point to HASH!";
+    }
+    
+    my $list_file = $self->get_file( $result_dir, $task_name, "." . $input_key . ".list" );
+    open( my $list, ">$list_file" ) or die "Cannot create $list_file";
+    for my $sample_name (keys %$input_value){
+      my $sample_values = $input_value->{$sample_name};
+      die "value for $sample_name in input_list should be array" if (ref($sample_values) ne "ARRAY");
+      
+      for my $sample_value (@$sample_values) {
+        print $list $sample_value . "\n";
+      }
+    }
+    close($list);
+    
+    $replace_values->{$input_name} = $list_file;
+  }
+}
+
+sub prepare_wdl_single {
+  my ($self, $config, $section, $task_name, $result_dir, $replace_dics, $replace_values) = @_;
+  
+  my $input_single = get_option($config, $section, "input_single", {});
+  die "$input_single should be hash" if(ref($input_single) ne 'HASH');
+
+  for my $input_key (keys %$input_single){
+    my ($input_name, $input_value, $is_key_ref) = get_input_value($config, $section, $input_single, $input_key);
+
+    if ((not $is_key_ref) and (ref($input_value) ne 'HASH')) {
+      die "Key $input_key in input_array of $section should point to HASH!";
+    }
+    
+    if (not defined $input_value->{$task_name}){
+      die "Value for key $input_key in $section should be at task level.";
+    }
+    
+    $replace_values->{$input_name} = $input_value->{$task_name}[0];
+  }
+}
+
+sub prepare_wdl_values {
+  my ($self, $config, $section, $task_name, $result_dir) = @_;
+
+  my $replace_dics = {};
+  my $replace_values = {};
+  
+  $self->prepare_wdl_parameters($config, $section, $task_name, $replace_dics, $replace_values);
+  
+  $self->prepare_wdl_single($config, $section, $task_name, $replace_dics, $replace_values);
+
+  $self->prepare_wdl_list($config, $section, $task_name, $result_dir, $replace_dics, $replace_values);
+    
+  $self->prepare_wdl_array($config, $section, $task_name, $replace_dics, $replace_values);
+  
+  return($replace_dics, $replace_values);
+}
+
 sub perform {
   my ( $self, $config, $section ) = @_;
 
@@ -46,117 +183,7 @@ sub perform {
     #`ls -la`;
   }
 
-  my $raw_files = get_raw_files( $config, $section );
-  
-  my $input_parameters = get_option($config, $section, "input_parameters");
-  die "input_parameters should be hash" if(ref($input_parameters) ne 'HASH');
-
-  my $input_list = get_option($config, $section, "input_list", {});
-  die "$input_list should be hash" if(ref($input_list) ne 'HASH');
-
-  my $input_single = get_option($config, $section, "input_single", {});
-  die "$input_single should be hash" if(ref($input_single) ne 'HASH');
-
-  my $input_array = get_option($config, $section, "input_array", {});
-  die "$input_array should be hash" if(ref($input_array) ne 'HASH');
-
-  my $replace_dics = {};
-  my $replace_values = {};
-
-  for my $input_key (keys %$input_parameters){
-    my $input_value = $input_parameters->{$input_key};
-    if (ref($input_value) ne "ARRAY"){
-      $replace_values->{$input_key} = $input_value;
-      next;
-    }
-    
-    if (substr($input_key, -4) eq "_ref") {
-      my $input_name = $input_key;
-      $input_name =~ s/_config_ref$//g;
-      $input_name =~ s/_ref$//g;
-      $config->{$section}{$input_key} = $input_parameters->{$input_key};
-      $replace_dics->{$input_name} = get_raw_files( $config, $section, $input_name );
-      delete $config->{$section}{$input_key};
-    }else{
-      $replace_values->{$input_key} = $input_parameters->{$input_key};
-    }
-  }
-
-  for my $input_key (keys %$input_array){
-    my $input_name = $input_key;
-    my $input_value = $input_array->{$input_key};
-    if (ref($input_value) eq "ARRAY"){
-      die "$input_key should include _ref suffix" if (substr($input_key, -4) ne "_ref");
-      $input_name =~ s/_config_ref$//g;
-      $input_name =~ s/_ref$//g;
-      $config->{$section}{$input_key} = $input_array->{$input_key};
-      $input_value = get_raw_files( $config, $section, $input_name );
-      delete $config->{$section}{$input_key};
-    }
-
-    die "$input_key should point to hash" if (ref($input_value) ne "HASH");
-    
-    my $res_array = [];
-    for my $sample_name (keys %$input_value){
-      my $sample_values = $input_value->{$sample_name};
-      die "value for $sample_name in input_list should be array" if (ref($sample_values) ne "ARRAY");
-      push @$res_array, @$sample_values;
-    }
-    
-    $replace_dics->{$input_name} = $res_array;
-  }
-
-  for my $input_key (keys %$input_list){
-    my $input_name = $input_key;
-    my $input_value = $input_list->{$input_key};
-    if (ref($input_value) eq "ARRAY"){
-      die "$input_key should include _ref suffix" if (substr($input_key, -4) ne "_ref");
-      $input_name =~ s/_config_ref$//g;
-      $input_name =~ s/_ref$//g;
-      $config->{$section}{$input_key} = $input_parameters->{$input_key};
-      $input_value = get_raw_files( $config, $section, $input_name );
-      delete $config->{$section}{$input_key};
-    }
-
-    die "$input_key should point to hash" if (ref($input_value) ne "HASH");
-    
-    my $list_file = $self->get_file( $result_dir, $task_name, "." . $input_key . ".list" );
-    open( my $list, ">$list_file" ) or die "Cannot create $list_file";
-    for my $sample_name (keys %$input_value){
-      my $sample_values = $input_value->{$sample_name};
-      die "value for $sample_name in input_list should be array" if (ref($sample_values) ne "ARRAY");
-      
-      for my $sample_value (@$sample_values) {
-        print $list $sample_value . "\n";
-      }
-    }
-    close($list);
-    
-    $replace_dics->{$input_name} = $list_file;
-  }
-  
-  for my $input_key (keys %$input_single){
-    my $input_value = $input_single->{$input_key};
-    
-    if ((substr($input_key, -4) ne "_ref") and (ref($input_value) eq ref(""))){
-      $replace_values->{$input_key} = $input_value;
-      next;
-    }
-    die "value of $input_key in input_single should be ARRAY" if (ref($input_value) ne "ARRAY");
-    
-    if (substr($input_key, -4) eq "_ref") {
-      my $input_name = $input_key;
-      $input_name =~ s/_config_ref$//g;
-      $input_name =~ s/_ref$//g;
-      $config->{$section}{$input_key} = $input_parameters->{$input_key};
-      my $singles = get_raw_files( $config, $section, $input_name );
-      delete $config->{$section}{$input_key};
-      my $single_file = $singles->{$task_name}[0];
-      $replace_values->{$input_key} = $single_file;
-    }else{
-      $replace_values->{$input_key} = $input_single->{$input_key}[0];
-    }
-  }
+  my ($replace_dics, $replace_values) = $self->prepare_wdl_values($config, $section, $task_name, $result_dir);
   
   my $json_dic = read_json($input_json_file);
   for my $input_key (keys %$replace_dics){
@@ -176,6 +203,10 @@ sub perform {
   
   for my $input_key (keys %$replace_dics){
     $json_dic->{$input_key} = $replace_dics->{$input_key};
+  }
+
+  for my $input_key (keys %$replace_values){
+    $json_dic->{$input_key} = $replace_values->{$input_key};
   }
   
   my $task_input_file = $self->get_file( $result_dir, $task_name, ".inputs.json" );
