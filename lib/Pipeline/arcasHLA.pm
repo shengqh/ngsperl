164fd841c99a04e3bf2f8235355445c97d0bae8e
Quanhu Sheng <shengqh@gmail.com>
2020-08-18 15:13:19 -0500

    init arcasHLA genotype pipeline

---
+++ lib/Pipeline/arcasHLA.pm
@@ -0,0 +1,178 @@
+#!/usr/bin/perl
+package Pipeline::arcasHLA;
+
+use strict;
+use warnings;
+use List::Util qw(first);
+use File::Basename;
+use Storable qw(dclone);
+use CQS::FileUtils;
+use CQS::SystemUtils;
+use CQS::ConfigUtils;
+use CQS::ClassFactory;
+use Pipeline::PipelineUtils;
+use Pipeline::Preprocession;
+use Data::Dumper;
+use Hash::Merge qw( merge );
+
+require Exporter;
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [qw(performArcasHLA)] );
+
+our @EXPORT = ( @{ $EXPORT_TAGS{'all'} } );
+
+our $VERSION = '0.01';
+
+sub initializeDefaultOptions {
+  my $def = shift;
+
+  fix_task_name($def);
+
+  initDefaultValue( $def, "emailType", "FAIL" );
+  initDefaultValue( $def, "cluster",   "slurm" );
+  initDefaultValue( $def, "perform_preprocessing",   0 );
+
+  return $def;
+}
+
+sub getConfig {
+  my ($def) = @_;
+  $def->{VERSION} = $VERSION;
+
+  $def = initializeDefaultOptions($def);
+
+  my $task_name = $def->{task_name};
+
+  my $email = $def->{email};
+
+  my ( $config, $individual, $summary, $source_ref, $preprocessing_dir, $untrimed_ref, $cluster ) = getPreprocessionConfig($def);
+
+  my $target_dir      = $def->{target_dir};
+
+  $config->{"extract"} = {
+    class                 => "CQS::ProgramWrapperOneToOne",
+    perform               => 1,
+    target_dir            => "$target_dir/extract",
+    init_command          => "ln -s __FILE__ __NAME__.bam",
+    option                => "extract --log __NAME__.log --paired __NAME__.bam",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "arcasHLA",
+    source_ref            => "files",
+    source_arg            => "--paired",
+    source_join_delimiter => "",
+    output_to_same_folder => 0,
+    output_arg            => "-o",
+    output_to_folder      => 1,
+    output_file_prefix    => "",
+    output_file_ext       => ".extracted.1.fq.gz",
+    output_other_ext      => ".extracted.2.fq.gz",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=1",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+
+  $config->{"genotype"} = {
+    class                 => "CQS::ProgramWrapperOneToOne",
+    perform               => 1,
+    target_dir            => "$target_dir/genotype",
+    option                => "genotype --log __NAME__.log",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "arcasHLA",
+    source_ref            => "extract",
+    source_arg            => "",
+    source_join_delimiter => " ",
+    output_to_same_folder => 1,
+    output_to_folder      => 1,
+    output_arg            => "-o",
+    output_file_prefix    => "",
+    output_file_ext       => ".genotype.json",
+    output_other_ext       => ".genes.json,.alignment.p",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=1",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+
+  $config->{"merge"} = {
+    class                 => "CQS::ProgramWrapper",
+    perform               => 1,
+    target_dir            => "$target_dir/merge",
+    option                => "merge -i $target_dir/genotype/result --run $task_name -o .",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "arcasHLA",
+    source_ref            => "genotype",
+    source_arg            => "-i",
+    source_join_delimiter => " ",
+    output_to_same_folder => 1,
+    output_to_folder      => 1,
+    output_arg            => "-o",
+    output_file_prefix    => "",
+    output_file_ext       => ".genotype.txt",
+    sh_direct             => 1,
+    pbs                   => {
+      "nodes"     => "1:ppn=1",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+
+  $config->{sequencetask} = {
+    class      => getSequenceTaskClassname($cluster),
+    perform    => 1,
+    target_dir => "${target_dir}/sequencetask",
+    option     => "",
+    source     => {
+      step1 => ["extract", "genotype"],
+      step2 => ["merge"],
+    },
+    sh_direct => 0,
+    cluster   => $cluster,
+    pbs       => {
+      "email"     => $def->{email},
+      "emailType" => $def->{emailType},
+      "nodes"     => "1:ppn=" . $def->{max_thread},
+      "walltime"  => $def->{sequencetask_run_time},
+      "mem"       => "40gb"
+    },
+  };
+
+  return ($config);
+}
+
+sub performArcasHLA {
+  my ( $def, $perform ) = @_;
+  if ( !defined $perform ) {
+    $perform = 1;
+  }
+
+  my $config = getConfig($def);
+
+  if ($perform) {
+    saveConfig( $def, $config );
+
+    performConfig($config);
+  }
+
+  return $config;
+}
+
+sub performArcasHLATask {
+  my ( $def, $task ) = @_;
+
+  my $config = getConfig($def);
+
+  performTask( $config, $task );
+
+  return $config;
+}
+
+1;
