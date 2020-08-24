b385e00f5a7cc623f71e09a1c7cd738b61fc1639
Quanhu Sheng <shengqh@gmail.com>
2020-08-24 14:31:47 -0500

    reorder sam

---
+++ lib/Utils/ReorderSam.pm
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+package Picard::ReorderSam;
+
+use strict;
+use warnings;
+use File::Basename;
+use CQS::PBS;
+use CQS::ConfigUtils;
+use CQS::SystemUtils;
+use CQS::FileUtils;
+use CQS::Task;
+use CQS::NGSCommon;
+use CQS::StringUtils;
+
+our @ISA = qw(CQS::Task);
+
+sub new {
+  my ($class) = @_;
+  my $self = $class->SUPER::new();
+  $self->{_name}   = __PACKAGE__;
+  $self->{_suffix} = "_ro";
+  bless $self, $class;
+  return $self;
+}
+
+sub perform {
+  my ( $self, $config, $section ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct, $cluster ) = get_parameter( $config, $section );
+
+  my $faFile     = get_param_file( $config->{$section}{fasta_file}, "fasta_file", 1 );
+  my $picard_jar = get_param_file( $config->{$section}{picard_jar}, "picard_jar", 1, not $self->using_docker() );
+
+  my $shfile = $self->get_task_filename( $pbs_dir, $task_name );
+  open( my $sh, ">$shfile" ) or die "Cannot create $shfile";
+  print $sh get_run_command($sh_direct);
+
+  my %raw_files = %{ get_raw_files( $config, $section ) };
+
+  for my $sample_name ( sort keys %raw_files ) {
+    my @sample_files = @{ $raw_files{$sample_name} };
+    my $sampleFile   = $sample_files[0];
+
+    my $pbs_file = $self->get_pbs_filename( $pbs_dir, $sample_name );
+    my $pbs_name = basename($pbs_file);
+    my $log      = $self->get_log_filename( $log_dir, $sample_name );
+
+    my $cur_dir = create_directory_or_die( $result_dir . "/$sample_name" );
+
+    my $final_file = "${sample_name}.reordered.bam";
+
+    my $log_desc = $cluster->get_log_description($log);
+
+    my $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $cur_dir, $final_file );
+
+    print $pbs "
+java -jar $picard_jar ReorderSam I=${sampleFile} O=${final_file} R=${faFile}
+samtools index $final_file
+";
+
+    $self->close_pbs( $pbs, $pbs_file );
+
+    print $sh "\$MYCMD ./$pbs_name \n";
+  }
+  print $sh "exit 0\n";
+  close $sh;
+
+  if ( is_linux() ) {
+    chmod 0755, $shfile;
+  }
+
+  print "!!!shell file $shfile created, you can run this shell file to submit all tasks.\n";
+}
+
+sub result {
+  my ( $self, $config, $section, $pattern ) = @_;
+
+  my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct ) = get_parameter( $config, $section, 0 );
+
+  my %raw_files = %{ get_raw_files( $config, $section ) };
+  my $sort_by_query = get_option_value( $config->{$section}{sort_by_query}, 0 );
+
+  my $result = {};
+  for my $sample_name ( keys %raw_files ) {
+    my $final_file   = "${sample_name}.reordered.bam";
+    my @result_files = ();
+    push( @result_files, "${result_dir}/${sample_name}/${final_file}" );
+    $result->{$sample_name} = filter_array( \@result_files, $pattern );
+  }
+  return $result;
+}
+
+1;
