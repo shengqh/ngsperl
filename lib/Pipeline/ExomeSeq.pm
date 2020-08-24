aec38763f7869cd750daa1d16e46fcd128395e06
Quanhu Sheng <shengqh@gmail.com>
2020-08-24 14:28:35 -0500

    filterVcf back to unzipped result

--- lib/Pipeline/ExomeSeq.pm
+++ lib/Pipeline/ExomeSeq.pm
@@ -705,7 +705,7 @@ sub getConfig {
         parameterFile1_ref    => $filter_name,
         output_to_same_folder => 1,
         output_arg            => "-o",
-        output_file_ext       => ".maf_filtered.vcf.gz",
+        output_file_ext       => ".maf_filtered.vcf",
         sh_direct             => 1,
         pbs                   => {
           "email"     => $def->{email},
