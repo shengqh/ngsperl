18b7b1909d08e3caea90878647e8dcc6bca1a725
Quanhu Sheng <shengqh@gmail.com>
2021-01-05 16:49:15 -0600

    addArcasHLA

--- lib/scRNA/Modules.pm
+++ lib/scRNA/Modules.pm
@@ -7,7 +7,7 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-our %EXPORT_TAGS = ( 'all' => [qw(addEnclone addClonotypeMerge addEncloneToClonotype)] );
+our %EXPORT_TAGS = ( 'all' => [qw(addEnclone addClonotypeMerge addEncloneToClonotype addArcasHLA)] );
 
 our @EXPORT = ( @{ $EXPORT_TAGS{'all'} } );
 
@@ -101,6 +101,87 @@ sub addEncloneToClonotype {
     },
   };
   push @$tasks, $taskname;
+}
+
+sub addArcasHLA {
+  my ( $config, $def, $tasks, $target_dir, $task_name, $prefix, $source_ref ) = @_;
+
+  $config->{"${prefix}_arcasHLA_1_extract"} = {
+    class                 => "CQS::ProgramWrapperOneToOne",
+    perform               => 1,
+    target_dir            => "$target_dir/${prefix}_arcasHLA_1_extract",
+    init_command          => "ln -s __FILE__ __NAME__.bam",
+    option                => "extract -t 8 -v --log __NAME__.log --paired __NAME__.bam",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "arcasHLA",
+    source_ref            => $source_ref,
+    source_arg            => "--paired",
+    source_join_delimiter => "",
+    output_to_same_folder => 0,
+    output_arg            => "-o",
+    output_to_folder      => 1,
+    output_file_prefix    => "",
+    output_file_ext       => ".extracted.1.fq.gz",
+    output_other_ext      => ".extracted.2.fq.gz",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=8",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+
+  $config->{"${prefix}_arcasHLA_2_genotype"} = {
+    class                 => "CQS::ProgramWrapperOneToOne",
+    perform               => 1,
+    target_dir            => "$target_dir/${prefix}_arcasHLA_2_genotype",
+    option                => "genotype -t 8 -v --log __NAME__.log",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "arcasHLA",
+    source_ref            => "${prefix}_arcasHLA_1_extract",
+    source_arg            => "",
+    source_join_delimiter => " ",
+    output_to_same_folder => 1,
+    output_to_folder      => 1,
+    output_arg            => "-o",
+    output_file_prefix    => "",
+    output_file_ext       => ".genotype.json",
+    output_other_ext       => ".genes.json,.alignment.p",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=8",
+      "walltime"  => "24",
+      "mem"       => "40gb"
+    },
+  };
+
+  $config->{"${prefix}_arcasHLA_3_merge"} = {
+    class                 => "CQS::ProgramWrapper",
+    perform               => 1,
+    target_dir            => "$target_dir/${prefix}_arcasHLA_3_merge",
+    option                => "merge -i $target_dir/genotype/result --run $task_name -o .",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "arcasHLA",
+    source_ref            => "${prefix}_arcasHLA_2_genotype",
+    source_arg            => "-i",
+    source_join_delimiter => " ",
+    output_to_same_folder => 1,
+    output_to_folder      => 1,
+    output_arg            => "-o",
+    output_file_prefix    => "",
+    output_file_ext       => ".genotype.txt",
+    sh_direct             => 1,
+    pbs                   => {
+      "nodes"     => "1:ppn=1",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+
+  push (@$tasks, ("${prefix}_arcasHLA_1_extract", "${prefix}_arcasHLA_2_genotype", "${prefix}_arcasHLA_3_merge"));
 }
 
 1;
