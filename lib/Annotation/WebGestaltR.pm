6a03cb65cf6538134b0ae6a8c7ab6cfbf8c0289c
Quanhu Sheng <shengqh@gmail.com>
2021-02-09 17:29:45 -0600

    remove zip file

--- lib/Annotation/WebGestaltR.pm
+++ lib/Annotation/WebGestaltR.pm
@@ -82,7 +82,7 @@ if [[ ! -s $final_file || ! -d $final_file ]]; then
   if [[ -f $inputFile ]]; then
     if [[ -s $inputFile ]]; then
       R --vanilla -f $script --args $organism $sample_name $inputFile . $interestGeneType $referenceSet
-      rm */*.tar.gz
+      rm */*/*.zip
     else
       echo \"Empty gene file\" > ${sample_name}.empty
     fi 
