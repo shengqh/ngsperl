aa19249c3995b5becdd81cb7ec38e50c4d8ef3a0
Quanhu Sheng <shengqh@gmail.com>
2021-04-05 21:09:13 -0500

    draw GenomicFeatureEnrichment image

--- lib/QC/ChipseqQC.r
+++ lib/QC/ChipseqQC.r
@@ -6,15 +6,15 @@ args = commandArgs(trailingOnly = TRUE)
 library(ChIPQC)
 library(BiocParallel)
 
-#if(length(args) > 0){
+if(length(args) > 0){
   configFile=args[1]
   annotationName=args[2]
   chromosomes=args[3]
-#}else{
-#  configFile=r"(C:\projects\jonathan_brown\20210321_cutrun_6048_human\macs2callpeak_narrow_chipqc\result\cutrun_6048.config.txt)"
-#  annotationName="hg38"
-#  chromosomes="chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22"
-#}
+}else{
+  configFile=r"(C:\projects\jonathan_brown\20210321_cutrun_6048_human\macs2callpeak_narrow_chipqc\result\cutrun_6048.config.txt)"
+  annotationName="hg38"
+  chromosomes="chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22"
+}
 
 if (annotationName == "hg38") {
   library(TxDb.Hsapiens.UCSC.hg38.knownGene)
@@ -47,3 +47,12 @@ if (file.exists(rdatafile)){
 }
 
 ChIPQCreport(qcresult)
+
+if(file.exists("ChIPQCreport/GenomicFeatureEnrichment.png")){
+  samples=qcresult@Samples
+  height=min(2000, length(names(samples)) * 500 + 1000)
+  png("ChIPQCreport/GenomicFeatureEnrichment.png", width=3000, height=height, res=300)
+  g<-plotRegi(qcresult)
+  print(g)
+  dev.off()
+}
