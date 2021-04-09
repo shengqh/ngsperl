df019e9a30fe50a8687d3daaff8d36b93bac74e3
Quanhu Sheng <shengqh@gmail.com>
2021-04-09 16:44:34 -0500

    bugfix for dba upgrade

--- lib/Comparison/DiffBind.r
+++ lib/Comparison/DiffBind.r
@@ -78,9 +78,8 @@ for (idx in c(1:nrow(comparisons))){
 #  mb2<-dba.contrast(mb1,group1=group1, group2=group2, name1=group1name, name2=group2name, categories=c(DBA_CONDITION), minMembers=2)
   mb2<-dba.contrast(mb1,group1=group1, group2=group2, name1=group1name, name2=group2name)
 
- # mb2<-dba.analyze(mb2, bSubControl=FALSE, bFullLibrarySize=TRUE, bTagwise=FALSE, bCorPlot=FALSE)
  #all normalizing parameters in dba.normalize
-  mb2<-dba.analyze(mb2, bSubControl=FALSE, bFullLibrarySize=TRUE, bTagwise=FALSE, bCorPlot=FALSE)
+  mb2<-dba.analyze(mb2)
 #  res<-dba.report(mb2,bCounts=TRUE,th=1)
   res<-dba.report(mb2,bCounts=TRUE,bNormalized=TRUE,th=1)
   write.table(as.data.frame(res,row.names=NULL),file=paste0(compPrefix, ".tsv"),quote=F,sep="\t",row.names=F)
