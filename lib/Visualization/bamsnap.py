07218fc83fb6eba5bbc22f6e99f6e00e92092206
Quanhu Sheng <shengqh@gmail.com>
2021-04-15 20:01:51 -0500

    bamsnap

--- lib/Visualization/bamsnap.py
+++ lib/Visualization/bamsnap.py
@@ -4,7 +4,7 @@ import logging
 import os
 import subprocess
 
-def bamsnap(logger, bed_file, bam_list_file, output_file):
+def bamsnap(logger, bed_file, bam_list_file, output_file, discard_gene=True):
   gene_locus_map = {}
   with open(bed_file, "r") as ins:
     for line in ins:
@@ -25,12 +25,13 @@ def bamsnap(logger, bed_file, bam_list_file, output_file):
 
   height = max(len(bam_names) * 300 + 1100, 2000)
 
+  draw_gene_option = "" if discard_gene else "gene "
   gene_file_map={}
   for gene in sorted(gene_locus_map.keys()):
     locus = gene_locus_map[gene]
     logger.info("Drawing " + gene + " " + locus + " ...")
     pngfile = gene + ".png"
-    snapCommand = "bamsnap -draw coordinates bamplot gene -bamplot coverage -width 1000 -height %d -pos %s -out %s %s %s" % (height, locus, pngfile, bam_name_str, bam_file_str)
+    snapCommand = "bamsnap -draw coordinates bamplot " + draw_gene_option + " -bamplot coverage -width 1000 -height %d -pos %s -out %s %s %s" % (height, locus, pngfile, bam_name_str, bam_file_str)
     print(snapCommand)
     subprocess.run(snapCommand, check=True, shell=True)
     gene_file_map[gene] = pngfile
@@ -51,6 +52,7 @@ def main():
   parser.add_argument('-i', '--input', action='store', nargs='?', help='Input bed file', required=NOT_DEBUG)
   parser.add_argument('-o', '--output', action='store', nargs='?', help="Output list file", required=NOT_DEBUG)
   parser.add_argument('-b', '--bam', action='store', nargs='?', help="Bam list file", required=NOT_DEBUG)
+  parser.add_argument('-d', '--discard_gene', action='store_true', help="Discard gene for genome not hg38")
   
   args = parser.parse_args()
   
@@ -58,11 +60,12 @@ def main():
     args.input="/scratch/jbrown_lab/shengq2/projects/20210321_cutrun_6048_human/annotation_genes_locus/result/cutrun_6048.bed"
     args.bam="/scratch/jbrown_lab/shengq2/projects/20210321_cutrun_6048_human/annotation_genes_bamsnap/result/cutrun_6048__fileList1.list"
     args.output="/scratch/jbrown_lab/shengq2/projects/20210321_cutrun_6048_human/annotation_genes_bamsnap/result/cutrun_6048.csv"
+    args.discard_gene=False
   
   logger = logging.getLogger('bamsnap')
   logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)-8s - %(message)s')
   
-  bamsnap(logger, args.input, args.bam, args.output)
+  bamsnap(logger, args.input, args.bam, args.output, args.discard_gene)
   
 if __name__ == "__main__":
     main()
