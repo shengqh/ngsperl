bc53e31ab7636e43afafc25b4850c85f1e5f247e
Quanhu Sheng <shengqh@gmail.com>
2021-04-20 20:56:05 -0500

    uniform black color

--- lib/Visualization/plotGene.r
+++ lib/Visualization/plotGene.r
@@ -9,8 +9,6 @@ if(! exists("inputFile")){
     inputFile<-"linton_exomeseq_3321.position.txt.1000"
     outputPrefix<-'linton_exomeseq_3321.position'
     sizeFactorFile<-"/scratch/cqs/shengq2/macrae_linton/20190517_linton_exomeseq_3321_human/GATK4_CNV_Germline_08_SizeFactor/result/linton_exomeseq_3321.txt.sizefactor"
-    #inputFile<-'/scratch/cqs/shengq2/vickers/20190504_smallRNA_as_chipseq_GCF_000005845.2_ASM584v2/plotPeak/result/Control.position.txt'
-    #outputPrefix<-'/scratch/cqs/shengq2/vickers/20190504_smallRNA_as_chipseq_GCF_000005845.2_ASM584v2/plotPeak/result/Control.position'
   }else{
     inputFile<-args[1]
     outputPrefix<-args[2]
@@ -43,13 +41,13 @@ for (selectedFeature in unique(rawTable$Feature)) {
   dataForPlot$FileName<-gsub("_", " ", dataForPlot$File)
 
   hasCNV<-(("CNV" %in% colnames(dataForPlot)) & (length(unique(dataForPlot$CNV)) > 1))
-  
+
   if(hasCNV){
     dataForPlot$CNV[dataForPlot$CNV=="FALSE"] = "DEL"
     dataForPlot$CNV[dataForPlot$CNV=="NOREAD"] = "DEL"
     dataForPlot$CNV[dataForPlot$CNV==""] = "NoIndel"
   }
-  
+
   if(grepl("_\\d+_\\d+$", selectedFeature)){
     parts=unlist(strsplit(selectedFeature, "_"))
     featureName=paste0(paste0(parts[1:(length(parts)-2)]), ":", parts[length(parts)-1], "-", parts[length(parts)])
@@ -66,25 +64,27 @@ for (selectedFeature in unique(rawTable$Feature)) {
     #  next
     #}
     pdf(outputFile, height=max(4, length(unique(dataForPlot$FileName))), width=6, onefile=TRUE)
-    g <- ggplot(dataForPlot, aes_string("Position", column)) + 
+    g <- ggplot(dataForPlot, aes_string("Position", column)) +
       ylab(columns[column]) +
-      xlab(unique(dataForPlot$Locus)) +
-      geom_line()
+      xlab(unique(dataForPlot$Locus))+ 
+      geom_line() 
 
     if(featureName != unique(dataForPlot$Locus)){
       g <- g + ggtitle(featureName)
     }
-    
+
     if(hasCNV){
       g <- g + geom_area(aes(fill=CNV), alpha=.5)+
         scale_fill_manual(values=cols)
+    }else{
+      g <- g + geom_area(fill = "black")
     }
-    
+
     g <- g +
       facet_grid(FileName~.) +
       theme_bw() +
       theme(strip.background=element_blank())
-    
+
     print(g)
     dev.off()
   }
