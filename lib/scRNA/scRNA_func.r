35921f58c82c67db7ad82bfce47097fd101d8810
Quanhu Sheng <shengqh@gmail.com>
2021-04-20 20:54:21 -0500

    add find_all_markers

--- lib/scRNA/scRNA_func.r
+++ lib/scRNA/scRNA_func.r
@@ -37,6 +37,13 @@ find_markers<-function(object, by_sctransform, ident.1, ident.2, min.pct = 0.5, 
   return(markers)
 }
 
+find_all_markers<-function(object, by_sctransform, min.pct = 0.5, logfc.threshold = 0.6){
+  assay=ifelse(by_sctransform, "SCT", "RNA")
+  markers=FindAllMarkers(object, assay=assay, only.pos=TRUE, min.pct=min.pct, logfc.threshold=logfc.threshold)
+  markers=markers[markers$p_val_adj < 0.01,]
+  return(markers)
+}
+
 get_cluster_count<-function(counts, clusters){
   if(is.null(levels(clusters))){
     allClusters=unique(clusters)
@@ -67,4 +74,65 @@ get_group_count=function(curobj, groupName="active.ident") {
   names(clusters)=rownames(curgroups)
   result=get_cluster_count(counts, clusters)
   return(result)
+}
+
+run_cluster<-function(object, Remove_Mt_rRNA, rRNApattern, Mtpattern, pca_dims, by_sctransform, min.pct = 0.5, logfc.threshold = 0.6){
+  if (by_sctransform) {
+    object <- RunPCA(object = object, verbose=FALSE)
+  }else{
+    if (Remove_Mt_rRNA) {
+      rRNA.genes <- grep(pattern = rRNApattern,  rownames(object), value = TRUE)
+      Mt.genes<- grep (pattern= Mtpattern,rownames(object), value=TRUE )
+      var.genes <- dplyr::setdiff(VariableFeatures(object), c(rRNA.genes,Mt.genes))
+    } else {
+      var.genes <- VariableFeatures(object)
+    }
+    object <- RunPCA(object = object, features = var.genes, verbose=FALSE)
+  }
+  object <- RunUMAP(object = object, dims=pca_dims, verbose = FALSE)
+  object <- FindNeighbors(object = object, dims=pca_dims, verbose=FALSE)
+  object <- FindClusters(object=object, verbose=FALSE, random.seed=random.seed, resolution=resolution)
+
+  if (by_sctransform) {
+    markers <- FindAllMarkers(object, only.pos = TRUE, min.pct = min.pct, logfc.threshold = logfc.threshold)
+  }else{
+    markers <- FindAllMarkers(object, features=var.genes, only.pos = TRUE, min.pct = min.pct, logfc.threshold = logfc.threshold)
+  }
+  markers <- markers[markers$p_val_adj < 0.01,]
+  return(list(object=object, markers=markers))
+}
+
+ORA_celltype<-function(medianexp,cellType,weight){
+  ORA_result<-matrix(NA, nrow=length(cellType),ncol=dim(medianexp)[2])
+  CTA_result<-matrix(0,nrow=length(cellType),ncol=dim(medianexp)[2])
+  exp_z<-scale(medianexp)
+  genenames<-rownames(medianexp)   
+  for (j in 1: dim(medianexp)[2]){
+    clusterexp<-medianexp[,j] 
+    clusterexp_z<-exp_z[,j]
+    for (i in 1:length(cellType)){
+      
+      ct_exp<-length(intersect(genenames[clusterexp>0],cellType[[i]]))
+      ct_not_exp<-length(cellType[[i]])-ct_exp
+      exp_not_ct<-sum(clusterexp>0)-ct_exp
+      not_exp_not_ct<-length(clusterexp)-ct_not_exp 
+      cont.table<-matrix(c(ct_exp,ct_not_exp,exp_not_ct,not_exp_not_ct),nrow=2)
+      ORA_result[i,j]<-fisher.test(cont.table,alternative="greater")$p.value
+      ###
+      weight_ss<-weight[names(weight)%in%cellType[[i]]]
+      ind<-match(names(weight_ss),genenames)
+      exp_ss<-clusterexp_z[ind[!is.na(ind)]]
+      weight_ss<-weight_ss[!is.na(ind)]
+      CTA_result[i,j]<-sum(exp_ss*weight_ss)/(length(exp_ss)^0.3)
+    }
+  }
+  rownames(ORA_result)<-rownames(CTA_result)<-names(cellType)
+  minp_ora_ind<- apply(ORA_result,2,function(x){which.min(x)})
+  minp_ora<-apply(ORA_result,2,min)
+  names(minp_ora)<-rownames(ORA_result)[minp_ora_ind]
+  
+  max_cta_ind<- apply(CTA_result,2,function(x){which.max(x)})
+  max_cta<-apply(CTA_result,2,max,na.rm=T)
+  names(max_cta)<-rownames(CTA_result)[max_cta_ind]
+  return(list(ora=ORA_result,cta=CTA_result,min_ora=minp_ora,max_cta=max_cta))
 }
