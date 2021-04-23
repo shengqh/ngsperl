7df3c690d8e57484de1e6b564b706ab5cd36795d
Quanhu Sheng <shengqh@gmail.com>
2021-04-22 20:11:18 -0500

    call draw_chromosome_count

--- lib/Alignment/Bowtie2Summary.r
+++ lib/Alignment/Bowtie2Summary.r
@@ -5,48 +5,14 @@ library(reshape2)
 library(ggplot2)
 library(stringr)
 
-if (parSampleFile1 != ""){
-  filelist = read.table(parSampleFile1, sep="\t", header=F, stringsAsFactors = F)
-
-  final=NULL
-  i=1
-  for(i in c(1:nrow(filelist))){
-    filename = filelist$V2[i]
-    filelocation =filelist$V1[i]
-    subdata = read.table(filelocation, sep="\t", header=F, stringsAsFactors = F)
-    subdata=subdata[,c(1,3)]
-    colnames(subdata)<-c("Chrom", "Reads")
-    subdata=subdata[str_length(subdata$Chrom) < 6,]
-    subdata=subdata[subdata$Chrom != '*',]
-    subdata$Sample=filename
-    final=rbind(final, subdata )
-  }
-  write.csv(file=paste0(outFile, ".chromosome.csv"), final, row.names=F)
-
-  chroms=paste0("chr", c(1:22,'X','Y','M'))
-  if(!any(final$Chrom %in% chroms)){
-    chroms=paste0("", c(1:22,'X','Y','M'))
-  }
-  chroms=chroms[chroms %in% final$Chrom]
-  final$Chrom=factor(final$Chrom, levels=chroms)
-  final$NoRead=final$Reads==0
-
-  colors=c("black","red")
-  names(colors)=c("FALSE","TRUE")
-
-  height=max(1000, 100 * length(unique(final$Sample)))
-  png(file=paste0(outFile, ".chromosome.png"), height=height, width=3000, res=300)
-  g<-ggplot(final, aes(x=Chrom, y=Sample)) + 
-    geom_point(aes(size=Reads, color=NoRead)) + theme_classic() + 
-    scale_color_manual(values=colors) +
-    theme(axis.text.x = element_text(angle = 90, hjust=1,vjust=0),
-          axis.title = element_blank())
-  print(g)
-  dev.off()
-}
+#source("AlignmentUtils.r")
 
 if (parSampleFile2 != ""){
-  filelist = read.table(parSampleFile2, sep="\t", header=F, stringsAsFactors = F)
+  draw_chromosome_count(parSampleFile2, outFile)
+}
+
+if (parSampleFile1 != ""){
+  filelist = read.table(parSampleFile1, sep="\t", header=F, stringsAsFactors = F)
 
   final=NULL
   i=1
