0cb8085a346bacba039a16ca5a5bc5eb6f27d287
Quanhu Sheng <shengqh@gmail.com>
2021-04-28 12:44:38 -0500

    update signac.r output

--- lib/scRNA/Signac.r
+++ lib/scRNA/Signac.r
@@ -4,39 +4,34 @@ library(Seurat)
 finalList=readRDS(parFile1)
 obj=finalList$obj
 
-defaultAssay = DefaultAssay(obj)
-if(defaultAssay == "integrated"){
-  if("SCT" %in% names(obj@assays)){
-    DefaultAssay(obj)="SCT"
-  }else{
-    DefaultAssay(obj)="RNA"
+if(DefaultAssay(obj) == "integrated"){
+  if(nrow(obj@assays$integrated@counts) == 0){
+    if ("SCT" %in% names(obj@assays)){
+      obj@assays$integrated@counts = obj@assays$SCT@counts
+    }else{
+      obj@assays$integrated@counts = obj@assays$RNA@counts
+    }
   }
-  obj <- SCTransform(obj, verbose = FALSE)
-  obj <- RunPCA(obj, verbose = FALSE)
-  obj <- RunUMAP(obj, dims = 1:50, verbose = FALSE)
-  obj <- FindNeighbors(obj, dims = 1:50, verbose = FALSE)  
 }
 
 labels <- SignacFast(obj)
 celltypes_fast = GenerateLabels(labels, E = obj)
 
-DefaultAssay(obj)=defaultAssay
-
 obj <- AddMetaData(obj, metadata = celltypes_fast$CellStates, col.name = "CellStates")
-png(paste0(outFile, ".CellStates.fast.png"), width=3300, height=3000, res=300)
+png(paste0(outFile, ".signac.fast.png"), width=3300, height=3000, res=300)
 g1=DimPlot(obj, group.by = "CellStates", reduction="umap", label=T)
 print(g1)
 dev.off()
 
-saveRDS(celltypes_fast, file=paste0(outFile, ".CellStates.fast.rds"))
+saveRDS(celltypes_fast, file=paste0(outFile, ".signac.fast.rds"))
 
-# labels <- Signac(obj)
-# celltypes = GenerateLabels(labels, E = obj)
+labels <- Signac(obj)
+celltypes = GenerateLabels(labels, E = obj)
 
-# obj <- AddMetaData(obj, metadata = celltypes$CellStates, col.name = "CellStates")
-# png(paste0(outFile, ".CellStates.slow.png"), width=3300, height=3000, res=300)
-# g1=DimPlot(obj, group.by = "CellStates", reduction="umap", label=T)
-# print(g1)
-# dev.off()
+obj <- AddMetaData(obj, metadata = celltypes$CellStates, col.name = "CellStates")
+png(paste0(outFile, "..signac.png"), width=3300, height=3000, res=300)
+g1=DimPlot(obj, group.by = "CellStates", reduction="umap", label=T)
+print(g1)
+dev.off()
 
-# saveRDS(celltypes, file=paste0(outFile, ".CellStates.slow.rds"))
+saveRDS(celltypes, file=paste0(outFile, ".signac.rds"))
