bf2e7318498628766abae62791affbd6675507f1
Quanhu Sheng <shengqh@gmail.com>
2021-05-06 16:28:11 -0500

    _use_tmp_folder

--- lib/Alignment/Bowtie2.pm
+++ lib/Alignment/Bowtie2.pm
@@ -20,6 +20,7 @@ sub new {
   my $self = $class->SUPER::new();
   $self->{_name}   = __PACKAGE__;
   $self->{_suffix} = "_bt2";
+  $self->{_use_tmp_folder} = 1;
   $self->{log_result} = 1;
   bless $self, $class;
   return $self;
