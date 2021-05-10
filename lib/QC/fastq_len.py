f086899264d5d721bdaa871d3df4ab7c1c9715f8
Quanhu Sheng <shengqh@gmail.com>
2021-05-09 21:27:09 -0500

    can use local storage

--- lib/QC/fastq_len.py
+++ lib/QC/fastq_len.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import os
 import gzip
+from shutil import copyfile
 from asyncore import read
 
 parser = argparse.ArgumentParser(description="Get length distribution in FASTQ file.",
@@ -12,11 +13,13 @@ DEBUG=False
 NOT_DEBUG=not DEBUG
 
 parser.add_argument('-i', '--input', action='store', nargs='?', help='Input FASTQ files (use "," as seperation)', required=NOT_DEBUG)
+parser.add_argument('-c', '--copy_to_local', action='store_true', help="Copy file to local first")
 parser.add_argument('-o', '--output', action='store', nargs='?', default="-", help="Output file name")
 args = parser.parse_args()
 
 if DEBUG:
   args.input = "/scratch/stein_lab/20210429_qiong_rnaseq_6130_gut_hg38/cutadapt/result/S324_DC_RA_RNA_clipped.1.fastq.gz,/scratch/stein_lab/20210429_qiong_rnaseq_6130_gut_hg38/cutadapt/result/S324_DC_RA_RNA_clipped.2.fastq.gz"
+  args.copy_to_local = False
   args.output = "/scratch/stein_lab/20210429_qiong_rnaseq_6130_gut_hg38/cutadapt/test.len"
 
 logger = logging.getLogger('fastq_len')
@@ -32,13 +35,22 @@ result={}
 for fq in fastq_files:
   logger.info("Processing %s ..." % fq)
   reads_count = 0
-  fin = gzip.open(fq, "rt") if fq.endswith(".gz") else open(fq, "rt")
+  if args.copy_to_local:
+    openfq = os.path.basename(fq)
+    if openfq != fq:
+      logger.info("Copy %s to local ..." % fq)
+      copyfile(fq, openfq, follow_symlinks=True)
+  else:
+    openfq = fq
+  
+  logger.info("Processing %s ..." % openfq)
+  fin = gzip.open(openfq, "rt") if openfq.endswith(".gz") else open(openfq, "rt")
   with fin:
     while True:
       try:
         query = fin.readline()
         sequence = fin.readline().rstrip()
-        fin.readline()
+        midline = fin.readline()
         score = fin.readline().rstrip()
       except Exception as e:
         with open(error_file, "wt") as fout:
@@ -47,17 +59,23 @@ for fq in fastq_files:
 
       if not query:
         break
-      if not query.startswith("@"):
-        error_msg = "Read count=%d\nError query=%s\n" % (reads_count, query)
-        with open(error_file, "wt") as fout:
-          fout.write("%s\n" % error_msg)
-        raise Exception(error_msg)
 
       reads_count += 1
       if reads_count % 1000000 == 0:
         logger.info("%d reads processed." % reads_count)
         #break
 
+      if not query.startswith("@"):
+        error_msg = "Read count=%d\nError query=%s\n" % (reads_count, query)
+        with open(error_file, "wt") as fout:
+          fout.write("%s\n" % error_msg)
+        raise Exception(error_msg)
+
+      if not midline.startswith("+"):
+        error_msg = "Read count=%d\nError midline=%s\n" % (reads_count, midline)
+        with open(error_file, "wt") as fout:
+          fout.write("%s\n" % error_msg)
+        raise Exception(error_msg)
 
       seqlen = len(sequence)
       scorelen = len(score)
@@ -70,6 +88,10 @@ for fq in fastq_files:
 
       result[seqlen] = result.setdefault(seqlen, 0) + 1
 
+  if args.copy_to_local:
+    os.remove(openfq)
+
+logger.info("Output result to %s ..." % args.output)
 with open(args.output, "wt") as fout:
   fout.write("Len\tCount\n")
   maxlen = max(result.keys())
