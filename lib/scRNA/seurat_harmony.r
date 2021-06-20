27802ff03d5326b017d077b3a96a9dcf5e29e915
Quanhu Sheng <shengqh@gmail.com>
2021-06-20 12:47:44 -0500

    discard cluster in harmony

--- lib/scRNA/seurat_harmony.r
+++ lib/scRNA/seurat_harmony.r
@@ -1,4 +1,5 @@
-source('scRNA_func.r')
+
+source("scRNA_func.r")
 
 library(dplyr)
 library(Seurat)
@@ -40,8 +41,8 @@ species=myoptions$species
 nCount_sd_cutoff=0
 
 has_batch_file<-file.exists(parSampleFile2)
-
-pca_dims<-1:as.numeric(myoptions$pca_dims)
+npcs<-as.numeric(myoptions$pca_dims)
+pca_dims<-1:npcs
 
 finalList<-list()
 finalListFile<-paste0(prefix, ".final.rds")
@@ -71,45 +72,12 @@ finalList$filter<-list(nFeature_cutoff_min=nFeature_cutoff_min,
                       nCount_cutoff=nCount_cutoff,
                       nCount_sd_cutoff=nCount_sd_cutoff)
 
-obj=do_harmony(objs, by_sctransform, parSampleFile2)
+obj=do_harmony(objs, by_sctransform, Remove_Mt_rRNA, Mtpattern, rRNApattern, npcs, parSampleFile2)
 reduction="harmony"
 rm(objs)
 
-cat("run_cluster_only ... ")
-obj <- run_cluster_only(obj, Remove_Mt_rRNA, rRNApattern, Mtpattern, pca_dims, by_sctransform, reduction=reduction)
-
-cat("saving result ... ")
-seurat_clusters<-unlist(obj[["seurat_clusters"]])
-seurat_colors<-hue_pal()(length(levels(seurat_clusters)))
-names(seurat_colors)<-levels(seurat_clusters)
-
-finalList$seurat_colors<-seurat_colors
-
-counts=GetAssayData(obj,assay="RNA",slot="counts")
-clusters=obj@active.ident
-sumcounts=get_cluster_count(counts, clusters)
-logsumcounts<-log2(sumcounts+1)
-
-data.quantileAll<-apply(logsumcounts, 2, function(x){quantile(x, 0.75)})
-
-norm_method=""
-if(any(data.quantileAll == 0)){
-  norm_method = ".normByTotal"
-  data.all <- apply(logsumcounts, 2, sum)
-  data.all<-data.all / median(data.all)
-  data.norm <- t(t(logsumcounts) / data.all)
-}else{
-  norm_method = ".normByUpQuantile"
-  data.quantileAll<-data.quantileAll / median(data.quantileAll)
-  data.norm <- t(t(logsumcounts) / data.quantileAll)
-}
-
-write.csv(sumcounts, file=paste0(prefix, ".cluster.count.csv"))
-write.csv(data.norm, file=paste0(prefix, ".cluster", norm_method, ".csv"))
-
-clusters<-data.frame("cell" = c(1:length(obj$seurat_clusters)), "seurat_clusters"=as.numeric(as.character(obj$seurat_clusters)), stringsAsFactors = F)
-rownames(clusters)<-names(obj$seurat_clusters)
-write.csv(clusters, file=paste0(prefix, ".cluster.csv"))
+cat("run_umap ... ")
+obj <- RunUMAP(object = obj, reduction=reduction, dims=pca_dims, verbose = FALSE)
 
 finalList$obj<-obj
 saveRDS(finalList, file=finalListFile)
@@ -119,27 +87,23 @@ mt<-data.frame(UMAP_1=obj@reductions$umap@cell.embeddings[,1],
                Sample=obj$orig.ident,
                batch=obj$batch)
 
-if(has_batch_file){
-  mt$batch=obj$batch
-}
-
 nSamples = length(unique(obj$orig.ident))
 nWidth=ceiling(sqrt(nSamples))
 nHeight=ceiling(nSamples / nWidth)
 
 cat("draw pictures ... ")
-draw_dimplot(mt, paste0(outFile, ".cluster_sample.png"), "Sample")
-draw_dimplot(mt, paste0(outFile, ".cluster_batch.png"), "batch")
-
-p1<-DimPlot(object = obj, reduction = 'umap', label=TRUE, group.by="seurat_clusters", cols=seurat_colors)
-p2<-DimPlot(object = obj, reduction = 'umap', label=FALSE, group.by="orig.ident")
-p<-p1+p2
-width=3000
+draw_dimplot(mt, paste0(outFile, ".harmony_sample.png"), "Sample")
 if(has_batch_file){
-  p3<-DimPlot(object = obj, reduction = 'umap', label=FALSE, group.by="batch")
-  p<-p1+p3+p2
-  width=4500
+  draw_dimplot(mt, paste0(outFile, ".harmony_batch.png"), "batch")
 }
-png(paste0(outFile, ".cluster.png"), width=width, height=1200, res=300)
+
+p1<-DimPlot(object = obj, reduction = 'umap', label=FALSE, group.by="orig.ident")
+width=1500
+if(has_batch_file){
+  p2<-DimPlot(object = obj, reduction = 'umap', label=FALSE, group.by="batch")
+  p<-p1+p2
+  width=3000
+}
+png(paste0(outFile, ".harmony.png"), width=width, height=1200, res=300)
 print(p)
 dev.off()
