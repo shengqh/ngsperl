0457073945d160729fed8f3fe61f27f9d22fd660
Quanhu Sheng <shengqh@gmail.com>
2021-06-20 12:45:57 -0500

    gene localization figure

--- lib/scRNA/gene_localization_map.r
+++ lib/scRNA/gene_localization_map.r
@@ -1,7 +1,5 @@
-
-setwd('C:/projects/scratch/cqs/shengq2/alexander_gelbard_projects/20210611_NoAAC_iSGS_3364_3800_6363_scRNA/seurat_harmony_celltype_gene_localization_map/result')
-
 source("scRNA_func.r")
+
 library(Seurat)
 library(ggplot2)
 library(ggpubr)
@@ -19,8 +17,21 @@ ngroup=length(unique(groups_tbl$V2))
 genes_tbl<-read.table(parSampleFile1, sep="\t", stringsAsFactors = F)
 gene=genes_tbl$V1[1]
 for (gene in genes_tbl$V1){
+  gdata<-FetchData(object = obj, gene)
+  colnames(gdata)<-"Gene"
+  coords<-data.frame(obj@reductions$umap@cell.embeddings)
+  gdata<-cbind(coords, data.frame(group=obj$group), gdata)
+  gdata1<-subset(gdata, gdata$Gene == 0)
+  gdata2<-subset(gdata, gdata$Gene > 0)
+  
   png(filename=paste0(outFile, gene, ".feature_plot.png"), width= ngroup * 2000, height=2000, res=300)
-  g<-FeaturePlot(obj, gene, split.by = "group", pt.size=1)
+  g<-ggplot(gdata, aes(UMAP_1, UMAP_2)) + 
+    geom_point(data=gdata1, aes(col=Gene)) + 
+    geom_point(data=gdata2, aes(col=Gene)) + 
+    scale_colour_gradient(name=gene, low="grey", high="blue") + 
+    facet_grid(~group) + 
+    theme_bw() +
+    theme(strip.background=element_rect(fill="white"))
   print(g)
   dev.off()
 }
