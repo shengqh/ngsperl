776a9d1868443d1b97b6cb96e6f2beadcbc86801
Quanhu Sheng <shengqh@gmail.com>
2021-06-30 21:28:15 -0500

    merge data

--- lib/scRNA/seurat_merge.r
+++ lib/scRNA/seurat_merge.r
@@ -1,6 +1,4 @@
-
 source("scRNA_func.r")
-
 
 library(dplyr)
 library(Seurat)
@@ -36,7 +34,7 @@ species=myoptions$species
 pca_dims<-1:as.numeric(myoptions$pca_dims)
 
 finalList<-list()
-finalListFile<-paste0(prefix, ".preprocess.rds")
+finalListFile<-paste0(prefix, ".final.rds")
 
 rawobj<-readRDS(parFile1)
 
@@ -90,71 +88,27 @@ if(by_sctransform){
   assay="RNA"
 }
 
-obj <- run_cluster_only(obj, Remove_Mt_rRNA, rRNApattern, Mtpattern, pca_dims, by_sctransform, reduction="pca")
+cat("run_pca ... \n")
+obj <- RunPCA(object = obj, assay=assay, verbose=FALSE)
 
-seurat_clusters<-unlist(obj[["seurat_clusters"]])
-seurat_colors<-hue_pal()(length(levels(seurat_clusters)))
-names(seurat_colors)<-levels(seurat_clusters)
-
-finalList$seurat_colors<-seurat_colors
-
-counts=GetAssayData(obj,assay="RNA",slot="counts")
-clusters=obj@active.ident
-sumcounts=get_cluster_count(counts, clusters)
-logsumcounts<-log2(sumcounts+1)
-
-data.quantileAll<-apply(logsumcounts, 2, function(x){quantile(x, 0.75)})
-
-norm_method=""
-if(any(data.quantileAll == 0)){
-  norm_method = ".normByTotal"
-  data.all <- apply(logsumcounts, 2, sum)
-  data.all<-data.all / median(data.all)
-  data.norm <- t(t(logsumcounts) / data.all)
-}else{
-  norm_method = ".normByUpQuantile"
-  data.quantileAll<-data.quantileAll / median(data.quantileAll)
-  data.norm <- t(t(logsumcounts) / data.quantileAll)
-}
-
-write.csv(sumcounts, file=paste0(prefix, ".cluster.count.csv"))
-write.csv(data.norm, file=paste0(prefix, ".cluster", norm_method, ".csv"))
-
-clusters<-data.frame("cell" = c(1:length(obj$seurat_clusters)), "seurat_clusters"=as.numeric(as.character(obj$seurat_clusters)), stringsAsFactors = F)
-rownames(clusters)<-names(obj$seurat_clusters)
-write.csv(clusters, file=paste0(prefix, ".cluster.csv"))
+cat("run_umap ... \n")
+obj <- RunUMAP(object = obj, dims=pca_dims, verbose = FALSE)
 
 finalList$obj<-obj
 saveRDS(finalList, file=finalListFile)
 
-
 mt<-data.frame(UMAP_1=obj@reductions$umap@cell.embeddings[,1], 
                UMAP_2=obj@reductions$umap@cell.embeddings[,2],
-               Sample=obj$orig.ident,
-               batch=obj$batch)
-
-if(has_batch_file){
-  mt$batch=obj$batch
-}
+               Sample=obj$orig.ident)
 
 nSamples = length(unique(obj$orig.ident))
 nWidth=ceiling(sqrt(nSamples))
 nHeight=ceiling(nSamples / nWidth)
 
-cat("draw pictures ... ")
-draw_dimplot(mt, paste0(outFile, ".cluster_sample.png"), "Sample")
-draw_dimplot(mt, paste0(outFile, ".cluster_batch.png"), "batch")
+cat("draw pictures ... \n")
+draw_dimplot(mt, paste0(outFile, ".merge_sample.png"), "Sample")
 
-p1<-DimPlot(object = obj, reduction = 'umap', label=TRUE, group.by="seurat_clusters", cols=seurat_colors)
-p2<-DimPlot(object = obj, reduction = 'umap', label=FALSE, group.by="orig.ident")
-p<-p1+p2
-width=3000
-if(has_batch_file){
-  p3<-DimPlot(object = obj, reduction = 'umap', label=FALSE, group.by="batch")
-  p<-p1+p3+p2
-  width=4500
-}
-png(paste0(outFile, ".cluster.png"), width=width, height=1200, res=300)
+png(paste0(outFile, ".merge.png"), width=1500, height=1200, res=300)
+p<-DimPlot(object = obj, reduction = 'umap', label=FALSE, group.by="orig.ident")
 print(p)
 dev.off()
-
