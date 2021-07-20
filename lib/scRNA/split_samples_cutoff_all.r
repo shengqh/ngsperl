4f964244cff88c6f3fbd358a5ce68f9901bf144c
Quanhu Sheng <shengqh@gmail.com>
2021-07-20 17:34:22 -0500

    get cutoff

--- lib/scRNA/split_samples_cutoff_all.r
+++ lib/scRNA/split_samples_cutoff_all.r
@@ -11,6 +11,7 @@ library(reshape2)
 library(gridExtra)
 library(ggExtra)
 
+#still keep it here in case we need it later.
 my_startval <- function(values,D1="normal",D2="normal",cutoff_point=0) {
   if(cutoff_point >0){
     thresh = cutoff_point
@@ -57,93 +58,28 @@ my_startval <- function(values,D1="normal",D2="normal",cutoff_point=0) {
   return(out)
 }
 
-t=1e-64
-my_em<-function(values, data_name="em", D1="normal", D2="normal", t=1e-64, cutoff_point=0, max_iteration=1000){
-  start <- as.list(my_startval(values, D1, D2, cutoff_point))
-  
-  D1b <- choisycutoff:::hash[[D1]]
-  D2b <- choisycutoff:::hash[[D2]]
-  lambda0 <- 0
-  with(start, {
-    iteration = 0
-    while (abs(lambda0 - mean(lambda)) > t) {
-      lambda <- mean(lambda)
-      lambda0 <- lambda
-      distr1 <- lambda * D1b(values, mu1, sigma1)
-      distr2 <- (1 - lambda) * D2b(values, mu2, sigma2)
-      lambda <- distr1/(distr1 + distr2)
-      mLL2 <- function(mu1, sigma1, mu2, sigma2) return(choisycutoff:::mLL(mu1, 
-                                                                     sigma1, mu2, sigma2, lambda, values, D1b, D2b))
-      start <- as.list(log(c(mu1 = mu1, sigma1 = sigma1, 
-                             mu2 = mu2, sigma2 = sigma2)))
-      out <- bbmle::mle2(mLL2, start, "Nelder-Mead")
-      coef <- out@coef
-      coef_n <- names(coef)
-      names(coef) <- NULL
-      for (i in 1:4) assign(coef_n[i], exp(coef[i]))
-      iteration = iteration + 1
-      if(iteration >= max_iteration){
-        warning(paste0("reach ", max_iteration, " iterations, break."))
-        break
-      }
-    }
-    out <- list(lambda = lambda, param = exp(out@coef), D1 = D1, 
-                D2 = D2, deviance = out@min, data = values, data_name = data_name, 
-                out = out, t = t)
-    class(out) <- "em"
-    return(out)
-  })
-}
-
-my_cutoff<-function (object, t = 1e-64, nb = 10, distr = 2, type1 = 0.05, level = 0.95) 
-{
-  coef <- object$out@coef
-  the_names <- names(coef)
-  coef <- exp(mc2d::rmultinormal(nb, coef, as.vector(object$out@vcov)))
-  coef <- as.list(data.frame(t(coef)))
-  coef <- lapply(coef, function(x) {
-      names(x) <- the_names
-      return(as.list(x))
-  })
-  out <- sapply(coef, function(x) choisycutoff:::lci0(x, mean(object$lambda), 
-      choisycutoff:::hash[[object$D1]], choisycutoff:::hash[[object$D2]], object$data, t))
-  lambda <- rnorm(nb, out[1, ], out[2, ])
-  coef <- sapply(coef, function(x) unlist(x))
-  the_names <- c(rownames(coef), "lambda")
-  coef <- rbind(coef, lambda)
-  coef <- lapply(as.data.frame(coef), function(x) {
-      names(x) <- the_names
-      return(as.list(x))
-  })
-  out <- sapply(coef, function(x) with(x, choisycutoff:::cutoff0(mu1, 
-      sigma1, mu2, sigma2, lambda, object$D1, object$D2, distr, type1)))
-  out <- MASS::fitdistr(out, "normal")
-  the_mean <- out$estimate["mean"]
-  level <- (1 - level)/2
-  level <- c(level, 1 - level)
-  ci <- the_mean + qt(level, Inf) * out$sd["mean"]
-  out <- c(the_mean, ci)
-  names(out) <- c("Estimate", paste(100 * level, "%"))
-  return(out)
-}
-
-get_cutoff<-function(values, prefix, cutoff_point=0){
-  my_out <- my_em(values,"normal","normal", cutoff_point=cutoff_point)
-  cut_off <- my_cutoff(my_out)
+get_cutoff<-function(values, prefix, cutoff_startval=NA){
+  my_out <- em(values,"normal","normal", cutoff_startval=cutoff_startval)
+  cut_off <- cutoff(my_out)
   png(paste0(prefix, ".cutoff.png"), width=2000, height=1600, res=300)
   hist(values,200,F,xlab="concentration",ylab="density", main=NULL,col="grey")
   lines(density(values),lwd=1.5,col="blue")
   lines(my_out,lwd=1.5,col="red")
   abline(v=cut_off[1],lwd=1.5,col="brown")
+  abline(v=my_out$start.val,lwd=1.5,col="green")
   dev.off()
-  return(cut_off[1])
+  return(list(em_out=my_out, cut_off=cut_off))
 }
 
 files_lines=read.table(parSampleFile1, sep="\t")
 files=split(files_lines$V1, files_lines$V2)
 
-cutoffs_lines=read.table(parSampleFile2, sep="\t")
-cutoffs=split(cutoffs_lines$V1, cutoffs_lines$V2)
+if(parSampleFile2 != ''){
+  cutoffs_lines=read.table(parSampleFile2, sep="\t")
+  cutoffs=split(cutoffs_lines$V1, cutoffs_lines$V2)
+}else{
+  cutoffs=list()
+}
 
 params_lines=read.table(parSampleFile3, sep="\t")
 params=split(params_lines$V1, params_lines$V2)
@@ -155,28 +91,39 @@ for(idx in c(1:length(files))){
   output_prefix = paste0(fname, ".HTO")
   output_file=paste0(output_prefix, ".csv")
   
-  if(file.exists(output_file) & params$hto_ignore_exists){
-    next
-  }
+  #if(file.exists(output_file) & params$hto_ignore_exists){
+  #  next
+  #}
 
   rdsfile=files[[idx]]
   cat(fname, ":", rdsfile, " ...\n")
 
   obj=read_hto(rdsfile, output_prefix)
   
-  cutoff_point=ifelse(fname %in% names(cutoffs), as.numeric(cutoffs[[fname]]), 0)
+  cutoff_point=ifelse(fname %in% names(cutoffs), as.numeric(cutoffs[[fname]]), NA)
 
   tagnames=rownames(obj[["HTO"]])
   data <- FetchData(object=obj, vars=tagnames)
+  write.csv(data, file=paste0(output_prefix, ".data.csv"))
   
+  res=NULL
   tagname=tagnames[1]  
   for (tagname in tagnames) {
     values=data[,tagname]
     values=values[values>0]
     cat(paste0("get cutoff of ", tagname, " ...\n"))
-    cutoff=get_cutoff(values, paste0(output_prefix, "_", tagname), cutoff_point)
-    data[,paste0(tagname,"_pos")] = ifelse(data[,tagname]>cutoff, tagname, "Negative")
+    my_cutoff=get_cutoff(values, paste0(output_prefix, "_", tagname), cutoff_point)
+    data[,paste0(tagname,"_pos")] = ifelse(data[,tagname]>my_cutoff$cut_off[1], tagname, "Negative")
+    res<-rbind(res, data.frame(
+      start.val=my_cutoff$em_out$start.val,
+      mu1=my_cutoff$em_out$param["mu1"], 
+      sigma1=my_cutoff$em_out$param["sigma1"], 
+      mu2=my_cutoff$em_out$param["mu2"], 
+      sigma2=my_cutoff$em_out$param["sigma2"], 
+      cutoff=my_cutoff$cut_off[1]))
   }
+  rownames(res)=tagnames
+  write.csv(res, paste0(output_prefix, ".estimated.csv"))
   
   cat(paste0("get classification ...\n"))
   class_names=paste0(tagnames, "_pos")
