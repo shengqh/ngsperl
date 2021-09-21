abe91374b99383476382c073326b4347fc6292e2
Quanhu Sheng <shengqh@gmail.com>
2021-09-20 22:12:50 -0500

    absolute chromosome count

--- lib/SmallRNA/absolute_chromosome_count.py
+++ lib/SmallRNA/absolute_chromosome_count.py
@@ -1,15 +1,17 @@
 import logging
 import argparse
 import pysam
-from DupCountUtils import readDupCountMap
+import pickle
+from DupCountUtils import readDupCountQueries
 
-DEBUG = 0
+DEBUG=False
 
 if DEBUG:
-  inputFile="/scratch/vickers_lab/projects/20210504_6282_RA_smRNAseq_mouse_byTiger/covid19/bowtie1_covid19/result/WTwdM19.bam"
-  countFile="/scratch/vickers_lab/projects/20210504_6282_RA_smRNAseq_mouse_byTiger/preprocessing/identical/result/WTwdM19_clipped_identical.fastq.dupcount"
-  outputFile="/scratch/vickers_lab/projects/20210504_6282_RA_smRNAseq_mouse_byTiger/covid19/bowtie1_covid19_count/result/WTwdM19.count"
+  inputFile="/scratch/vickers_lab/projects/20210817_smallRNA_contamination/3626_RA/bowtie1_contamination_all_01_align/result/HDL_36_JPN.bam"
+  countFile="/scratch/vickers_lab/projects/20210818_3626_RA_smRNA_human_contamination/preprocessing/identical/result/HDL_36_JPN_clipped_identical.fastq.dupcount"
+  outputFile="/scratch/vickers_lab/projects/20210817_smallRNA_contamination/3626_RA/bowtie1_contamination_all_02_count/result/HDL_36_JPN.count"
   name_map_file=None
+  extend_bam=False
 else:
   parser = argparse.ArgumentParser(description="Extract mapped reads to Fastq.",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -18,6 +20,7 @@ else:
   parser.add_argument('-n', '--name_map', action='store', nargs='?', help='Input chromosome to name map file')
   parser.add_argument('-c', '--count', action='store', nargs='?', help="Original dupcount file")
   parser.add_argument('-o', '--output', action='store', nargs='?', help="Output result file")
+  parser.add_argument('-e', '--extend_bam', action='store_true', help="Output extend bam file")
 
   args = parser.parse_args()
   
@@ -27,12 +30,14 @@ else:
   countFile = args.count
   outputFile = args.output
   name_map_file = args.name_map
+  extend_bam = args.extend_bam
 
 logger = logging.getLogger('count')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)-8s - %(message)s')
 
 logger.info("Reading counts from " + countFile)
-counts = readDupCountMap(countFile)
+queries = readDupCountQueries(countFile, 0)
+query_map = {q.Name:q for q in queries}
 
 if name_map_file == None:
   name_map = None
@@ -46,21 +51,66 @@ else:
       name = int(parts[1])
       name_map[chromosome] = name
 
-
 count_map = {}
 logger.info("Reading BAM file " + inputFile)
 with pysam.Samfile(inputFile) as sam:
   for read in sam.fetch():
     refname = read.reference_name
-    count_map.setdefault(refname, {})[read.query_name] = counts[read.query_name]
+    query = query_map[read.query_name]
+    count_map.setdefault(refname, {})[query.Sequence] = [query.Count, read.reference_start, read.reference_end]
 
 with open(outputFile, "w") as sw:
   sw.write("Chrom\tUniqueCount\tReadCount\n")
   for chrom in sorted(count_map.keys()):
     chrom_map = count_map[chrom]
     unique_count = len(chrom_map)
-    chrom_count = sum(chrom_map.values())
+    chrom_count = sum([v[0] for v in chrom_map.values()])
     sw.write(f"{chrom}\t{unique_count}\t{chrom_count}\n")
-  sw.write(f"total\t{len(counts)}\t{sum(counts.values())}\n")
+  sw.write(f"total\t{len(query_map)}\t{sum([q.Count for q in query_map.values()])}\n")
+
+with open(outputFile + ".obj", 'wb') as fout:
+  pickle.dump(count_map, fout)  
+
+chroms = sorted(count_map.keys())
+seqs={}
+for chrom in chroms:
+  chrom_map = count_map[chrom]
+  items = sorted(chrom_map.items(), key=lambda item: item[1][0],  reverse=True)
+  for idx in range(0, min(20, len(items))):
+    item=items[idx]
+    seqs[item[0]] = item[1][0]
+
+seq_items = sorted(seqs.items(), key=lambda item: item[1],  reverse=True)
+#print(seq_items)
+
+with open(outputFile + ".seq", "w") as sw:
+  sw.write("Index\tSequence\tCount\t%s\n" % "\t".join(chroms))
+  for idx in range(0, len(seq_items)):
+    item = seq_items[idx]
+    seq = item[0]
+    count = item[1]
+    sw.write(f"{idx+1}\t{seq}\t{count}")
+    for chrom in chroms:
+      chrom_map = count_map[chrom]
+      if seq in chrom_map:
+        citem = chrom_map[seq]
+        sw.write(f"\t{citem[1]}-{citem[2]}")
+      else:
+        sw.write("\t")
+    sw.write("\n")
+
+if extend_bam:
+  extend_bam_file = outputFile + ".bam"
+  with pysam.Samfile(inputFile) as sam:
+    header = sam.header
+    with pysam.AlignmentFile(extend_bam_file, "wb", header=header) as outf:
+      for read in sam.fetch():
+        query_name = read.query_name
+        query_count = query_map[query_name].Count
+        for idx in range(0, query_count):
+          read.query_name = query_name + ":" + str(idx)
+          outf.write(read)
+    
+  pysam.index(extend_bam_file)
 
 logger.info("Done")
--- lib/SmallRNA/absolute_chromosome_count_table.py
+++ lib/SmallRNA/absolute_chromosome_count_table.py
@@ -2,30 +2,90 @@ import logging
 import argparse
 import subprocess
 import os
+import pickle
+import statistics
 from FileListUtils import readFileMap
 
 DEBUG = 0
 
 if DEBUG:
-  inputFile="/scratch/vickers_lab/projects/20210504_6282_RA_smRNAseq_mouse_byTiger/covid19/bowtie1_covid19_04_table/result/RA_6282_mouse__fileList1.list"
-  outputFile="/scratch/vickers_lab/projects/20210504_6282_RA_smRNAseq_mouse_byTiger/covid19/bowtie1_covid19_04_table/result/RA_6282_mouse.count"
-  name_map_file=None
+  inputFile="/scratch/vickers_lab/projects/20210817_smallRNA_contamination/3626_RA/bowtie1_contamination_all_03_table/result/3626_RA__fileList1.list"
+  objectFile='/scratch/vickers_lab/projects/20210817_smallRNA_contamination/3626_RA/bowtie1_contamination_all_03_table/result/3626_RA__fileList2.list'
+  outputFile="/scratch/vickers_lab/projects/20210817_smallRNA_contamination/3626_RA/bowtie1_contamination_all_03_table/result/3626_RA.count.txt"
 else:
   parser = argparse.ArgumentParser(description="Build count table.",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-  parser.add_argument('-i', '--input', action='store', nargs='?', help='Input count list file')
-  parser.add_argument('-o', '--output', action='store', nargs='?', help="Output result file")
+  parser.add_argument('-i', '--input', action='store', nargs='?', help='Input count list file', required=True)
+  parser.add_argument('-s', '--object', action='store', nargs='?', help='Input count object list file', required=True)
+  parser.add_argument('-o', '--output', action='store', nargs='?', help="Output result file", required=True)
 
   args = parser.parse_args()
   
   print(args)
   
   inputFile = args.input
+  objectFile = args.object
   outputFile = args.output
 
 logger = logging.getLogger('count')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)-8s - %(message)s')
+
+
+logger.info("Reading count map table from " + objectFile)
+objectList = readFileMap(objectFile)
+
+sample_names = sorted(objectList.keys())
+sample_objs = {}
+for sample in sample_names:
+  object_file = objectList[sample]
+  with (open(object_file, "rb")) as fin:
+    sample_objs[sample] = pickle.load(fin)
+
+chroms = set()
+seqcount={}
+for sample in sample_names:
+  count_map = sample_objs[sample]
+  seq_map = {}
+  for chrom in count_map.keys():
+    chroms.add(chrom)
+    chrom_map = count_map[chrom]
+    for seq in chrom_map.keys():
+      seqcount.setdefault(seq, [{}, {sample:0 for sample in sample_names}])
+      seqcount[seq][0][chrom] = f"{chrom_map[seq][1]}-{chrom_map[seq][2]}"
+      seqcount[seq][1][sample] = chrom_map[seq][0]
+
+#calculate seq median value cross samples
+for seq in seqcount.keys():
+  median_count = statistics.median(seqcount[seq][1].values())
+  seqcount[seq].append(median_count)
+
+sorted_seqcount = sorted(seqcount.items(), key=lambda item: item[1][2],  reverse=True)
+sorted_chroms = sorted(chroms)
+
+count = 0
+with open(outputFile + ".top50.txt", "wt") as fout:
+  fout.write("Sequence\t%s\t%s\n" % ("\t".join(sorted_chroms), "\t".join(sample_names)))
+  for item in sorted_seqcount:
+    seq = item[0]
+    chroms = item[1][0]
+    count_map = item[1][1]
+    fout.write(seq)
+
+    for chrom in sorted_chroms:
+      if chrom in chroms:
+        fout.write(f"\t{chrom}:{chroms[chrom]}")
+      else:
+        fout.write("\t")
+
+    for sample in sample_names:
+      fout.write("\t%d" % count_map[sample])
+    
+    fout.write("\n")
+
+    count += 1
+    if count == 50:
+      break
 
 logger.info("Reading count table from " + inputFile)
 countList = readFileMap(inputFile)
