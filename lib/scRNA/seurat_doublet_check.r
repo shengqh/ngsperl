09a454d24fcbc5fb2b690fa7b30525db3cbe8625
Quanhu Sheng <shengqh@gmail.com>
2022-04-21 15:01:20 -0500

    rate from 0.01 to 0.1

--- lib/scRNA/seurat_doublet_check.r
+++ lib/scRNA/seurat_doublet_check.r
@@ -1,59 +1,52 @@
-source("scRNA_func.r")
+
 library(Seurat)
-library(ggplot2)
-library(ggpubr)
-library(cowplot)
-library(scales)
-library(stringr)
-library(htmltools)
-library(patchwork)
+library(DoubletFinder)
 
 options(future.globals.maxSize= 10779361280)
+random.seed=20200107
+min.pct=0.5
+logfc.threshold=0.6
 
 options_table<-read.table(parSampleFile1, sep="\t", header=F, stringsAsFactors = F)
 myoptions<-split(options_table$V1, options_table$V2)
 
-bubblemap_file=myoptions$bubblemap_file
-has_bubblemap <- !is.null(bubblemap_file) && file.exists(bubblemap_file)
+by_sctransform<-ifelse(myoptions$by_sctransform == "0", FALSE, TRUE)
+reduction<-myoptions$reduction
+npcs<-as.numeric(myoptions$pca_dims)
+doublet_rates<-as.numeric(myoptions$doublet_rates)
 
-by_sctransform<-ifelse(myoptions$by_sctransform == "1", TRUE, FALSE)
 assay=ifelse(by_sctransform, "SCT", "RNA")
+by_harmony<-reduction=="harmony"
 
 prefix<-outFile
 
-obj=readRDS(parFile1)
-meta=readRDS(parFile2)
-obj@meta.data=meta
+finalList=readRDS(parFile1)
+obj<-finalList$obj
 
-doublet=read.csv(parFile3, row.names=1)
-colname=colnames(doublet)[grepl('DF.classification', colnames(doublet))][1]
-doublet=doublet[colnames(obj),]
-obj=AddMetaData(obj, doublet[,colname], col.name="DF.classification")
+sweep.res <- paramSweep_v3(obj, PCs = 1:npcs, sct = by_sctransform)
+sweep.stats <- summarizeSweep(sweep.res, GT = FALSE)
+bcmvn <- find.pK(sweep.stats)
+bcmvn$pK=as.numeric(as.character(bcmvn$pK))
 
-dt=table(obj@meta.data[,myoptions$cluster_celltype_layer], obj$DF.classification)
-write.csv(dt, paste0(outFile, ".doublet.csv"))
+pK=bcmvn$pK
+BCmetric=bcmvn$BCmetric
+pK_choose = pK[which(BCmetric %in% max(BCmetric))]
+pN=0.25
 
-doublet_cells=colnames(obj)[obj$DF.classification=="Doublet"]
+params_df<-data.frame("pN"=pN, "pK"=pK_choose, "doublet_rate"=c(1:10)*0.01)
+params_df$nExp = round(ncol(obj) * params_df$doublet_rate)
+params_df$label=paste0("DF.classification_", params_df$pN, "_", params_df$pK, "_", params_df$nExp)
+write.csv(params_df, paste0(outFile, ".options.csv"), row.names=F)
 
-g1<-DimPlot(obj, group.by = myoptions$cluster_layer, label=T) + ggtitle(myoptions$celltype_layer)+
-  scale_color_discrete(labels = levels(obj@meta.data[,myoptions$celltype_layer]))
-g2<-DimPlot(obj, label=F, cells.highlight =doublet_cells) + 
-  ggtitle("DoubletFinder") + scale_color_discrete(type=c("gray", "red"), labels = c("Singlet", "Doublet"))
-g<-g1+g2+plot_layout(ncol=2)
-
-png(paste0(outFile, ".doublet.umap.png"), width=4400, height=2000, res=300)
-print(g)
-dev.off()
-
-
-if(has_bubblemap){
-  g1<-get_bubble_plot(obj, myoptions$cluster_layer, myoptions$celltype_layer, bubblemap_file, assay)
-
-  singlet_obj=subset(obj, DF.classification=="Singlet")
-  g2<-get_bubble_plot(singlet_obj, myoptions$cluster_layer, myoptions$celltype_layer, bubblemap_file, assay)
-
-  g<-g1+g2+plot_layout(ncol=1)
-  png(paste0(outFile, ".bubble.png"), width=4400, height=4000, res=300)
-  print(g)
-  dev.off()
+doublet_rate=0.01
+for (doublet_rate in c(1:10) * 0.01){
+  cat("doublet_rate=", doublet_rate, "\n")
+  nExp <- round(ncol(obj) * doublet_rate)  # expect 4% doublets
+  obj <- doubletFinder_v3(obj, pN =pN, pK = pK_choose, nExp = nExp, PCs = 1:npcs)
 }
+
+write.csv(obj@meta.data, paste0(outFile, ".meta.csv"))
+saveRDS(obj@meta.data, paste0(outFile, ".meta.rds"))
+
+#https://nbisweden.github.io/workshop-scRNAseq/labs/compiled/seurat/seurat_01_qc.html
+  
