2e653b5b54a63baa520487795aa94d1a1e05c4bd
Quanhu Sheng <shengqh@gmail.com>
2022-06-09 22:50:46 -0500

    draw umap for group

---
+++ lib/scRNA/seurat_group_umap.r
@@ -0,0 +1,30 @@
+source("scRNA_func.r")
+
+obj<-read_object(parFile1)
+
+
+my_dimplot<-function(object, group.by, label, labels=NULL, title=NULL, scale_colors=NULL,random.seed=20220606,...){
+  require(ggplot2)
+  require(scales)
+  g<-DimPlot(object, group.by=group.by, label=label, ...)
+  if(!is.null(title)){
+    g<-g+ggtitle(title)
+  }
+  if(!is.null(labels)){
+    nclusters<-length(unlist(unique(object[[group.by]])))
+    ccolors<-hue_pal()(nclusters)
+    set.seed(random.seed)
+    scolors<-sample(ccolors, size=nclusters)
+    g<-g+scale_color_manual(values=scolors, labels = labels)
+  }
+  return(g)
+}
+
+
+for(label in c(TRUE, FALSE)){
+  label_str=ifelse(label, ".label", ".nolabel")
+  png(paste0("AG3669", ".all", label_str, ".umap.png"), width=2300, height=2000, res=300)
+  g<-my_dimplot(object=object, group.by="seurat_clusters", title = "AG3669", label=label, labels=levels(object$seurat_cell_type))
+  print(g)
+  dev.off()
+}
