e368f25223b8bf7dc74e33e39ef557adbf6aad54
Quanhu Sheng <shengqh@gmail.com>
2022-06-17 11:30:28 -0500

    bugfix

--- lib/scRNA/seurat_group_umap.r
+++ lib/scRNA/seurat_group_umap.r
@@ -1,5 +1,18 @@
-source("scRNA_func.r")
+rm(list=ls()) 
+outFile='AK6383'
+parSampleFile1='fileList1.txt'
+parSampleFile2=''
+parSampleFile3=''
+parFile1='C:/projects/nobackup/kirabo_lab/shengq2/20220506_6383_scRNA_human/seurat_merge_03_choose_res/result/AK6383.final.rds'
+parFile2=''
+parFile3=''
 
+
+setwd('C:/projects/nobackup/kirabo_lab/shengq2/20220506_6383_scRNA_human/seurat_merge_04_group_umap/result')
+
+### Parameter setting end ###
+
+source("scRNA_func.r")
 obj<-read_object(parFile1)
 
 
@@ -24,7 +37,7 @@ my_dimplot<-function(object, group.by, label, labels=NULL, title=NULL, scale_col
 for(label in c(TRUE, FALSE)){
   label_str=ifelse(label, ".label", ".nolabel")
   png(paste0("AG3669", ".all", label_str, ".umap.png"), width=2300, height=2000, res=300)
-  g<-my_dimplot(object=object, group.by="seurat_clusters", title = "AG3669", label=label, labels=levels(object$seurat_cell_type))
+  g<-my_dimplot(object=obj, group.by="seurat_clusters", title = "AG3669", label=label, labels=levels(obj$seurat_cell_type))
   print(g)
   dev.off()
 }
