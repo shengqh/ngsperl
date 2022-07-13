124e20a89eaa2d5fa592dca0c256bdb2fb8914eb
Quanhu Sheng <shengqh@gmail.com>
2022-07-13 13:40:16 -0500

    for clonotype and subclontype

--- lib/scRNA/clonotype_cluster.r
+++ lib/scRNA/clonotype_cluster.r
@@ -1,9 +1,22 @@
+rm(list=ls()) 
+outFile='AG3669'
+parSampleFile1='fileList1.txt'
+parSampleFile2=''
+parSampleFile3=''
+parFile1=''
+parFile2='/data/h_gelbard_lab/projects/20220508_scRNA_3669/seurat_merge_03_choose_res/result/AG3669.meta.rds'
+parFile3=''
+outputDirectory='.'
+
+
+setwd('/data/h_gelbard_lab/projects/20220508_scRNA_3669/clonotype_04_db_cluster/result')
+
+### Parameter setting end ###
 
 library("tools")
+library(reshape2)
 
-clonos<-read.csv(parFile1, stringsAsFactors=F)
-clonos<-clonos[order(clonos$frequency, decreasing=T),]
-clono_cells<-unique(clonos[,c("clonotype_id", "cells")])
+file_map = read.table("fileList1.txt", sep="\t")
 
 if(file_ext(parFile2) == "rds"){
   ct<-readRDS(parFile2)
@@ -13,44 +26,51 @@ if(file_ext(parFile2) == "rds"){
   ct$ident_celltype<-paste0(ct$orig.ident, ":", ct$seurat_cluster, ":", ct$cellactivity_clusters)
 }
 
-clon_ic<-apply(clono_cells, 1, function(x){
-  clonotype_id=x[['clonotype_id']]
-  cells=x[['cells']]
-  scells<-unlist(strsplit(cells,';'))
-  not_in_cluster<-scells[!(scells %in% rownames(ct))]
-  scells<-scells[!(scells %in% not_in_cluster)]
-  if(length(scells) > 0){
-    ctv<-ct[scells, "ident_celltype"]
-    res=data.frame(table(ctv))
-    #print(res)
-    if(length(not_in_cluster) > 0){
-      res<-rbind(res, data.frame("ctv"="not_in_cluster", "Freq"=length(not_in_cluster)))
+for (parFile1 in file_map$V1){
+  samplename <- gsub(".csv", "", basename(parFile1))
+
+  clonos<-read.csv(parFile1, stringsAsFactors=F)
+  clonos<-clonos[order(clonos$frequency, decreasing=T),]
+  clono_cells<-unique(clonos[,c("clonotype_id", "cells")])
+
+  clon_ic<-apply(clono_cells, 1, function(x){
+    clonotype_id=x[['clonotype_id']]
+    cells=x[['cells']]
+    scells<-unlist(strsplit(cells,';'))
+    not_in_cluster<-scells[!(scells %in% rownames(ct))]
+    scells<-scells[!(scells %in% not_in_cluster)]
+    if(length(scells) > 0){
+      ctv<-ct[scells, "ident_celltype"]
+      res=data.frame(table(ctv))
+      #print(res)
+      if(length(not_in_cluster) > 0){
+        res<-rbind(res, data.frame("ctv"="not_in_cluster", "Freq"=length(not_in_cluster)))
+      }
+    }else{
+      res<-data.frame("ctv"="not_in_cluster", "Freq"=length(not_in_cluster))
     }
-  }else{
-    res<-data.frame("ctv"="not_in_cluster", "Freq"=length(not_in_cluster))
-  }
-  res$clonotype_id=clonotype_id
-  return(res)
-})
+    res$clonotype_id=clonotype_id
+    return(res)
+  })
 
-clon_ic_df<-do.call(rbind, clon_ic)
-clon_ic_df$ctv<-factor(clon_ic_df$ctv, levels=sort(unique(as.character(clon_ic_df$ctv))))
-clon_ic_df$clonotype_id<-factor(clon_ic_df$clonotype_id, levels=unique(clon_ic_df$clonotype_id))
-#head(clon_ic_df)
+  clon_ic_df<-do.call(rbind, clon_ic)
+  clon_ic_df$ctv<-factor(clon_ic_df$ctv, levels=sort(unique(as.character(clon_ic_df$ctv))))
+  clon_ic_df$clonotype_id<-factor(clon_ic_df$clonotype_id, levels=unique(clon_ic_df$clonotype_id))
+  #head(clon_ic_df)
 
-library(reshape2)
-clon_ic_mdf<-data.frame(acast(clon_ic_df, clonotype_id~ctv, value.var="Freq", fill=0), check.names=F)
-clon_ic_mdf$clonotype_id<-rownames(clon_ic_mdf)
+  clon_ic_mdf<-data.frame(acast(clon_ic_df, clonotype_id~ctv, value.var="Freq", fill=0), check.names=F)
+  clon_ic_mdf$clonotype_id<-rownames(clon_ic_mdf)
 
-final <-
-  merge(clonos, clon_ic_mdf,
-        by = "clonotype_id",
-        all.x = T,
-        sort = F)
+  final <-
+    merge(clonos, clon_ic_mdf,
+          by = "clonotype_id",
+          all.x = T,
+          sort = F)
 
-write.csv(
-  final,
-  paste0(outFile, ".clonotype_cluster.csv"),
-  row.names = F,
-  quote = F
-)
+  write.csv(
+    final,
+    paste0(samplename, ".cluster.csv"),
+    row.names = F,
+    quote = F
+  )
+}
--- lib/scRNA/clonotype_db.r
+++ lib/scRNA/clonotype_db.r
@@ -1,3 +1,18 @@
+rm(list=ls()) 
+outFile='AG3669'
+parSampleFile1='fileList1.txt'
+parSampleFile2=''
+parSampleFile3=''
+parFile1=''
+parFile2='/data/cqs/references/scrna/TCR_clonotypedatabases/McPAS-TCR.csv'
+parFile3='/data/cqs/references/scrna/TCR_clonotypedatabases/TBAdb.csv'
+parFile4='/data/cqs/references/scrna/TCR_clonotypedatabases/vdjdb.csv'
+outputDirectory='.'
+
+
+setwd('/data/h_gelbard_lab/projects/20220508_scRNA_3669/clonotype_04_db/result')
+
+### Parameter setting end ###
 
 ### TCR database search ###
 
@@ -7,262 +22,267 @@ tbadb <- read.csv(parFile3)
 vdjdb <- read.csv(parFile4)
 
 #load clonotypes file
-samplename <- outFile
-clon <- read.csv(parFile1)
 
-##############################################################
-#split TCR sequences into alpha and beta columns
-cdr_aa <- strsplit(as.character(clon$cdr3s_aa), ";")
+file_map = read.table("fileList1.txt", sep="\t")
 
-alpha1 <- data.frame(clon[, 1])
-alpha2 <- data.frame(clon[, 1])
-beta1 <- data.frame(clon[, 1])
-beta2 <- data.frame(clon[, 1])
+for (parFile1 in file_map$V1){
+  samplename <- gsub(".csv", "", basename(parFile1))
+  clon <- read.csv(parFile1)
 
-for (i in 1:nrow(clon)) {
-  cdr <- cdr_aa[[i]]
-  if (length(grep("TRA", cdr)) == 1) {
-    alpha1[i, 2] <- cdr[1]
-  } else if (length(grep("TRA", cdr)) == 2) {
-    alpha1[i, 2] <- cdr[1]
-    alpha2[i, 2] <- cdr[2]
+  ##############################################################
+  #split TCR sequences into alpha and beta columns
+  cdr_aa <- strsplit(as.character(clon$cdr3s_aa), ";")
+
+  alpha1 <- data.frame(clon[, 1])
+  alpha2 <- data.frame(clon[, 1])
+  beta1 <- data.frame(clon[, 1])
+  beta2 <- data.frame(clon[, 1])
+
+  for (i in 1:nrow(clon)) {
+    cdr <- cdr_aa[[i]]
+    if (length(grep("TRA", cdr)) == 1) {
+      alpha1[i, 2] <- cdr[1]
+    } else if (length(grep("TRA", cdr)) == 2) {
+      alpha1[i, 2] <- cdr[1]
+      alpha2[i, 2] <- cdr[2]
+    }
+    
+    if (length(grep("TRB", cdr)) == 1) {
+      beta1[i, 2] <- cdr[grep("TRB", cdr)[1]]
+    } else if (length(grep("TRB", cdr)) == 2) {
+      beta1[i, 2] <- cdr[grep("TRB", cdr)[1]]
+      beta2[i, 2] <- cdr[grep("TRB", cdr)[2]]
+    }
   }
-  
-  if (length(grep("TRB", cdr)) == 1) {
-    beta1[i, 2] <- cdr[grep("TRB", cdr)[1]]
-  } else if (length(grep("TRB", cdr)) == 2) {
-    beta1[i, 2] <- cdr[grep("TRB", cdr)[1]]
-    beta2[i, 2] <- cdr[grep("TRB", cdr)[2]]
-  }
-}
 
-clon2 <-
-  data.frame(
-    clon[, 1:4],
-    cdr3s_aa_TRA1 = gsub("TRA:", "", as.character(alpha1[, 2])),
-    cdr3s_aa_TRA2 = if (ncol(alpha2) == 2) {
-      gsub("TRA:", "", as.character(alpha2[, 2]))
-    } else {
-      rep("NA", nrow(clon))
-    },
-    cdr3s_aa_TRB1 = gsub("TRB:", "", as.character(beta1[, 2])),
-    cdr3s_aa_TRB2 = if (ncol(beta2) == 2) {
-      gsub("TRB:", "", as.character(beta2[, 2]))
-    } else {
-      rep("NA", nrow(clon))
-    },
-    stringsAsFactors = F
+  clon2 <-
+    data.frame(
+      clon[, 1:4],
+      cdr3s_aa_TRA1 = gsub("TRA:", "", as.character(alpha1[, 2])),
+      cdr3s_aa_TRA2 = if (ncol(alpha2) == 2) {
+        gsub("TRA:", "", as.character(alpha2[, 2]))
+      } else {
+        rep("NA", nrow(clon))
+      },
+      cdr3s_aa_TRB1 = gsub("TRB:", "", as.character(beta1[, 2])),
+      cdr3s_aa_TRB2 = if (ncol(beta2) == 2) {
+        gsub("TRB:", "", as.character(beta2[, 2]))
+      } else {
+        rep("NA", nrow(clon))
+      },
+      stringsAsFactors = F
+    )
+  clon2[is.na(clon2)] <- "0"
+
+  #merge with databases
+  #mcpas
+  mcpas_hsa <- mcpas[which(mcpas$Species == "Human"), ]
+  mcpas_hsa_slim <-
+    data.frame(mcpas_hsa[, c(1, 2, 5)], stringsAsFactors = F)
+  mcpas_hsa[] <- lapply(mcpas_hsa, as.character)
+
+  mcpas_list <- list()
+  for (k in 1:4) {
+    cdr3s_aa <-
+      c("cdr3s_aa_TRA1",
+        "cdr3s_aa_TRA2",
+        "cdr3s_aa_TRB1",
+        "cdr3s_aa_TRB2")
+    if (k == 1 | k == 2) {
+      clon2_mcpas1 <-
+        merge(
+          clon2,
+          mcpas_hsa[, c(1, 5)],
+          by.x = cdr3s_aa[k],
+          by.y = "CDR3.alpha.aa",
+          sort = F
+        )
+      colnames(clon2_mcpas1)[9] <- "Pathology_McPAS-TCR"
+      clon2_mcpas1_unique <-
+        clon2_mcpas1[!duplicated(clon2_mcpas1[c(1, 9)]), ]
+      if (nrow(clon2_mcpas1_unique) > 0) {
+        clon2_mcpas1_unique["cdr3_match_McPAS-TCR"] <-
+          paste0("TRA:", clon2_mcpas1_unique[, 1])
+      }
+    } else if (k == 3 | k == 4) {
+      clon2_mcpas1 <-
+        merge(
+          clon2,
+          mcpas_hsa[, c(2, 5)],
+          by.x = cdr3s_aa[k],
+          by.y = "CDR3.beta.aa",
+          sort = F
+        )
+      colnames(clon2_mcpas1)[9] <- "Pathology_McPAS-TCR"
+      clon2_mcpas1_unique <-
+        clon2_mcpas1[!duplicated(clon2_mcpas1[c(1, 9)]), ]
+      if (nrow(clon2_mcpas1_unique) > 0) {
+        clon2_mcpas1_unique["cdr3_match_McPAS-TCR"] <-
+          paste0("TRB:", clon2_mcpas1_unique[, 1])
+      }
+    }
+    mcpas_list[[k]] <- clon2_mcpas1_unique
+  }
+
+  mcpas_final <- do.call("rbind", mcpas_list)
+  if (nrow(mcpas_final) > 0) {
+    mcpas_final <- mcpas_final[, c(2:5, 9, 10)]
+    mcpas_final[] <-
+      sapply(mcpas_final, function(x)
+        gsub("^0$", "-", x))
+  }
+  #write.csv(mcpas_final, paste0(date, "_", samplename, "_McPAS-TCR.csv"), row.names=F)
+
+  #tbadb
+  tbadb[] <- lapply(tbadb, as.character)
+
+  tbadb_list <- list()
+  for (k in 1:4) {
+    cdr3s_aa <-
+      c("cdr3s_aa_TRA1",
+        "cdr3s_aa_TRA2",
+        "cdr3s_aa_TRB1",
+        "cdr3s_aa_TRB2")
+    if (k == 1 | k == 2) {
+      clon2_tbadb <-
+        merge(clon2,
+              tbadb[, c(2, 8)],
+              by.x = cdr3s_aa[k],
+              by.y = "CDR3.alpha.aa",
+              sort = F)
+      colnames(clon2_tbadb)[9] <- "Disease.name_TBAdb"
+      clon2_tbadb_unique <-
+        clon2_tbadb[!duplicated(clon2_tbadb[c(1, 9)]), ]
+      if (nrow(clon2_tbadb_unique) > 0) {
+        clon2_tbadb_unique["cdr3_match_TBAdb"] <-
+          paste0("TRA:", clon2_tbadb_unique[, 1])
+      }
+    } else if (k == 3 | k == 4) {
+      clon2_tbadb <-
+        merge(clon2,
+              tbadb[, c(2, 9)],
+              by.x = cdr3s_aa[k],
+              by.y = "CDR3.beta.aa",
+              sort = F)
+      colnames(clon2_tbadb)[9] <- "Disease.name_TBAdb"
+      clon2_tbadb_unique <-
+        clon2_tbadb[!duplicated(clon2_tbadb[c(1, 9)]), ]
+      if (nrow(clon2_tbadb_unique) > 0) {
+        clon2_tbadb_unique["cdr3_match_TBAdb"] <-
+          paste0("TRB:", clon2_tbadb_unique[, 1])
+      }
+    }
+    tbadb_list[[k]] <- clon2_tbadb_unique
+  }
+
+  tbadb_final <- do.call("rbind", tbadb_list)
+  if (nrow(tbadb_final) > 0) {
+    tbadb_final <- tbadb_final[, c(2:5, 9, 10)]
+    tbadb_final[] <-
+      sapply(tbadb_final, function(x)
+        gsub("^0$", "-", x))
+  }
+  #write.csv(tbadb_final, paste0(date,  "_", samplename, "_tbadb-TCR.csv"), row.names=F)
+
+  #vdjdb
+  vdjdb[] <- lapply(vdjdb, as.character)
+  vdjdb_tra <-
+    vdjdb[which(vdjdb$gene == "TRA") &&
+            which(vdjdb$species == "HomoSapiens"),]
+  vdjdb_trb <-
+    vdjdb[which(vdjdb$gene == "TRB") &&
+            which(vdjdb$species == "HomoSapiens"),]
+
+  vdjdb_list <- list()
+  for (k in 1:4) {
+    cdr3s_aa <-
+      c("cdr3s_aa_TRA1",
+        "cdr3s_aa_TRA2",
+        "cdr3s_aa_TRB1",
+        "cdr3s_aa_TRB2")
+    if (k == 1 | k == 2) {
+      clon2_vdjdb <-
+        merge(
+          clon2,
+          vdjdb_tra[, c(3, 12)],
+          by.x = cdr3s_aa[k],
+          by.y = "cdr3",
+          sort = F
+        )
+      colnames(clon2_vdjdb)[9] <- "antigen.species_vdjdb"
+      clon2_vdjdb_unique <-
+        clon2_vdjdb[!duplicated(clon2_vdjdb[c(1, 9)]), ]
+      if (nrow(clon2_vdjdb_unique) > 0) {
+        clon2_vdjdb_unique["cdr3_match_vdjdb"] <-
+          paste0("TRA:", clon2_vdjdb_unique[, 1])
+      }
+    } else if (k == 3 | k == 4) {
+      clon2_vdjdb <-
+        merge(
+          clon2,
+          vdjdb_trb[, c(3, 12)],
+          by.x = cdr3s_aa[k],
+          by.y = "cdr3",
+          sort = F
+        )
+      colnames(clon2_vdjdb)[9] <- "antigen.species_vdjdb"
+      clon2_vdjdb_unique <-
+        clon2_vdjdb[!duplicated(clon2_vdjdb[c(1, 9)]), ]
+      if (nrow(clon2_vdjdb_unique) > 0) {
+        clon2_vdjdb_unique["cdr3_match_vdjdb"] <-
+          paste0("TRB:", clon2_vdjdb_unique[, 1])
+      }
+    }
+    vdjdb_list[[k]] <- clon2_vdjdb_unique
+  }
+
+  vdjdb_final <- do.call("rbind", vdjdb_list)
+  if (nrow(vdjdb_final) > 0) {
+    vdjdb_final <- vdjdb_final[, c(2:5, 9, 10)]
+    vdjdb_final[] <-
+      sapply(vdjdb_final, function(x)
+        gsub("^0$", "-", x))
+  }
+  #write.csv(vdjdb_final, paste0(date,  "_", samplename, "_vdjdb-TCR.csv"), row.names=F)
+
+  # put results together in one single file
+  final <-
+    rbind(mcpas_final[, 1:4], tbadb_final[, 1:4], vdjdb_final[, 1:4])
+  final <- final[!duplicated(final$clonotype_id), ]
+  if (nrow(mcpas_final) > 0) {
+    final <-
+      merge(final,
+            mcpas_final[, c(1, 5, 6)],
+            by = "clonotype_id",
+            all.x = T,
+            sort = F)
+  }
+  if (nrow(tbadb_final) > 0) {
+    final <-
+      merge(final,
+            tbadb_final[, c(1, 5, 6)],
+            by = "clonotype_id",
+            all.x = T,
+            sort = F)
+  }
+  if (nrow(vdjdb_final) > 0) {
+    final <-
+      merge(final,
+            vdjdb_final[, c(1, 5, 6)],
+            by = "clonotype_id",
+            all.x = T,
+            sort = F)
+  }
+
+  #merge with other sample and cell columns
+  final <- merge(final, clon[,c(1,6:ncol(clon))], by="clonotype_id")
+
+  final <- final[order(final$frequency, decreasing = T), ]
+  final[is.na(final)] <- "-"
+  write.csv(
+    final,
+    paste0(samplename, ".db.csv"),
+    row.names = F,
+    quote = F
   )
-clon2[is.na(clon2)] <- "0"
-
-#merge with databases
-#mcpas
-mcpas_hsa <- mcpas[which(mcpas$Species == "Human"), ]
-mcpas_hsa_slim <-
-  data.frame(mcpas_hsa[, c(1, 2, 5)], stringsAsFactors = F)
-mcpas_hsa[] <- lapply(mcpas_hsa, as.character)
-
-mcpas_list <- list()
-for (k in 1:4) {
-  cdr3s_aa <-
-    c("cdr3s_aa_TRA1",
-      "cdr3s_aa_TRA2",
-      "cdr3s_aa_TRB1",
-      "cdr3s_aa_TRB2")
-  if (k == 1 | k == 2) {
-    clon2_mcpas1 <-
-      merge(
-        clon2,
-        mcpas_hsa[, c(1, 5)],
-        by.x = cdr3s_aa[k],
-        by.y = "CDR3.alpha.aa",
-        sort = F
-      )
-    colnames(clon2_mcpas1)[9] <- "Pathology_McPAS-TCR"
-    clon2_mcpas1_unique <-
-      clon2_mcpas1[!duplicated(clon2_mcpas1[c(1, 9)]), ]
-    if (nrow(clon2_mcpas1_unique) > 0) {
-      clon2_mcpas1_unique["cdr3_match_McPAS-TCR"] <-
-        paste0("TRA:", clon2_mcpas1_unique[, 1])
-    }
-  } else if (k == 3 | k == 4) {
-    clon2_mcpas1 <-
-      merge(
-        clon2,
-        mcpas_hsa[, c(2, 5)],
-        by.x = cdr3s_aa[k],
-        by.y = "CDR3.beta.aa",
-        sort = F
-      )
-    colnames(clon2_mcpas1)[9] <- "Pathology_McPAS-TCR"
-    clon2_mcpas1_unique <-
-      clon2_mcpas1[!duplicated(clon2_mcpas1[c(1, 9)]), ]
-    if (nrow(clon2_mcpas1_unique) > 0) {
-      clon2_mcpas1_unique["cdr3_match_McPAS-TCR"] <-
-        paste0("TRB:", clon2_mcpas1_unique[, 1])
-    }
-  }
-  mcpas_list[[k]] <- clon2_mcpas1_unique
 }
-
-mcpas_final <- do.call("rbind", mcpas_list)
-if (nrow(mcpas_final) > 0) {
-  mcpas_final <- mcpas_final[, c(2:5, 9, 10)]
-  mcpas_final[] <-
-    sapply(mcpas_final, function(x)
-      gsub("^0$", "-", x))
-}
-#write.csv(mcpas_final, paste0(date, "_", samplename, "_McPAS-TCR.csv"), row.names=F)
-
-#tbadb
-tbadb[] <- lapply(tbadb, as.character)
-
-tbadb_list <- list()
-for (k in 1:4) {
-  cdr3s_aa <-
-    c("cdr3s_aa_TRA1",
-      "cdr3s_aa_TRA2",
-      "cdr3s_aa_TRB1",
-      "cdr3s_aa_TRB2")
-  if (k == 1 | k == 2) {
-    clon2_tbadb <-
-      merge(clon2,
-            tbadb[, c(2, 8)],
-            by.x = cdr3s_aa[k],
-            by.y = "CDR3.alpha.aa",
-            sort = F)
-    colnames(clon2_tbadb)[9] <- "Disease.name_TBAdb"
-    clon2_tbadb_unique <-
-      clon2_tbadb[!duplicated(clon2_tbadb[c(1, 9)]), ]
-    if (nrow(clon2_tbadb_unique) > 0) {
-      clon2_tbadb_unique["cdr3_match_TBAdb"] <-
-        paste0("TRA:", clon2_tbadb_unique[, 1])
-    }
-  } else if (k == 3 | k == 4) {
-    clon2_tbadb <-
-      merge(clon2,
-            tbadb[, c(2, 9)],
-            by.x = cdr3s_aa[k],
-            by.y = "CDR3.beta.aa",
-            sort = F)
-    colnames(clon2_tbadb)[9] <- "Disease.name_TBAdb"
-    clon2_tbadb_unique <-
-      clon2_tbadb[!duplicated(clon2_tbadb[c(1, 9)]), ]
-    if (nrow(clon2_tbadb_unique) > 0) {
-      clon2_tbadb_unique["cdr3_match_TBAdb"] <-
-        paste0("TRB:", clon2_tbadb_unique[, 1])
-    }
-  }
-  tbadb_list[[k]] <- clon2_tbadb_unique
-}
-
-tbadb_final <- do.call("rbind", tbadb_list)
-if (nrow(tbadb_final) > 0) {
-  tbadb_final <- tbadb_final[, c(2:5, 9, 10)]
-  tbadb_final[] <-
-    sapply(tbadb_final, function(x)
-      gsub("^0$", "-", x))
-}
-#write.csv(tbadb_final, paste0(date,  "_", samplename, "_tbadb-TCR.csv"), row.names=F)
-
-#vdjdb
-vdjdb[] <- lapply(vdjdb, as.character)
-vdjdb_tra <-
-  vdjdb[which(vdjdb$gene == "TRA") &&
-          which(vdjdb$species == "HomoSapiens"),]
-vdjdb_trb <-
-  vdjdb[which(vdjdb$gene == "TRB") &&
-          which(vdjdb$species == "HomoSapiens"),]
-
-vdjdb_list <- list()
-for (k in 1:4) {
-  cdr3s_aa <-
-    c("cdr3s_aa_TRA1",
-      "cdr3s_aa_TRA2",
-      "cdr3s_aa_TRB1",
-      "cdr3s_aa_TRB2")
-  if (k == 1 | k == 2) {
-    clon2_vdjdb <-
-      merge(
-        clon2,
-        vdjdb_tra[, c(3, 12)],
-        by.x = cdr3s_aa[k],
-        by.y = "cdr3",
-        sort = F
-      )
-    colnames(clon2_vdjdb)[9] <- "antigen.species_vdjdb"
-    clon2_vdjdb_unique <-
-      clon2_vdjdb[!duplicated(clon2_vdjdb[c(1, 9)]), ]
-    if (nrow(clon2_vdjdb_unique) > 0) {
-      clon2_vdjdb_unique["cdr3_match_vdjdb"] <-
-        paste0("TRA:", clon2_vdjdb_unique[, 1])
-    }
-  } else if (k == 3 | k == 4) {
-    clon2_vdjdb <-
-      merge(
-        clon2,
-        vdjdb_trb[, c(3, 12)],
-        by.x = cdr3s_aa[k],
-        by.y = "cdr3",
-        sort = F
-      )
-    colnames(clon2_vdjdb)[9] <- "antigen.species_vdjdb"
-    clon2_vdjdb_unique <-
-      clon2_vdjdb[!duplicated(clon2_vdjdb[c(1, 9)]), ]
-    if (nrow(clon2_vdjdb_unique) > 0) {
-      clon2_vdjdb_unique["cdr3_match_vdjdb"] <-
-        paste0("TRB:", clon2_vdjdb_unique[, 1])
-    }
-  }
-  vdjdb_list[[k]] <- clon2_vdjdb_unique
-}
-
-vdjdb_final <- do.call("rbind", vdjdb_list)
-if (nrow(vdjdb_final) > 0) {
-  vdjdb_final <- vdjdb_final[, c(2:5, 9, 10)]
-  vdjdb_final[] <-
-    sapply(vdjdb_final, function(x)
-      gsub("^0$", "-", x))
-}
-#write.csv(vdjdb_final, paste0(date,  "_", samplename, "_vdjdb-TCR.csv"), row.names=F)
-
-# put results together in one single file
-final <-
-  rbind(mcpas_final[, 1:4], tbadb_final[, 1:4], vdjdb_final[, 1:4])
-final <- final[!duplicated(final$clonotype_id), ]
-if (nrow(mcpas_final) > 0) {
-  final <-
-    merge(final,
-          mcpas_final[, c(1, 5, 6)],
-          by = "clonotype_id",
-          all.x = T,
-          sort = F)
-}
-if (nrow(tbadb_final) > 0) {
-  final <-
-    merge(final,
-          tbadb_final[, c(1, 5, 6)],
-          by = "clonotype_id",
-          all.x = T,
-          sort = F)
-}
-if (nrow(vdjdb_final) > 0) {
-  final <-
-    merge(final,
-          vdjdb_final[, c(1, 5, 6)],
-          by = "clonotype_id",
-          all.x = T,
-          sort = F)
-}
-
-#merge with other sample and cell columns
-final <- merge(final, clon[,c(1,6:ncol(clon))], by="clonotype_id")
-
-final <- final[order(final$frequency, decreasing = T), ]
-final[is.na(final)] <- "-"
-write.csv(
-  final,
-  paste0(samplename, ".clonotype_db.csv"),
-  row.names = F,
-  quote = F
-)
