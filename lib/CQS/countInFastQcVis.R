a05606b6150215789191f67c9f497c88e474c648
Quanhu Sheng <shengqh@gmail.com>
2022-08-31 00:15:21 -0500

    refine qc image

--- lib/CQS/countInFastQcVis.R
+++ lib/CQS/countInFastQcVis.R
@@ -1,4 +1,18 @@
+rm(list=ls()) 
+outFile='obesity.countInFastQcVis.Result'
+parSampleFile1=''
+parSampleFile2=''
+parSampleFile3=''
+parFile1='/scratch/cqs/ravi_shah_projects/20220823_obesity_smallRNA_hg38_all_VAT/preprocessing/fastqc_raw_summary/result/obesity.FastQC.reads.tsv'
+parFile2='/scratch/cqs/ravi_shah_projects/20220823_obesity_smallRNA_hg38_all_VAT/preprocessing/fastqc_post_trim_summary/result/obesity.FastQC.reads.tsv'
+parFile3=''
 
+
+setwd('/scratch/cqs/ravi_shah_projects/20220823_obesity_smallRNA_hg38_all_VAT/preprocessing/fastqc_post_trim_summary/result')
+
+### Parameter setting end ###
+
+source("countTableVisFunctions.R")
 options(bitmapType='cairo')
 
 resultFile<-outFile
@@ -34,44 +48,59 @@ if (countInFastQcTrim1=="") {
   count1$Label="Reads for Mapping"
   countForFigure<-count1
 } else {
-if (countInFastQcTrim2=="") {
-  count2<-readCount(countInFastQcTrim1)
-  
-  count1<-count1[rownames(count2),]
-  count1$Reads<-count1$Reads-count2$Reads
-  
-  if(grepl("post_remove", countInFastQcTrim1)){
-    count1$Label="Removed by Removing Sequence"
-  } else if (grepl("post_trim", countInFastQcTrim1)){
-    count1$Label="Removed by Trimming"
+  if (countInFastQcTrim2=="") {
+    count2<-readCount(countInFastQcTrim1)
+
+    stopifnot(nrow(count1) == nrow(count2))
+    
+    count1<-count1[rownames(count2),]
+    count1$Reads<-count1$Reads-count2$Reads
+    
+    if(grepl("post_remove", countInFastQcTrim1)){
+      count1$Label="Removed by Removing Sequence"
+    } else if (grepl("post_trim", countInFastQcTrim1)){
+      count1$Label="Removed by Trimming"
+    } else {
+      count1$Label="Removed by Trimming or Removing Sequence"
+    }  
+    
+    count2$Label="Reads for Mapping"
+    
+    countForFigure<-rbind(count2,count1)
   } else {
-    count1$Label="Removed by Trimming or Removing Sequence"
-  }  
-  
-  count2$Label="Reads for Mapping"
-  
-  countForFigure<-rbind(count2,count1)
-} else {
-  count2<-readCount(countInFastQcTrim1)
-  count3<-readCount(countInFastQcTrim2)
-  
-  count1<-count1[rownames(count3),]
-  count2<-count2[rownames(count3),]
-  
-  count1$Reads<-count1$Reads-count2$Reads
-  count2$Reads<-count2$Reads-count3$Reads
-  
-  count1$Label="Removed by Removing Sequence"
-  count2$Label="Removed by Trimming"
-  count3$Label="Reads for Mapping"
-  
-  countForFigure<-rbind(count3,count1,count2)
+    count2<-readCount(countInFastQcTrim1)
+    count3<-readCount(countInFastQcTrim2)
+    
+    count1<-count1[rownames(count3),]
+    count2<-count2[rownames(count3),]
+    
+    count1$Reads<-count1$Reads-count2$Reads
+    count2$Reads<-count2$Reads-count3$Reads
+    
+    count1$Label="Removed by Removing Sequence"
+    count2$Label="Removed by Trimming"
+    count3$Label="Reads for Mapping"
+    
+    countForFigure<-rbind(count3,count1,count2)
+  }
 }
-}
+
+g<-ggplot(countForFigure,aes(x=Sample,y=Reads,fill=Label))+
+  geom_bar(stat="identity", width=0.5)+
+  theme_bw2() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        legend.position="top",
+        legend.title=element_blank()) +
+  xlab("")
 
 width<-max(14, length(unique(countForFigure$Sample)) * 0.15)
 pdf(paste0(resultFile,".pdf"),width=width)
-print(ggplot(countForFigure,aes(x=Sample,y=Reads,fill=Label))+geom_bar(stat="identity", width=0.5)+theme(axis.text.x = element_text(angle = 90, hjust = 1)))
+print(g)
+dev.off()
+
+width<-max(2000, length(unique(countForFigure$Sample)) * 75)
+png(paste0(resultFile,".png"),width=width, height=1500, res=300)
+print(g)
 dev.off()
 
 countForFigureOut<-acast(countForFigure,Label~Sample,value.var="Reads")
