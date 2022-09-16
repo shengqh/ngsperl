174cbe1f25bdcc33613c0dde1b11e28adc05f255
Quanhu Sheng <shengqh@gmail.com>
2022-09-16 07:48:30 -0500

    output individual group umap

--- lib/scRNA/seurat_group_umap.r
+++ lib/scRNA/seurat_group_umap.r
@@ -1,14 +1,14 @@
 rm(list=ls()) 
-outFile='AK6383'
+outFile='AG_integrated'
 parSampleFile1='fileList1.txt'
 parSampleFile2=''
 parSampleFile3=''
-parFile1='C:/projects/nobackup/kirabo_lab/shengq2/20220506_6383_scRNA_human/seurat_merge_multires_03_choose/result/AK6383.final.rds'
+parFile1='/data/h_gelbard_lab/projects/20220907_8566_project/seurat_sct_harmony_multires_03_choose/result/AG_integrated.final.rds'
 parFile2=''
 parFile3=''
 
 
-setwd('C:/projects/nobackup/kirabo_lab/shengq2/20220506_6383_scRNA_human/seurat_merge_multires_04_group_umap/result')
+setwd('/data/h_gelbard_lab/projects/20220907_8566_project/seurat_sct_harmony_multires_04_group_umap/result')
 
 ### Parameter setting end ###
 
@@ -17,27 +17,44 @@ if(!exists('obj')){
   obj<-read_object(parFile1)
 }
 
-groups<-read.table(parSampleFile1, sep="\t", stringsAsFactors = F)
-groups_map<-unlist(split(groups$V2, groups$V1))
-
-obj$group <- groups_map[obj$orig.ident]
-
 g<-get_dim_plot(obj, group.by="seurat_clusters", label.by = "seurat_cell_type")
 print(g)
 
+label<-TRUE
 for(label in c(TRUE, FALSE)){
   label_str=ifelse(label, ".label", ".nolabel")
   g<-get_dim_plot(obj, group.by="seurat_clusters", label.by = "seurat_cell_type", label=label, title = "", legend.title = "Cell type")
   png(paste0(outFile, ".all", label_str, ".umap.png"), width=2500, height=2000, res=300)
   print(g)
   dev.off()
-  
-  ngroups=length(unique(obj$group))
-  g<-get_dim_plot(obj, group.by="seurat_clusters", label.by = "seurat_cell_type", label=label, title = "", legend.title = "Cell type", split.by="group")
-  g<-add_x_y_axis(g)
-  png(paste0(outFile, ".group", label_str, ".umap.png"), width=2000 * ngroups + 500, height=2000, res=300)
-  print(g)
-  dev.off()
+
+  groups<-read.table(parSampleFile1, sep="\t", stringsAsFactors = F)
+  groups_map<-split(groups$V1, groups$V2)
+
+  for(gname in names(groups_map)){
+    samples = groups_map[[gname]]
+    cells<-colnames(obj)[obj$orig.ident %in% samples]
+    subobj<-subset(obj, cells=cells)
+    g<-get_dim_plot(subobj, group.by="seurat_clusters", label.by = "seurat_cell_type", label=label, title = gname, legend.title = "Cell type")
+    png(paste0(outFile, ".", gname, label_str, ".umap.png"), width=2500, height=2000, res=300)
+    print(g)
+    dev.off()
+  }
+
+  if(all(table(groups$V1) == 1)){ 
+    #each sample belongs to one group
+    cells<-colnames(obj)[obj$orig.ident %in% groups$V1]
+    subobj<-subset(obj, cells=cells)
+    groups_map<-unlist(split(groups$V2, groups$V1))
+    subobj$group <- groups_map[subobj$orig.ident]
+
+    ngroups=length(unique(subobj$group))
+    g<-get_dim_plot(subobj, group.by="seurat_clusters", label.by = "seurat_cell_type", label=label, title = "", legend.title = "Cell type", split.by="group")
+    g<-add_x_y_axis(g)
+    png(paste0(outFile, ".group", label_str, ".umap.png"), width=2000 * ngroups + 500, height=2000, res=300)
+    print(g)
+    dev.off()
+  }
 }
 
 save_highlight_cell_plot(paste0(outFile, ".cell.png"), obj, group.by = "seurat_cell_type")
