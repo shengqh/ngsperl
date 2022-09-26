d292512cd6f31d39017b523f5321536c18cc68ac
Quanhu Sheng <shengqh@gmail.com>
2022-09-26 10:32:07 -0500

    output heat figure

--- lib/scRNA/seurat_group_umap.r
+++ lib/scRNA/seurat_group_umap.r
@@ -1,24 +1,31 @@
 rm(list=ls()) 
-outFile='AG_integrated'
+outFile='PL_7114_human'
 parSampleFile1='fileList1.txt'
 parSampleFile2=''
 parSampleFile3=''
-parFile1='/data/h_gelbard_lab/projects/20220907_8566_project/seurat_sct_harmony_multires_03_choose/result/AG_integrated.final.rds'
+parFile1='C:/projects/nobackup/h_turner_lab/shengq2/20220805_7114_scRNA_human/seurat_sct_harmony_multires_03_choose/result/PL_7114_human.final.rds'
 parFile2=''
 parFile3=''
 
 
-setwd('/data/h_gelbard_lab/projects/20220907_8566_project/seurat_sct_harmony_multires_04_group_umap/result')
+setwd('C:/projects/nobackup/h_turner_lab/shengq2/20220805_7114_scRNA_human/seurat_sct_harmony_multires_04_group_umap/result')
 
 ### Parameter setting end ###
+
+#library(scico)
 
 source("scRNA_func.r")
 if(!exists('obj')){
   obj<-read_object(parFile1)
 }
 
-g<-get_dim_plot(obj, group.by="seurat_clusters", label.by = "seurat_cell_type")
-print(g)
+groups<-read.table(parSampleFile1, sep="\t", stringsAsFactors = F)
+groups_map<-split(groups$V1, groups$V2)
+
+#g<-get_dim_plot(obj, group.by="seurat_clusters", label.by = "seurat_cell_type")
+#print(g)
+
+heatlist=list()
 
 label<-TRUE
 for(label in c(TRUE, FALSE)){
@@ -28,9 +35,7 @@ for(label in c(TRUE, FALSE)){
   print(g)
   dev.off()
 
-  groups<-read.table(parSampleFile1, sep="\t", stringsAsFactors = F)
-  groups_map<-split(groups$V1, groups$V2)
-
+  gname = names(groups_map)[1]
   for(gname in names(groups_map)){
     samples = groups_map[[gname]]
     cells<-colnames(obj)[obj$orig.ident %in% samples]
@@ -39,6 +44,18 @@ for(label in c(TRUE, FALSE)){
     png(paste0(outFile, ".", gname, label_str, ".umap.png"), width=2500, height=2000, res=300)
     print(g)
     dev.off()
+    
+    if(label){
+      coord=FetchData(obj, c("UMAP_1", "UMAP_2", "orig.ident"))
+      in_group<-coord[colnames(subobj),]
+      out_group<-coord[setdiff(rownames(coord), colnames(subobj)),]
+      
+      g<-ggplot(out_group, aes(UMAP_1, UMAP_2)) + geom_point(color="grey") + 
+        geom_hex(aes(UMAP_1, UMAP_2), data=in_group, bins=70) +
+        scale_fill_gradient(low="white", high="red") + ggtitle(gname) + theme_bw3()
+      
+      heatlist[[gname]] = g
+    }
   }
 
   if(all(table(groups$V1) == 1)){ 
@@ -56,6 +73,17 @@ for(label in c(TRUE, FALSE)){
     dev.off()
   }
 }
+
+library(patchwork)
+
+nf<-length(heatlist)
+ncol=ceiling(sqrt(nf))
+nrow=ceiling(nf/ncol)
+
+gg<-patchwork::wrap_plots(heatlist, nrow =nrow, ncol=ncol)
+png(paste0(outFile, ".heat.png"),width=ncol * 1500, height=nrow*1300, res=300)
+print(gg)
+dev.off()
 
 save_highlight_cell_plot(paste0(outFile, ".cell.png"), obj, group.by = "seurat_cell_type")
 
