d4f3f8b1a3afab59bac262ae832013d20834528e
Quanhu Sheng <shengqh@gmail.com>
2022-10-03 21:21:18 -0500

    compatible with read table missing some samples

--- lib/SmallRNA/nonhostSampleCount.r
+++ lib/SmallRNA/nonhostSampleCount.r
@@ -1,5 +1,5 @@
 rm(list=ls()) 
-outFile='CM_8643_bakeoff'
+outFile='GSE137617'
 parSampleFile1='fileList1.txt'
 parSampleFile2='fileList2.txt'
 parSampleFile3=''
@@ -8,7 +8,7 @@ parFile2=''
 parFile3=''
 
 
-setwd('/scratch/vickers_lab/projects/20220830_8643_CM_smRNA_human_bakeoff/nonhost_genome/nonhost_genome_count/result')
+setwd('/scratch/cqs/shengq2/bugfix/nonhost_count')
 
 ### Parameter setting end ###
 
@@ -20,11 +20,28 @@ myoptions<-read.table(parSampleFile2, header=F, sep="\t")
 myoptions<-split(myoptions$V1, myoptions$V2)
 
 all_reads<-NULL
-file<-files$V1[1]
+file<-files$V1[6]
 for(file in files$V1){
   cat("reading ", file, "...\n")
   cur_reads<-fread(file, sep="\t", header=T)
-  all_reads<-rbind(all_reads, cur_reads)
+  if (is.null(all_reads)){
+    all_reads = cur_reads
+  }else{
+    if(ncol(all_reads) != ncol(cur_reads)){
+      all_colnames = sort(unique(c(colnames(all_reads)[2:ncol(all_reads)], colnames(cur_reads)[2:ncol(cur_reads)])))
+      for(colname in all_colnames){
+        if(!(colname %in% colnames(all_reads))){
+          all_reads[,colname] = 0
+        }
+        if(!(colname %in% colnames(cur_reads))){
+          cur_reads[,colname] = 0
+        }
+      }
+      all_reads<-all_reads[,c("Sequence", all_colnames),with=FALSE]
+      cur_reads<-cur_reads[,c("Sequence", all_colnames),with=FALSE]
+    }
+    all_reads<-rbind(all_reads, cur_reads)
+  }
 }
 
 unique_reads<-all_reads[!duplicated(all_reads$Sequence), c(2:ncol(all_reads)), with=FALSE]
