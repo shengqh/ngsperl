b41c005c17ed8eb06b9213d4e5d20489d23de452
Quanhu Sheng <shengqh@gmail.com>
2022-11-16 10:40:50 -0600

    refine integration

--- lib/scRNA/seurat_integration.r
+++ lib/scRNA/seurat_integration.r
@@ -1,3 +1,18 @@
+rm(list=ls()) 
+outFile='PH_combine'
+parSampleFile1='fileList1.txt'
+parSampleFile2=''
+parSampleFile3=''
+parFile1='/scratch/cqs/shengq2/paula_hurley_projects/20221115_scRNA_7467_benign_hg38/seurat_rawdata/result/PH_combine.rawobj.rds'
+parFile2='/scratch/cqs/shengq2/paula_hurley_projects/20221115_scRNA_7467_benign_hg38/essential_genes/result/PH_combine.txt'
+parFile3=''
+
+
+setwd('/scratch/cqs/shengq2/paula_hurley_projects/20221115_scRNA_7467_benign_hg38/seurat_sct_integration/result')
+
+### Parameter setting end ###
+
+source("scRNA_func.r")
 library(dplyr)
 library(Seurat)
 library(ggplot2)
@@ -42,87 +57,72 @@ objs<-SplitObject(object = rawobj, split.by = "batch")
 rm(rawobj)
 
 #filter cells
-finalList$filter<-list(nFeature_cutoff_min=nFeature_cutoff_min,
-                      nFeature_cutoff_max=nFeature_cutoff_max,
-                      mt_cutoff=mt_cutoff,
-                      nCount_cutoff=nCount_cutoff,
-                      nCount_sd_cutoff=nCount_sd_cutoff)
-
-if(by_sctransform){
-  cat("performing SCTransform ...")
-  #perform sctransform
-  objs<-lapply(objs, function(x){
-    x <- SCTransform(x, verbose = FALSE)
-    return(x)
-  })  
-  obj_features <- SelectIntegrationFeatures(object.list = objs, nfeatures = 3000)
-  assay="SCT"
-}else{
-  cat("performing NormalizeData/FindVariableFeatures ...")
-  #perform standard workflow
-  objs<-lapply(objs, function(x){
-    x <- NormalizeData(x, verbose = FALSE)
-    x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000, verbose = FALSE)  
-    return(x)
-  })  
-  assay="RNA"
-}
+finalList$filter<-list(nFeature_cutoff_min=myoptions$nFeature_cutoff_min,
+                      nFeature_cutoff_max=myoptions$nFeature_cutoff_max,
+                      mt_cutoff=myoptions$mt_cutoff,
+                      nCount_cutoff=myoptions$nCount_cutoff,
+                      nCount_sd_cutoff=myoptions$nCount_sd_cutoff)
 
 if(length(objs) == 1){
   obj <- objs[[1]]
+  if(by_sctransform){
+    obj <- SCTransform(obj, method = "glmGamPoi", verbose = FALSE)
+  }
 }else{
   if(by_sctransform){
+    # https://satijalab.org/seurat/articles/sctransform_v2_vignette.html
+    cat("performing SCTransform ...")
+    # perform sctransform
+    objs <- lapply(objs, FUN=SCTransform, method = "glmGamPoi")
+    features <- SelectIntegrationFeatures(object.list = objs, nfeatures = 3000)
+    assay="SCT"
     all_genes <- Reduce(intersect, lapply(objs, rownames)) 
-    objs <- PrepSCTIntegration(object.list = objs, anchor.features = obj_features, verbose = FALSE)
-    obj_anchors <- FindIntegrationAnchors(object.list = objs, normalization.method = "SCT", anchor.features = obj_features, verbose = FALSE)
-    obj <- IntegrateData(anchorset = obj_anchors, normalization.method = "SCT", verbose = FALSE, features.to.integrate=all_genes)      
-    obj <- RunPCA(object = obj, verbose=FALSE)
-  }else{
-    all_genes <- Reduce(intersect, lapply(objs, rownames)) 
-    obj.anchors <- FindIntegrationAnchors(object.list = objs, dims = 1:20)
-    obj <- IntegrateData(anchorset = obj.anchors, dims = 1:20, features.to.integrate=all_genes)  
+    objs <- PrepSCTIntegration(object.list = objs, anchor.features = features, verbose = FALSE)
+    obj.anchors <- FindIntegrationAnchors(object.list = objs, normalization.method = "SCT", anchor.features = features, verbose = FALSE)
+    obj <- IntegrateData(anchorset = obj.anchors, normalization.method = "SCT", verbose = FALSE, features.to.integrate=all_genes)      
     DefaultAssay(obj) <- "integrated"  
-    obj <- ScaleData(obj, verbose = FALSE)
-    obj <- RunPCA(object = obj, verbose=FALSE)
+  }else{
+    # https://satijalab.org/seurat/articles/integration_introduction.html
+    cat("performing NormalizeData/FindVariableFeatures ...")
+
+    # normalize and identify variable features for each dataset independently
+    objs<-lapply(objs, function(x){
+      x <- NormalizeData(x, verbose = FALSE)
+      x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 2000, verbose = FALSE)  
+      return(x)
+    })  
+    assay="RNA"
+    all_genes <- Reduce(intersect, lapply(objs, rownames)) 
+
+    # select features that are repeatedly variable across datasets for integration
+    features <- SelectIntegrationFeatures(object.list = objs)
+
+    obj.anchors <- FindIntegrationAnchors(object.list = objs, dims = 1:20, anchor.features = features)
+
+    # this command creates an 'integrated' data assay
+    obj <- IntegrateData(anchorset = obj.anchors, dims = 1:20, features.to.integrate=all_genes)  
+
+    # specify that we will perform downstream analysis on the corrected data note that the
+    # original unmodified data still resides in the 'RNA' assay
+    DefaultAssay(obj) <- "integrated"  
   }
 }
 
-obj <- run_cluster_only(obj, Remove_Mt_rRNA, rRNApattern, Mtpattern, pca_dims, by_sctransform, reduction="pca")
+cat("ScaleData ... \n")
+obj <- ScaleData(obj, verbose = FALSE)
 
-seurat_clusters<-unlist(obj[["seurat_clusters"]])
-seurat_colors<-hue_pal()(length(levels(seurat_clusters)))
-names(seurat_colors)<-levels(seurat_clusters)
+cat("RunPCA ... \n")
+obj <- RunPCA(object = obj, verbose=FALSE)
 
-finalList$seurat_colors<-seurat_colors
+output_ElbowPlot(obj, outFile, "pca")
 
-counts=GetAssayData(obj,assay="RNA",slot="counts")
-clusters=obj@active.ident
-sumcounts=get_cluster_count(counts, clusters)
-logsumcounts<-log2(sumcounts+1)
-
-data.quantileAll<-apply(logsumcounts, 2, function(x){quantile(x, 0.75)})
-
-norm_method=""
-if(any(data.quantileAll == 0)){
-  norm_method = ".normByTotal"
-  data.all <- apply(logsumcounts, 2, sum)
-  data.all<-data.all / median(data.all)
-  data.norm <- t(t(logsumcounts) / data.all)
-}else{
-  norm_method = ".normByUpQuantile"
-  data.quantileAll<-data.quantileAll / median(data.quantileAll)
-  data.norm <- t(t(logsumcounts) / data.quantileAll)
-}
-
-write.csv(sumcounts, file=paste0(prefix, ".cluster.count.csv"))
-write.csv(data.norm, file=paste0(prefix, ".cluster", norm_method, ".csv"))
-
-clusters<-data.frame("cell" = c(1:length(obj$seurat_clusters)), "seurat_clusters"=as.numeric(as.character(obj$seurat_clusters)), stringsAsFactors = F)
-rownames(clusters)<-names(obj$seurat_clusters)
-write.csv(clusters, file=paste0(prefix, ".cluster.csv"))
+cat("RunUMAP ... \n")
+obj <- RunUMAP(obj, reduction = "pca", dims = 1:30)    
 
 finalList$obj<-obj
 saveRDS(finalList, file=finalListFile)
 
+cat("output_integration_dimplot ... \n")
 output_integration_dimplot(obj, outFile, FALSE, myoptions$qc_genes)
 
+writeLines(capture.output(sessionInfo()), 'sessionInfo.txt')
