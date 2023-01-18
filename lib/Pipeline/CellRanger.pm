914a99b68fdaec85ab135f584214e86b83756958
Quanhu Sheng <shengqh@gmail.com>
2023-01-17 23:03:33 -0600

    can do vdj only

--- lib/Pipeline/CellRanger.pm
+++ lib/Pipeline/CellRanger.pm
@@ -49,36 +49,40 @@ sub getConfig {
 
   my ( $config, $individual, $summary, $source_ref, $preprocessing_dir, $untrimed_ref, $cluster ) = getPreprocessionConfig($def);
 
-  my $target_dir      = $def->{target_dir};
+  $summary = [@$individual, @$summary];
+
+  my $target_dir = $def->{target_dir};
 
   $config->{count_files} = $def->{count_files};
   $config->{vdj_files} = $def->{vdj_files};
 
-  if(getValue($def, "cellranger_multi_mode", 0)){
-    my $multi_jobmode = getValue($def, "multi_jobmode", "local");
-    my $multi_task_name = "multi_${multi_jobmode}";
-    addCellRangerMulti($config, 
-      $def, 
-      $individual, 
-      $target_dir, 
-      $multi_task_name, 
-      getValue($def, "count_fastq_folder"),
-      "count_files", 
-      getValue($def, "csv_config"),
-      $multi_jobmode);
-  }else{
-    my $count_jobmode = getValue($def, "count_jobmode", "local");
-    my $count_task_name = (defined $def->{count_chemistry}) ? "count_chemistry_$count_jobmode" : "count_$count_jobmode";
-    addCellRangerCount($config, 
-      $def, 
-      $individual, 
-      $target_dir, 
-      $count_task_name, 
-      getValue($def, "count_fastq_folder"),
-      "count_files", 
-      getValue($def, "count_reference"),
-      $count_jobmode,
-      $def->{count_chemistry});
+  if(defined $def->{count_files}){
+    if(getValue($def, "cellranger_multi_mode", 0)){
+      my $multi_jobmode = getValue($def, "multi_jobmode", "local");
+      my $multi_task_name = "multi_${multi_jobmode}";
+      addCellRangerMulti($config, 
+        $def, 
+        $summary, 
+        $target_dir, 
+        $multi_task_name, 
+        getValue($def, "count_fastq_folder"),
+        "count_files", 
+        getValue($def, "csv_config"),
+        $multi_jobmode);
+    }else{
+      my $count_jobmode = getValue($def, "count_jobmode", "local");
+      my $count_task_name = (defined $def->{count_chemistry}) ? "count_chemistry_$count_jobmode" : "count_$count_jobmode";
+      addCellRangerCount($config, 
+        $def, 
+        $summary, 
+        $target_dir, 
+        $count_task_name, 
+        getValue($def, "count_fastq_folder"),
+        "count_files", 
+        getValue($def, "count_reference"),
+        $count_jobmode,
+        $def->{count_chemistry});
+    }
   }
 
   if (defined $def->{vdj_files}) {
@@ -86,7 +90,7 @@ sub getConfig {
     my $vdj_task_name = (defined $def->{vdj_chain}) ? "vdj_chain_$vdj_jobmode" : "vdj_$vdj_jobmode";
     addCellRangerVdj($config, 
       $def, 
-      $individual, 
+      $summary, 
       $target_dir, 
       $vdj_task_name, 
       getValue($def, "vdj_fastq_folder"),
@@ -103,8 +107,7 @@ sub getConfig {
     target_dir => "${target_dir}/sequencetask",
     option     => "",
     source     => {
-      step1 => $individual,
-      step2 => $summary,
+      step1 => $summary,
     },
     sh_direct => 0,
     cluster   => $cluster,
