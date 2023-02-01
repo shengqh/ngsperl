6e3483bf4f16ab60bb6e7c86edd79fd1d3a53179
Quanhu Sheng <shengqh@gmail.com>
2023-02-01 00:45:41 -0600

    refine gsea

--- lib/Pipeline/Pipeline.R
+++ lib/Pipeline/Pipeline.R
@@ -44,49 +44,66 @@ display_webgestalt=function(files) {
   }
 }
 
-processGseaTable=function(gseaTableFile,maxCategoryFdr=0.05,maxCategory=0,absLinkPath=FALSE) {
-  rawTable<-read.delim(gseaTableFile,header=T,as.is=T)
-  rawTable<-rawTable[rawTable$FDR.q.val<=maxCategoryFdr,,drop=F]
-  
-  rawTableOut<-NULL
-  if(maxCategory > 0){
-    rawTable<-head(rawTable, maxCategory)
+processGseaTable=function(gseaTableFile, maxCategoryFdr=0.05) {
+  if(require('msigdbr')){
+    gs<-msigdbr:::msigdbr_genesets
+    gsd_map=split(gs$gs_description, gs$gs_name)
+  }else{
+    gsd_map=list()
   }
-  
-  for (i in nrow(rawTable)) {
-    categoryName=rawTable[i,1]
-    gseaUrl=paste0("http://www.gsea-msigdb.org/gsea/msigdb/geneset_page.jsp?geneSetName=",categoryName)
-    categoryNameInTable<-addLinkTag(text=categoryName,link=gseaUrl)
-    
-    gseaWeb<-getURL(gseaUrl)
-    temp<-strsplit(gseaWeb,"description|\\<td\\>")[[1]]
-    j=grep("Brief",temp)
-    categoryDescription<-gsub("^>","",temp[j+2])
-    categoryDescription<-gsub("<\\/$","",categoryDescription)
-    
-    reg_pattern = "<a href=.+?]</a>"
 
-    if(length(categoryDescription) > 0){
-      while(grepl(reg_pattern, categoryDescription)){
-        url = str_extract(categoryDescription, reg_pattern) 
-        
-        name=str_match(url, '\\[(.+?)]')[[2]]
-        name_url=str_match(url, "<a href='(.+)'")[[2]]
-        namelink=addLinkTag(name, name_url)
-        
-        categoryDescription<-gsub(url, namelink, fixed=TRUE, categoryDescription)
+  rawTable<-read.delim(gseaTableFile,header=T,as.is=T)
+  rawTable<-subset(rawTable, NES != "---")
+
+  rawTable<-rawTable[rawTable$FDR.q.val<=maxCategoryFdr,,drop=F]
+  if(nrow(rawTable) == 0){
+    return(NULL)
+  }
+
+  rawTable$URL = paste0("http://www.gsea-msigdb.org/gsea/msigdb/geneset_page.jsp?geneSetName=",rawTable$NAME)
+  # For RMD
+  # rawTable$NAME_URL=unlist(apply(rawTable, 1, function(x){
+  #   addLinkTag(text=x['NAME'],link=x['URL'])
+  # }))
+  rawTable$NAME_URL=unlist(apply(rawTable, 1, function(x){
+    paste0("<a href='", x['URL'], "' target='_blank'>", gsub("_", " ", x['NAME']), "</a>")
+  }))
+  
+  rawTable$Description=unlist(apply(rawTable, 1, function(x){
+    cname = x['NAME']
+    if(cname %in% names(gsd_map)){
+      categoryDescription=gsd_map[cname]
+    }else{
+      gseaUrl=x['URL']
+      gseaWeb<-getURL(gseaUrl)
+      temp<-strsplit(gseaWeb,"description|\\<td\\>")[[1]]
+      j=grep("Brief",temp)
+      categoryDescription<-gsub("^>","",temp[j+2])
+      categoryDescription<-gsub("<\\/$","",categoryDescription)
+      reg_pattern = "<a href=.+?]</a>"
+      if(length(categoryDescription) > 0){
+        while(grepl(reg_pattern, categoryDescription)){
+          url = str_extract(categoryDescription, reg_pattern) 
+          
+          name=str_match(url, '\\[(.+?)]')[[2]]
+          name_url=str_match(url, "<a href='(.+)'")[[2]]
+          namelink=addLinkTag(name, name_url)
+          
+          categoryDescription<-gsub(url, namelink, fixed=TRUE, categoryDescription)
+        }
       }
     }
+    return(categoryDescription)    
+  }))
 
-    rawTableOut<-rbind(rawTableOut,unlist(c(categoryNameInTable,categoryDescription,rawTable[i,c(4,5,6,8)])))
-  }
-  if(!is.null(rawTableOut)){
-    colnames(rawTableOut)[1:2]<-c("Name","Description")
-  }
-  return(rawTableOut)
+  rawTable$Core=round(rawTable$SIZE * as.numeric(str_extract(rawTable$LEADING.EDGE, "\\d+")) / 100)
+
+  rawTable<-rawTable[,c("NAME", "NAME_URL", "Description", "SIZE", "NES", "FDR.q.val", "Core")]
+  
+  return(rawTable)
 }
 
-parse_gsea_subfolder=function(resultDirSub, gname, gseaCategory, is_positive, maxCategory=0){
+parse_gsea_subfolder=function(resultDirSub, gname, gseaCategory, is_positive){
   pstr = ifelse(is_positive, "pos", "neg")
   title = ifelse(is_positive, "Positive-regulated", "Negative-regulated")
   pattern = paste0("gsea_report_for_na_", pstr, '_\\d+\\.(tsv|xls)$')
@@ -98,21 +115,12 @@ parse_gsea_subfolder=function(resultDirSub, gname, gseaCategory, is_positive, ma
     return(NULL)
   }
   
-  rawTableOut<-processGseaTable(gseaTableFile, maxCategory=maxCategory)
-  if(is.null(rawTableOut)){
+  rawTable<-processGseaTable(gseaTableFile)
+  if(is.null(rawTable)){
     warning(paste("Can't find significant", title, "GSEA gene set in", resultDirSub))
     return(NULL)
   }
-
-  rawTable<-read.delim(gseaTableFile,header=T,as.is=T)
-  rawTable<-rawTable[rawTable$FDR.q.val <= 0.05,,drop=F]
-  if (nrow(rawTable) == 0){
-    return(NULL)
-  }
-
-  rawTable$Core.Enrichment=round(rawTable$SIZE * as.numeric(str_extract(rawTable$LEADING.EDGE, "\\d+")) / 100)
-  rawTable<-rawTable[rawTable$FDR.q.val <= 0.05, c("NAME", "NES", "FDR.q.val", "Core.Enrichment")]
-  return(list(rawTable=rawTable, rawTableOut=rawTableOut))
+  return(rawTable)
 }
 
 display_gsea=function(files, target_folder="", gsea_prefix="#", print_rmd=TRUE) {
@@ -169,57 +177,49 @@ display_gsea=function(files, target_folder="", gsea_prefix="#", print_rmd=TRUE) 
           cat("  ", gseaCategory, "\n")
         }
 
-        pos = parse_gsea_subfolder(resultDirSub, gname, gseaCategory, TRUE, maxCategory=maxCategory)
-        neg = parse_gsea_subfolder(resultDirSub, gname, gseaCategory, FALSE, maxCategory=maxCategory)
+        pos = parse_gsea_subfolder(resultDirSub, gname, gseaCategory, TRUE)
+        neg = parse_gsea_subfolder(resultDirSub, gname, gseaCategory, FALSE)
+
         if (is.null(pos) & is.null(neg)){
-          next
-        }
-
-        prefix = paste0(gname, ".", gseaCategory)
-
-        final = NULL
-        all = NULL
-        if(!is.null(pos)){
-          pos_file=paste0(target_folder, prefix, ".pos.csv")
-          write.csv(pos$rawTableOut, pos_file)
-          result[nrow(result) + 1,] = c(comparison, gname, gseaCategory, "pos_file", pos_file)
-
-          all = pos$rawTable[pos$rawTable$Core.Enrichment > 0,]
-          final = head(all, 10)
-        }
-        if(!is.null(neg)){
-          neg_file=paste0(target_folder, prefix, ".neg.csv")
-          write.csv(neg$rawTableOut, neg_file)
-          result[nrow(result) + 1,] = c(comparison, gname, gseaCategory, "neg_file", neg_file)
-
-          neg_all = head(neg$rawTable[neg$rawTable$Core.Enrichment > 0,], 10)
-          all <- rbind(all, neg_all)
-          final = rbind(final, head(neg_all, 10))
-        }
-
-        if(is.null(all)){
           next
         }
 
         has_enriched = TRUE
 
+        prefix = paste0(gname, ".", gseaCategory)
+
+        top_enriched = NULL
+        if(!is.null(pos)){
+          pos_file=paste0(target_folder, prefix, ".pos.csv")
+          write.csv(pos[,c(2:ncol(pos))], pos_file, row.names=F)
+          result[nrow(result) + 1,] = c(comparison, gname, gseaCategory, "pos_file", pos_file)
+          top_enriched = head(pos, 10)
+        }
+
+        if(!is.null(neg)){
+          neg_file=paste0(target_folder, prefix, ".neg.csv")
+          write.csv(neg[,c(2:ncol(neg))], neg_file, row.names=F)
+          result[nrow(result) + 1,] = c(comparison, gname, gseaCategory, "neg_file", neg_file)
+          top_enriched = rbind(top_enriched, head(neg, 10))
+        }
+
         enriched_file=paste0(target_folder, prefix, ".enriched.csv")
-        write.csv(all, enriched_file, row.names=F)
+        write.csv(top_enriched, enriched_file, row.names=F)
         result[nrow(result) + 1,] = c(comparison, gname, gseaCategory, "enriched_file", enriched_file)
 
-        final$NES = as.numeric(final$NES)
-        final$Core.Enrichment = as.numeric(final$Core.Enrichment)
-        final$FDR.q.val = as.numeric(final$FDR.q.val)
+        top_enriched$NES = as.numeric(top_enriched$NES)
+        top_enriched$Core = as.numeric(top_enriched$Core)
+        top_enriched$FDR.q.val = as.numeric(top_enriched$FDR.q.val)
 
         enriched_png=paste0(enriched_file, ".png")
         result[nrow(result) + 1,] = c(comparison, gname, gseaCategory, "enriched_png", enriched_png)
 
-        final<-final[order(final$NES, decreasing=T),]
-        final$NAME<-factor(final$NAME, levels=final$NAME)
-        g<-ggplot(final, aes(NES, NAME)) + geom_point(aes(size=Core.Enrichment, color=FDR.q.val)) + geom_vline(xintercept=0) + theme_bw() + ylab("") + xlab("Normalized enrichment score")
+        top_enriched<-top_enriched[order(top_enriched$NES, decreasing=T),]
+        top_enriched$NAME<-factor(top_enriched$NAME, levels=top_enriched$NAME)
+        g<-ggplot(top_enriched, aes(NES, NAME)) + geom_point(aes(size=Core, color=FDR.q.val)) + geom_vline(xintercept=0) + theme_bw() + ylab("") + xlab("Normalized enrichment score")
 
-        height=max(1000, 3000/40*nrow(final))
-        ncharmax=max(nchar(as.character(final$NAME)))
+        height=max(1000, 3000/40*nrow(top_enriched))
+        ncharmax=max(nchar(as.character(top_enriched$NAME)))
         width=max(3000, ncharmax * 40 + 1000)
         png(enriched_png, width=width, height=height, res=300)
         print(g)
@@ -228,6 +228,10 @@ display_gsea=function(files, target_folder="", gsea_prefix="#", print_rmd=TRUE) 
         if(print_rmd){
           cat(paste0("\n\n<img src='", enriched_png, "'>\n\n"))
         }
+      }
+
+      if(is_singlecell){
+        break
       }
     }  
 
@@ -238,6 +242,46 @@ display_gsea=function(files, target_folder="", gsea_prefix="#", print_rmd=TRUE) 
     }
   }
   return(result)
+}
+
+save_gsea_rmd<-function(files, resFile, rmd_prefix=""){
+  source("reportFunctions.R")
+  figureRmd<-function(files){
+    result<-""
+    comparisons = unique(files$comparison)
+    comparison<-comparisons[1]
+    for(comparison in comparisons){
+      result<-paste0(result, paste0("\n\n", rmd_prefix, "# ", comparison, "\n\n"))
+
+      comp_files<-files[files$comparison==comparison,,drop=FALSE]
+
+      categories = unique(comp_files$category)
+      category=categories[1]
+      for(category in categories){
+        result<-paste0(result, paste0("\n\n", rmd_prefix, "## ", category, "\n\n"))
+
+        cat_files<-comp_files[comp_files$category == category,,drop=F]
+        file_map<-split(cat_files$file_path, cat_files$file_key)
+
+        if(!is.null(file_map$pos_file)){
+          result<-paste0(result, paste0("\n\n", rmd_prefix, "### Positive-regulated\n\n"))
+          result<-paste0(result, getPagedTable(file_map$pos_file, 0, FALSE))
+        }
+
+        if(!is.null(file_map$neg_file)){
+          result<-paste0(result, paste0("\n\n", rmd_prefix, "### Negative-regulated\n\n"))
+          result<-paste0(result, getPagedTable(file_map$neg_file, 0, FALSE))
+        }
+
+        if(!is.null(file_map$enriched_png)){
+          result<-paste0(result, paste0("\n\n", rmd_prefix, "### Top enriched gene sets\n\n"))
+          result<-paste0(result, getFigure(file_map$enriched_png))
+        }
+      }
+    }
+    return(result)
+  }
+  cat(figureRmd(files), file=resFile)
 }
 
 get_versions=function(){
@@ -262,49 +306,6 @@ get_versions=function(){
   df<-df[order(df$V1),]
   colnames(df)<-c("Software", "Version")
   return(df)
-}
-
-save_gsea_rmd<-function(files, resFile){
-  source("reportFunctions.R")
-  figureRmd<-function(files){
-    result<-""
-    comparisons = unique(files$comparison)
-    comparison<-comparisons[1]
-    for(comparison in comparisons){
-      result<-paste0(result, paste0("\n\n# ", comparison, "\n\n"))
-
-      comp_files<-files[files$comparison==comparison,,drop=FALSE]
-
-      categories = unique(comp_files$category)
-      category=categories[1]
-      for(category in categories){
-        result<-paste0(result, paste0("\n\n## ", category, "\n\n"))
-
-        cat_files<-comp_files[comp_files$category == category,,drop=F]
-        file_map<-split(cat_files$file_path, cat_files$file_key)
-
-        if(!is.null(file_map$pos_file)){
-          result<-paste0(result, paste0("\n\n### Top positive-regulated\n\n"))
-          result<-paste0(result, getTable(file_map$pos_file))
-        }
-
-        if(!is.null(file_map$neg_file)){
-          result<-paste0(result, paste0("\n\n### Top negative-regulated\n\n"))
-          result<-paste0(result, getTable(file_map$neg_file))
-        }
-
-        if(!is.null(file_map$enriched_file)){
-          result<-paste0(result, paste0("\n\n### All enriched gene sets\n\n"))
-          result<-paste0(result, getPagedTable(file_map$enriched_file, 0))
-          
-          result<-paste0(result, paste0("\n\n### Top enriched gene sets\n\n"))
-          result<-paste0(result, getFigure(file_map$enriched_png))
-        }
-      }
-    }
-    return(result)
-  }
-  cat(figureRmd(files), file=resFile)
 }
 
 display_versions=function(){
