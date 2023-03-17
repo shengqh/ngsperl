6a7004a2cf6601b481f783d39253f8ac994cff1b
Quanhu Sheng <shengqh@gmail.com>
2023-03-17 12:44:41 -0500

    test read_bubble_genes_2

--- test/scRNA/test_scRNA_func.r
+++ test/scRNA/test_scRNA_func.r
@@ -6,3 +6,21 @@ test_that("celltype_to_filename", {
   expect_equal(celltype_to_filename("0: Smooth muscle cells"), "0_Smooth_muscle_cells")
   expect_equal(celltype_to_filename("15: Endothelial cells (aorta)"), "15_Endothelial_cells_aorta_")
 })
+
+test_that("read_bubble_genes_1", {
+  bubblemap_file = "/home/shengq2/program/collaborations/alexander_gelbard/Gelbard_BubbleMap_20220804.xlsx"
+  genes_df = read_bubble_genes(bubblemap_file)
+  expect_equal(nrow(genes_df), 57)
+  expect_equal(genes_df$gene[3], "CD19")
+  expect_equal(as.character(genes_df$cell_type[3]), "Pan B Cells")
+})
+
+test_that("read_bubble_genes_2", {
+  bubblemap_file="/home/shengq2/program/projects/justin_turner/20230107_bubble_airway.xlsx"
+  genes_df = read_bubble_genes(bubblemap_file)
+  expect_equal(nrow(genes_df), 68)
+  expect_equal(genes_df$gene[3], "MS4A1")
+  expect_equal(as.character(genes_df$cell_type[3]), "Pan B Cells")
+})
+
+
