a10f69a7418d99a5be9e93bbeb21ea1e896f3337
Quanhu Sheng <shengqh@gmail.com>
2023-03-17 12:44:33 -0500

    output more figure

--- lib/scRNA/SingleR.r
+++ lib/scRNA/SingleR.r
@@ -1,14 +1,14 @@
 rm(list=ls()) 
-outFile='PH_combine'
+outFile='crs'
 parSampleFile1='fileList1.txt'
 parSampleFile2=''
 parSampleFile3=''
-parFile1='C:/projects/scratch/cqs/shengq2/paula_hurley_projects/20220824_scRNA_7467_benign_hg38/seurat_sct_harmony/result/PH_combine.final.rds'
+parFile1='/nobackup/h_turner_lab/shengq2/20230314_7114_8822_scRNA_hg38/seurat_sct_merge/result/crs.final.rds'
 parFile2=''
 parFile3=''
 
 
-setwd('C:/projects/scratch/cqs/shengq2/paula_hurley_projects/20220824_scRNA_7467_benign_hg38/seurat_sct_harmony_SingleR/result')
+setwd('/nobackup/h_turner_lab/shengq2/20230314_7114_8822_scRNA_hg38/seurat_sct_merge_singleR/result')
 
 ### Parameter setting end ###
 
@@ -31,32 +31,44 @@ if(!exists("obj")){
   obj=read_object(parFile1)
 }
 
-sce=as.SingleCellExperiment(DietSeurat(obj))
-labels<-SingleR(sce, ref=hpca.se, assay.type.test=1, labels=hpca.se$label.main)
+force=TRUE
+rds_file=paste0(outFile, ".SingleR.rds")
+if(file.exists(rds_file) & !force){
+  labels<-readRDS(rds_file)
+}else{
+  sce=as.SingleCellExperiment(DietSeurat(obj))
+  labels<-SingleR(sce, ref=hpca.se, assay.type.test=1, labels=hpca.se$label.main)
+  rm(sce)
 
-saveRDS(labels, file=paste0(outFile, ".SingleR.rds"))
+  labels$pruned.labels[is.na(labels$pruned.labels)] <- "unclassified"
+  saveRDS(labels, file=rds_file)
+}
 
 stopifnot(all(colnames(obj) == rownames(labels)))
 
-ct_tbl<-table(labels$labels)
+ct_tbl<-table(labels$pruned.labels)
+ct_tbl<-ct_tbl[names(ct_tbl) != "unclassified"]
 ct_tbl<-ct_tbl[order(ct_tbl, decreasing = T)]
 ct_tbl<-ct_tbl[ct_tbl >= sum(ct_tbl) * 0.01]
 
-labels$major_labels=labels$labels
-labels$major_labels[!(labels$major_labels %in% names(ct_tbl))]="other"
+major_cts=names(ct_tbl)
+
+labels$major_labels=labels$pruned.labels
+labels$major_labels[!(labels$major_labels %in% c(major_cts, "unclassified"))]="other"
 
 ct_name="SingleR_labels"
-obj <- AddMetaData(obj, metadata = labels$labels, col.name = ct_name)
+obj <- AddMetaData(obj, metadata = labels$pruned.labels, col.name = ct_name)
 ct_name="SingleR_major_labels"
 obj <- AddMetaData(obj, metadata = labels$major_labels, col.name = ct_name)
 
 saveRDS(obj@meta.data, paste0(outFile, ".meta.rds"))
 
-ct<-data.frame("SingleR"=obj$SingleR_labels, "Sample"=obj$orig.ident)
-ct_tbl<-table(ct$SingleR,ct$Sample)
-write.csv(ct_tbl, paste0(outFile, ".SingleR_Sample.csv"))
+df<-data.frame("SingleR"=obj$SingleR_labels, "Sample"=obj$orig.ident)
+df_tbl<-table(df$SingleR,df$Sample)
+write.csv(df_tbl, paste0(outFile, ".SingleR_Sample.csv"))
 
-major_obj<-subset(obj, cells=colnames(obj)[obj$SingleR_major_labels != "other"])
+major_obj<-subset(obj, cells=colnames(obj)[obj$SingleR_major_labels %in% major_cts])
+rm(obj)
 
 bubblemap_file=myoptions$bubblemap_file
 has_bubblemap <- !is.null(bubblemap_file) && file.exists(bubblemap_file)
@@ -76,4 +88,19 @@ height=2000
 
 png(paste0(outFile, ".SingleR.png"), width=width, height=height, res=300)
 print(g)
+dev.off()
+
+rm(major_obj)
+
+slim_labels=subset(labels, labels %in% major_cts)
+nct=length(unique(slim_labels$pruned.labels))
+ncol=ceiling(sqrt(nct))
+nrow=ceiling(nct / ncol)
+
+png(paste0(outFile, ".delta.png"), width=max(1500, ncol * 500), height=max(1000, nrow * 500), res=300)
+plotDeltaDistribution(slim_labels, ncol = ncol)
+dev.off()
+
+png(paste0(outFile, ".score.png"), width=4000, height=3000, res=300)
+plotScoreHeatmap(slim_labels)
 dev.off()
