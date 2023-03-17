d6de2c9909b84c6dcd31518380ad59595cb97126
Quanhu Sheng <shengqh@gmail.com>
2023-03-17 12:42:59 -0500

    use overwrite to force redo analysis

--- lib/scRNA/sctk.r
+++ lib/scRNA/sctk.r
@@ -5,25 +5,37 @@ source("scRNA_func.r")
 sample_dirs <- read.table("fileList1.txt", header=F)
 sample_map = unlist(split(sample_dirs$V1, sample_dirs$V2))
 
+overwrite=TRUE
+
 sce_list <- list()
 sample_name=names(sample_map)[1]
 for (sample_name in names(sample_map)){
   cat("sample_name\n")
   countfile = sample_map[sample_name]
 
-  cat("  read_scrna_data\n")
-  lst = read_scrna_data(countfile)
-  counts<-lst$counts
-  sce <- SingleCellExperiment(assays=list(counts=counts))
+  sctk_rds = paste0(sample_name, ".sctk.rds")
 
-  cat("  runCellQC\n")
-  sce = runCellQC(sce, algorithms= c("QCMetrics", "scrublet", "doubletFinder", "scDblFinder", "cxds", "bcds", "cxds_bcds_hybrid", "decontX"))
+  redo = !file.exists(sctk_rds) | overwrite
+  if(!redo){
+    cat("  read", sctk_rds, "\n")
+    sce = readRDS(sctk_rds)
+  }else{
+    cat("  read_scrna_data\n")
+    lst = read_scrna_data(countfile)
+    counts<-lst$counts
+    sce <- SingleCellExperiment(assays=list(counts=counts))
 
-  saveRDS(sce, paste0(sample_name, ".sctk.rds"))
+    cat("  runCellQC\n")
+    sce = runCellQC(sce, algorithms= c("QCMetrics", "scrublet", "doubletFinder", "scDblFinder", "cxds", "bcds", "cxds_bcds_hybrid", "decontX"))
+
+    saveRDS(sce, sctk_rds)
+  }
+
+  saveRDS(colData(sce), paste0(sample_name, ".meta.rds"))
 
   cat("  reportCellQC\n")
-  reportCellQC(inSCE = sce, output_file = paste0(sample_name, "_reportCellQC"))
+  #reportCellQC(inSCE = sce, output_file = paste0(sample_name, "_reportCellQC"))
 
   cat("  exportSCE\n")
-  exportSCE(inSCE = sce, samplename = sample_name, type = "Cells", format = c("Seurat"))
+  #exportSCE(inSCE = sce, samplename = sample_name, type = "Cells", format = c("Seurat"))
 }
