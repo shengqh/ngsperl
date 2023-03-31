17ce78f0932ed8cad79bd553fb4928fe31a504b2
Quanhu Sheng <shengqh@gmail.com>
2023-03-31 10:36:25 -0500

    use_sctransform_v2

--- lib/scRNA/seurat_merge.r
+++ lib/scRNA/seurat_merge.r
@@ -1,5 +1,18 @@
-source("scRNA_func.r")
+rm(list=ls()) 
+outFile='mouse_9622'
+parSampleFile1='fileList1.txt'
+parSampleFile2=''
+parSampleFile3=''
+parFile1='/nobackup/jbrown_lab/shengq2/test/20230330_9622_scRNA_mouse_nodoublets/seurat_nodoublets_rawdata/result/mouse_9622.rawobj.rds'
+parFile2='/nobackup/jbrown_lab/shengq2/test/20230330_9622_scRNA_mouse_nodoublets/essential_genes/result/mouse_9622.txt'
+parFile3=''
 
+
+setwd('/nobackup/jbrown_lab/shengq2/test/20230330_9622_scRNA_mouse_nodoublets/seurat_nodoublets_sct2_merge/result')
+
+### Parameter setting end ###
+
+source("scRNA_func.r")
 library(dplyr)
 library(Seurat)
 library(ggplot2)
@@ -21,8 +34,10 @@ myoptions<-split(options_table$V1, options_table$V2)
 npcs<-as.numeric(myoptions$pca_dims)
 pca_dims<-1:npcs
 
-by_sctransform<-ifelse(myoptions$by_sctransform == "1", TRUE, FALSE)
-regress_by_percent_mt<-ifelse(myoptions$regress_by_percent_mt == "1", TRUE, FALSE)
+by_sctransform<-is_one(myoptions$by_sctransform)
+use_sctransform_v2<-is_one(myoptions$use_sctransform_v2)
+regress_by_percent_mt<-is_one(myoptions$regress_by_percent_mt)
+thread<-to_number(myoptions$thread, 1)
 
 if(regress_by_percent_mt){
   vars.to.regress="percent.mt"
@@ -47,7 +62,7 @@ finalList<-finalList[names(finalList) != "rawobj"]
 DefaultAssay(obj)<-"RNA"
 
 if(by_sctransform){
-  obj<-do_sctransform(obj, vars.to.regress=vars.to.regress, return.only.var.genes=FALSE, mc.cores=8)
+  obj<-do_sctransform(obj, vars.to.regress=vars.to.regress, return.only.var.genes=FALSE, mc.cores=thread, use_sctransform_v2=use_sctransform_v2)
   assay="SCT"
 }else{
   assay="RNA"
@@ -70,10 +85,18 @@ obj <- RunPCA(object = obj, assay=assay, verbose=FALSE)
 
 output_ElbowPlot(obj, outFile, "pca")
 
-cat("run_umap ... \n")
+cat("RunUMAP ... \n")
 obj <- RunUMAP(object = obj, dims=pca_dims, verbose = FALSE)
 
+if(by_sctransform){
+  #clear SCT data for small object
+  #https://github.com/satijalab/seurat/issues/2587
+  obj[["SCT"]]@misc <- NULL    
+}    
+
 finalList$obj<-obj
+
+cat("saveRDS ... \n")
 saveRDS(finalList, file=finalListFile)
 
 cat("output_integration_dimplot ... \n")
