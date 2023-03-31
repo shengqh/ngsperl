d5b5d267c7ab4acf455843eacce7951a9125051b
Quanhu Sheng <shengqh@gmail.com>
2023-03-31 10:36:15 -0500

    adjust bubblemap width/height

--- lib/scRNA/seurat_bubblemap_multi.r
+++ lib/scRNA/seurat_bubblemap_multi.r
@@ -1,21 +1,21 @@
 rm(list=ls()) 
-outFile='GPA_NML'
+outFile='P9551'
 parSampleFile1='fileList1.txt'
 parSampleFile2='fileList2.txt'
 parSampleFile3=''
-parFile1='/data/h_gelbard_lab/projects/20230108_9112_3885_JH_scRNA/seurat_sct_merge_dr0.5_03_choose/result/GPA_NML.final.rds'
-parFile2=''
+parFile1='/nobackup/jbrown_lab/projects/20230329_9551_flynn_hg38_liver_seurat/seurat_merge/result/P9551.final.rds'
+parFile2='/nobackup/jbrown_lab/projects/20230329_9551_flynn_hg38_liver_seurat/seurat_merge_dr0.5_01_call/result/P9551.scDynamic.meta.rds'
 parFile3=''
 
 
-setwd('/data/h_gelbard_lab/projects/20230108_9112_3885_JH_scRNA/seurat_sct_merge_dr0.5_03_choose_bubblemap/result')
+setwd('/nobackup/jbrown_lab/projects/20230329_9551_flynn_hg38_liver_seurat/seurat_merge_dr0.5_01_call_bubblemap_final/result')
 
 ### Parameter setting end ###
 
 source("scRNA_func.r")
 library("Seurat")
 library("readxl")
-library(ggplot2)
+library("ggplot2")
 
 bubble_files=read.table(parSampleFile1, sep="\t", header=F, stringsAsFactors = F)
 
@@ -34,7 +34,7 @@ if(!dir.exists(tmp_folder)){
   dir.create(tmp_folder)
 }
 
-draw_figure<-function(subobj, group1, group2, bubblemap_file, png_prefix, width, rotate.title, tmp_folder, by_group1=TRUE){
+draw_figure<-function(subobj, group1, group2, bubblemap_file, png_prefix, rotate.title, tmp_folder, by_group1, width, min_height, height_per_entry, height_additional_space){
   if(group2 == ""){
     subobj$dump_cluster = unlist(subobj[[group1]])
   }else{
@@ -57,7 +57,8 @@ draw_figure<-function(subobj, group1, group2, bubblemap_file, png_prefix, width,
 
   g=get_bubble_plot(subobj, NULL, NULL, bubblemap_file, assay="RNA", orderby_cluster=TRUE, rotate.title=rotate.title, group.by="dump_cluster_c") + scale_color_gradient2(low="blue", mid="yellow", high="red")
 
-  png(paste0(png_prefix, ".png"), width=width, height=get_dot_height(subobj, "dump_cluster_c"),res=300)
+
+  png(paste0(png_prefix, ".png"), width=width, height=get_dot_height_num(length(unique(subobj$dump_cluster_c)), min_height, height_per_entry, height_additional_space),res=300)
   print(g)
   dev.off()
 
@@ -69,7 +70,7 @@ draw_figure<-function(subobj, group1, group2, bubblemap_file, png_prefix, width,
       c_meta<-meta[meta[,group1]==ct,]
       g_data<-galldata[galldata$id %in% c_meta$dump_cluster_c,]
       g$data<-g_data
-      png(paste0(tmp_folder, "/", png_prefix, ".", celltype_to_filename(ct), ".png"), width=width, height=get_dot_height_vec(g_data$id),res=300)
+      png(paste0(tmp_folder, "/", png_prefix, ".", celltype_to_filename(ct), ".png"), width=width, height=get_dot_height_num(length(unique(g_data$id)), min_height, height_per_entry, height_additional_space),res=300)
       print(g)
       dev.off()
     }
@@ -96,6 +97,10 @@ for(bn in bnames){
   ignore.case=is_one(bn_map$ignore.case)
   rotate.title= is_one(bn_map$rotate.title)
   width=as.numeric(bn_map$width)
+  min_height=to_numeric(bn_map$min_height, 1500)
+  height_per_entry=to_numeric(bn_map$height_per_entry, 80)
+  height_additional_space=to_numeric(bn_map$height_additional_space, 800)
+
   #height=as.numeric(bn_map$height)
   
   if(is.null(cell_type_pattern)){
@@ -107,11 +112,11 @@ for(bn in bnames){
     subobj=obj
   }
 
-  draw_figure(subobj, celltype_name, "", bubblemap_file, paste0(outFile, ".", bn, ".bubblemap.ct"), width, rotate.title, tmp_folder, by_group1 = FALSE)
+  draw_figure(subobj, celltype_name, "", bubblemap_file, paste0(outFile, ".", bn, ".bubblemap.ct"), rotate.title, tmp_folder, by_group1 = FALSE, width, min_height, height_per_entry, height_additional_space)
 
-  draw_figure(subobj, celltype_name, cluster_name, bubblemap_file, paste0(outFile, ".", bn, ".bubblemap.ct_cluster"), width, rotate.title, tmp_folder)
+  draw_figure(subobj, celltype_name, cluster_name, bubblemap_file, paste0(outFile, ".", bn, ".bubblemap.ct_cluster"), rotate.title, tmp_folder, TRUE, width, min_height, height_per_entry, height_additional_space)
 
-  draw_figure(subobj, celltype_name, "orig.ident", bubblemap_file, paste0(outFile, ".", bn, ".bubblemap.ct_ident"), width, rotate.title, tmp_folder)
+  draw_figure(subobj, celltype_name, "orig.ident", bubblemap_file, paste0(outFile, ".", bn, ".bubblemap.ct_ident"), rotate.title, tmp_folder, TRUE, width, min_height, height_per_entry, height_additional_space)
 
-  draw_figure(subobj, "orig.ident", celltype_name, bubblemap_file, paste0(outFile, ".", bn, ".bubblemap.ident_ct"), width, rotate.title, tmp_folder)
+  draw_figure(subobj, "orig.ident", celltype_name, bubblemap_file, paste0(outFile, ".", bn, ".bubblemap.ident_ct"), rotate.title, tmp_folder, TRUE, width, min_height, height_per_entry, height_additional_space)
 }
