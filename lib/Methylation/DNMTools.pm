97f2abdcdf6f085da25ef01a3c70e91d99c00bba
YuWang-VUMC <yu.wang.2@vumc.org>
2023-04-05 05:05:58 -0500

    fix MT

--- lib/Methylation/DNMTools.pm
+++ lib/Methylation/DNMTools.pm
@@ -140,7 +140,7 @@ if [ ! -s ${sample_name}.pmd ]; then
 fi
 
 
-CHRM=('chr1' 'chr2' 'chr3' 'chr4' 'chr5' 'chr6' 'chr7' 'chr8' 'chr9' 'chr10' 'chr11' 'chr12' 'chr13' 'chr14' 'chr15' 'chr16' 'chr17' 'chr18' 'chr19' 'chr20' 'chr21' 'chr22' 'chrX' 'chrY' 'chrM')
+CHRM=('chr1' 'chr2' 'chr3' 'chr4' 'chr5' 'chr6' 'chr7' 'chr8' 'chr9' 'chr10' 'chr11' 'chr12' 'chr13' 'chr14' 'chr15' 'chr16' 'chr17' 'chr18' 'chr19' 'chr20' 'chr21' 'chr22' 'chrX' 'chrY' 'chrMT')
 for chr in \"\${CHRM[\@]}\"
 do
   if [[ ! -s ${sample_name}.\${chr}.epiread && ! -s ${sample_name}.epiread ]]; then
