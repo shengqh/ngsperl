481924573b7890ceb90ff7fa5a057977da16dc0b
Quanhu Sheng <shengqh@gmail.com>
2023-04-26 15:55:50 -0500

    remove legend from pca and umap

--- lib/scRNA/markerCode_filter.R
+++ lib/scRNA/markerCode_filter.R
@@ -456,8 +456,8 @@ preprocess<-function( SampleInfo,
     print(p)
     dev.off()
 
-    plot5<-MyDimPlot(subobj, reduction = "pca",label=T,label.size=4)
-    plot6<-MyDimPlot(subobj, reduction = "umap",label=T,label.size=4)
+    plot5<-MyDimPlot(subobj, reduction = "pca",label=T,label.size=4) + NoLegend()
+    plot6<-MyDimPlot(subobj, reduction = "umap",label=T,label.size=4) + NoLegend()
     
     #cat("\n\n### ", "Fig.5 Boxplot of nUMI,nGene, mtRNA and nCells distribution and PCA, UMAP results\n\n") 
     p<-plot5+plot6+plot_layout(ncol = 2)
