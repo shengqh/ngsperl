8ff8a1a2cc6843976494a0c27b2e5c7975ea7a33
Quanhu Sheng <shengqh@gmail.com>
2023-08-28 14:54:49 -0500

    add cellranger summary

---
+++ lib/scRNA/cellranger_summary.r
@@ -0,0 +1,13 @@
+rm(list=ls())
+outFile='humanatlas_trachea'
+parSampleFile1='fileList1.txt'
+parSampleFile2=''
+parSampleFile3=''
+parFile1=''
+parFile2=''
+parFile3=''
+
+
+setwd('/nobackup/h_cqs/shengq2/test/20230713_humanatlas_trachea_cellranger/count_slurm_summary/result')
+
+### Parameter setting end ###
