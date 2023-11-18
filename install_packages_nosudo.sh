cfaacadf7f71e9329f938bdd1de57e8cdc2cf32e
Quanhu Sheng <shengqh@gmail.com>
2023-11-18 01:20:11 -0600

    install cqs4 and cqs1 perl modules to different folders

--- install_packages_nosudo.sh
+++ install_packages_nosudo.sh
@@ -1,29 +1,39 @@
-curl -L http://cpanmin.us | perl - Data::Dumper
-curl -L http://cpanmin.us | perl - Data::Table
-curl -L http://cpanmin.us | perl - File::Basename
-curl -L http://cpanmin.us | perl - File::Copy
-curl -L http://cpanmin.us | perl - File::Slurp
-curl -L http://cpanmin.us | perl - File::Spec
-curl -L http://cpanmin.us | perl - File::Temp
-curl -L http://cpanmin.us | perl - Getopt::Long
-curl -L http://cpanmin.us | perl - Hash::Merge
-curl -L http://cpanmin.us | perl - HTML::Entities
-curl -L http://cpanmin.us | perl - HTML::Parser
-curl -L http://cpanmin.us | perl - IO::Uncompress::Gunzip
-curl -L http://cpanmin.us | perl - JSON
-curl -L http://cpanmin.us | perl - List::Compare
-curl -L http://cpanmin.us | perl - List::Util
-curl -L http://cpanmin.us | perl - LWP::Simple
-curl -L http://cpanmin.us | perl - LWP::UserAgent
-curl -L http://cpanmin.us | perl - Net::FTP
-curl -L http://cpanmin.us | perl - Scalar::Util
-curl -L http://cpanmin.us | perl - Statistics::Descriptive
-curl -L http://cpanmin.us | perl - String::Util
-curl -L http://cpanmin.us | perl - Test::Deep
-curl -L http://cpanmin.us | perl - Test::Simple
-curl -L http://cpanmin.us | perl - WWW::Mechanize
-curl -L http://cpanmin.us | perl - Algorithm::Loops
-curl -L http://cpanmin.us | perl - JSON::Streaming::Reader
-curl -L http://cpanmin.us | perl - Tie::IxHash
-curl -L http://cpanmin.us | perl - Text::CSV
+#!/bin/bash
+
+if [[ $HOSTNAME == 'cqs1.vampire' || $HOSTNAME == 'cqs3.vampire' ]]; then
+  export TARGET_FOLDER=/nobackup/h_vangard_1/shengq2/perl5_cqs13lib/
+elif [[ $HOSTNAME == 'cqs4.vampire' ]]; then
+  export TARGET_FOLDER=/data/cqs/softwares/perl5_lib/
+else
+  export TARGET_FOLDER="~/perl5"
+fi
+
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Data::Dumper
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Data::Table
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER File::Basename
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER File::Copy
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER File::Slurp
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER File::Spec
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER File::Temp
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Getopt::Long
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Hash::Merge
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER HTML::Entities
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER HTML::Parser
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER IO::Uncompress::Gunzip
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER JSON
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER List::Compare
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER List::Util
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER LWP::Simple
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER LWP::UserAgent
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Net::FTP
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Scalar::Util
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Statistics::Descriptive
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER String::Util
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Test::Deep
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Test::Simple
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER WWW::Mechanize
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Algorithm::Loops
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER JSON::Streaming::Reader
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Tie::IxHash
+curl -L http://cpanmin.us | perl - -l $TARGET_FOLDER Text::CSV
 
