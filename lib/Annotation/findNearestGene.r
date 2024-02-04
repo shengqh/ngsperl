ab4048d636f3df9ae00c1c196be12ca4faf8938e
Quanhu Sheng <shengqh@gmail.com>
2024-02-04 00:40:04 -0600

    bugfix for findNearestGene

--- lib/Annotation/findNearestGene.r
+++ lib/Annotation/findNearestGene.r
@@ -1,14 +1,28 @@
+rm(list=ls()) 
+outFile='P11057_cutrun_hg38'
+parSampleFile1='fileList1.txt'
+parSampleFile2=''
+parSampleFile3=''
+parFile1='/data/cqs/references/gencode/GRCh38.p13/gencode.v38.annotation.gtf.map.bed'
+parFile2=''
+parFile3=''
+
+
+setwd('/nobackup/brown_lab/shengq2/20240202_11057_cutruntools2_hg38_test/seacr_gene/result')
+
+### Parameter setting end ###
 
 options(bitmapType='cairo')
 
 library(testit)
 library(ChIPpeakAnno)
+
 genes <- read.table(parFile1, sep="\t", header=F)
 dup<-genes[duplicated(genes$V4),]
 dup$V4<-paste0(dup$V4,":",dup$V2)
 genes[rownames(dup), "V4"] = dup$V4
 
-assert(length(duplicated(genes$V4)) == 0)
+assert(sum(duplicated(genes$V4)) == 0)
  
 colnames(genes)<-c("seqnames", "start", "end", "name")
 gene2<-toGRanges(genes)
@@ -32,9 +46,11 @@ for (i in c(1:nrow(files))){
   df<-df[,c("seqnames", "peak_start", "peak_end", "peak", "score", "feature", "start_position", "end_position", "insideFeature", "distancetoFeature", "shortestDistance")]
   colnames(df)<-c("chr", "start", "end", "feature", "score", "gene", "gene_start", "gene_end", "insideGene", "distanceToGene", "shortestDistanceToGene")
   
-  write.table(df, file=paste0(file, ".nearest_gene.txt"), quote=F, row.names=F, sep="\t")
+  gene_file=paste0(file, ".nearest_gene.txt")
+  write.table(df, file=gene_file, quote=F, row.names=F, sep="\t")
 
-  res=rbind(res, data.frame("File"=paste0(name, ".nearest_gene.txt"), "Sample"=name))
+  res=rbind(res, data.frame("File"=gene_file, "Sample"=name))
 }
 
-write.csv(res, paste0(outFile, ".csv"), row.names=F)
+write.csv(res, paste0(outFile, ".files.csv"), row.names=F)
+
