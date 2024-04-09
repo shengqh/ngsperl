9c65880e0a321c8e421cb7b7a6207f971e3d7993
Quanhu Sheng <shengqh@gmail.com>
2024-04-09 00:31:05 -0500

    write parameter file

--- lib/Comparison/DESeq2covariance.pm
+++ lib/Comparison/DESeq2covariance.pm
@@ -30,6 +30,9 @@ sub perform {
 
   my ( $task_name, $path_file, $pbs_desc, $target_dir, $log_dir, $pbs_dir, $result_dir, $option, $sh_direct, $cluster, $thread, $memory, $init_command ) = $self->init_parameter( $config, $section );
 
+  #print("writeParameterSampleFile in DESeq2\n");
+  writeParameterSampleFile( $config, $section, $result_dir, 1 );
+
   my $comparisons = get_raw_files( $config, $section );
   my @comparison_names = keys %{$comparisons};
   
