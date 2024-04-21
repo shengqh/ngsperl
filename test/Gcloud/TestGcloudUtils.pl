e26e5939c4bdb51a09329940ee53049aacf0bd99
Quanhu Sheng <shengqh@gmail.com>
2024-04-21 10:16:40 -0500

    bwa with -p

--- test/Gcloud/TestGcloudUtils.pl
+++ test/Gcloud/TestGcloudUtils.pl
@@ -4,7 +4,7 @@ use warnings;
 use Test::More tests => 1;
 use Gcloud::GcloudUtils;
 
-is( replaceSampleNameBWACommand("bwa mem -K 100000000 -v 3 -t 16 -R '\@RG\\tID:TP0097_Norecur_Base\\tPU:illumina\\tLB:TP0097_Norecur_Base\\tSM:TP0097_Norecur_Base\\tPL:illumina' -Y \$bash_ref_fasta", "TEST"), 
-"bwa mem -K 100000000 -v 3 -t 16 -R '\@RG\\tID:TEST\\tPU:illumina\\tLB:TEST\\tSM:TEST\\tPL:illumina' -Y \$bash_ref_fasta");
+is( replaceSampleNameBWACommand("bwa mem -K 100000000 -p -v 3 -t 16 -R '\@RG\\tID:TP0097_Norecur_Base\\tPU:illumina\\tLB:TP0097_Norecur_Base\\tSM:TP0097_Norecur_Base\\tPL:illumina' -Y \$bash_ref_fasta", "TEST"), 
+"bwa mem -K 100000000 -p -v 3 -t 16 -R '\@RG\\tID:TEST\\tPU:illumina\\tLB:TEST\\tSM:TEST\\tPL:illumina' -Y \$bash_ref_fasta");
 
 1;
