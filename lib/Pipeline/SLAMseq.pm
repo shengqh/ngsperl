1dfba856d25ef99df691c28d6ce0d778cbabda4d
Quanhu Sheng <shengqh@gmail.com>
2024-05-21 01:10:26 -0500

    slamseq workflow

--- lib/Pipeline/SLAMseq.pm
+++ lib/Pipeline/SLAMseq.pm
@@ -56,6 +56,9 @@ sub getConfig {
   my $utr3_bed = getValue($def, "utr3_bed");
   my $max_thread = getValue($def, "max_thread", 8);
 
+  #https://github.com/t-neumann/slamdunk/issues/25#issuecomment-385730642
+  #--max-polya 4 is required to generate XA tag, otherwise, count will raise error
+  #since filter command will sort the bam file, we don't need to sort bam file in ngm mapping
   my $nextgenmap_task = "T01_nextgenmap";
   $config->{$nextgenmap_task} = {
     class                 => "CQS::ProgramWrapperOneToOne",
@@ -66,48 +69,37 @@ sub getConfig {
 echo ngm=`date`
 ngm -r $reference_fasta \\
   -1 __FILE__ \\
-  -o __NAME__.sam \\
+  -t $max_thread \\
+  --no-progress \\
+  --slam-seq 2 \\
+  -5 12 \\
+  -l \\
+  --max-polya 4 \\
   --rg-id 1 \\
-  --rg-sm __NAME__ \\
-  --rg-lb __NAME__ \\
-  --rg-pu __NAME__ \\
-  --rg-pl ILLUMINA \\
-  -t $max_thread
+  --rg-sm __NAME__:NA:-1 | samtools view -bS -o __NAME__.tmp.bam -
   
 status=\$?
 if [ \$status -ne 0 ]; then
   touch __NAME__.ngm.failed
-  rm -f __NAME__.sam __NAME__.ngm.succeed
+  rm -f __NAME__.tmp.bam __NAME__.ngm.succeed
 else
   touch __NAME__.ngm.succeed
   rm -f __NAME__.ngm.failed
-
-  echo sort=`date`
-  samtools sort -@ $max_thread -o __NAME__.sorted.tmp.bam __NAME__.sam
-  
-  status=\$?
-  if [ \$status -ne 0 ]; then
-    touch __NAME__.sort.failed
-    rm -f __NAME__.sorted.tmp.bam __NAME__.sort.succeed
-  else
-    touch __NAME__.sort.succeed
-    rm -f __NAME__.sam __NAME__.sort.failed
-    mv __NAME__.sorted.tmp.bam __NAME__.sorted.bam
-  fi
+  mv __NAME__.tmp.bam __NAME__.bam
 fi
 ",
     docker_prefix         => "nextgenmap_",
     interpretor           => "",
     check_program         => 0,
     program               => "",
-    source_ref            => "files",
+    source_ref            => $source_ref,
     source_arg            => "",
     source_join_delimiter => " -2 ",
     output_to_same_folder => 0,
     output_arg            => "-o",
     output_to_folder      => 1,
     output_file_prefix    => "",
-    output_file_ext       => ".sorted.bam",
+    output_file_ext       => ".bam",
     sh_direct             => 0,
     pbs                   => {
       "nodes"     => "1:ppn=$max_thread",
@@ -125,14 +117,13 @@ fi
     init_command          => "",
     option                => "
 slamdunk filter -o . \\
-  --bed $utr3_bed \\
   --threads $max_thread \\
   __FILE__
 
 status=\$?
 if [ \$status -ne 0 ]; then
   touch __NAME__.filter.failed
-  rm -f __NAME__.bam __NAME__.filter.succeed
+  rm -f __NAME___filtered.bam __NAME__.filter.succeed
 else
   touch __NAME__.filter.succeed
   rm -f __NAME__.filter.failed
@@ -143,14 +134,15 @@ fi
     check_program         => 0,
     program               => "",
     source_ref            => $nextgenmap_task,
+    no_bai => 1,
     source_arg            => "",
     source_join_delimiter => "",
     no_output => 1,
-    output_to_same_folder => 0,
+    output_to_same_folder => 1,
     output_arg            => "",
     output_to_folder      => 0,
     output_file_prefix    => "",
-    output_file_ext       => ".bam",
+    output_file_ext       => "_filtered.bam",
     sh_direct             => 0,
     pbs                   => {
       "nodes"     => "1:ppn=$max_thread",
@@ -189,11 +181,11 @@ fi
     source_arg            => "",
     source_join_delimiter => "",
     no_output => 1,
-    output_to_same_folder => 0,
+    output_to_same_folder => 1,
     output_arg            => "",
     output_to_folder      => 0,
     output_file_prefix    => "",
-    output_file_ext       => ".bam",
+    output_file_ext       => "_filtered_snp.vcf",
     sh_direct             => 0,
     pbs                   => {
       "nodes"     => "1:ppn=$max_thread",
@@ -203,6 +195,7 @@ fi
   };
   push (@$tasks, $slamdunk_snp_task);
 
+  my $read_length = getValue($def, "read_length");
   my $slamdunk_count_task = "T04_slamdunk_count";
   $config->{$slamdunk_count_task} = {
     class                 => "CQS::ProgramWrapperOneToOne",
@@ -212,6 +205,9 @@ fi
     option                => "
 slamdunk count -o . \\
   -t $max_thread \\
+  -l $read_length \\
+  -v __FILE2__ \\
+  -b $utr3_bed \\
   -r $reference_fasta \\
   __FILE__
 
@@ -228,11 +224,193 @@ fi
     interpretor           => "",
     check_program         => 0,
     program               => "",
-    source_ref            => $slamdunk_snp_task,
+    source_ref            => $slamdunk_filter_task,
+    source_arg            => "",
+    source_join_delimiter => "",
+    parameterSampleFile2_ref => $slamdunk_snp_task,
+    no_output => 1,
+    output_to_same_folder => 1,
+    output_arg            => "",
+    output_to_folder      => 0,
+    output_file_prefix    => "",
+    output_file_ext       => "_filtered_tcount.tsv",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=$max_thread",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+  push (@$tasks, $slamdunk_count_task);
+
+  #https://github.com/nf-core/slamseq/blob/1.0.0/main.nf
+  my $alleyoop_collapse_task = "T05_alleyoop_collapse";
+  $config->{$alleyoop_collapse_task} = {
+    class                 => "CQS::ProgramWrapperOneToOne",
+    perform               => 1,
+    target_dir            => "$target_dir/$alleyoop_collapse_task",
+    init_command          => "",
+    option                => "
+alleyoop collapse \\
+    -o . \\
+    -t $max_thread \\
+    __FILE__
+
+status=\$?
+if [ \$status -ne 0 ]; then
+  touch __NAME__.collapse.failed
+  rm -f __NAME__.collapse.succeed
+else
+  touch __NAME__.collapse.succeed
+  rm -f __NAME__.collapse.failed
+fi
+",
+    docker_prefix         => "slamdunk_",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "",
+    source_ref            => $slamdunk_count_task,
     source_arg            => "",
     source_join_delimiter => "",
     no_output => 1,
-    output_to_same_folder => 0,
+    output_to_same_folder => 1,
+    output_arg            => "",
+    output_to_folder      => 0,
+    output_file_prefix    => "",
+    output_file_ext       => "_filtered_tcount_collapsed.csv",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=$max_thread",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+  push (@$tasks, $alleyoop_collapse_task);
+
+  my $alleyoop_rates_task = "T06_alleyoop_rates";
+  $config->{$alleyoop_rates_task} = {
+    class                 => "CQS::ProgramWrapperOneToOne",
+    perform               => 1,
+    target_dir            => "$target_dir/$alleyoop_rates_task",
+    init_command          => "",
+    option                => "
+alleyoop rates \\
+  -o . \\
+  -r $reference_fasta \\
+  -t $max_thread \\
+  __FILE__
+
+status=\$?
+if [ \$status -ne 0 ]; then
+  touch __NAME__.rates.failed
+  rm -f __NAME__.rates.succeed
+else
+  touch __NAME__.rates.succeed
+  rm -f __NAME__.rates.failed
+fi
+",
+    docker_prefix         => "slamdunk_",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "",
+    source_ref            => $slamdunk_filter_task,
+    source_arg            => "",
+    source_join_delimiter => "",
+    no_output => 1,
+    output_to_same_folder => 1,
+    output_arg            => "",
+    output_to_folder      => 0,
+    output_file_prefix    => "",
+    output_file_ext       => "_filtered_overallrates.csv",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=$max_thread",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+  push (@$tasks, $alleyoop_rates_task);
+
+  my $alleyoop_utrrates_task = "T07_alleyoop_utrrates";
+  $config->{$alleyoop_utrrates_task} = {
+    class                 => "CQS::ProgramWrapperOneToOne",
+    perform               => 1,
+    target_dir            => "$target_dir/$alleyoop_utrrates_task",
+    init_command          => "",
+    option                => "
+alleyoop utrrates \\
+  -o . \\
+  -r $reference_fasta \\
+  -b $utr3_bed \\
+  -l $read_length \\
+  -t $max_thread \\
+  __FILE__
+
+status=\$?
+if [ \$status -ne 0 ]; then
+  touch __NAME__.utrrates.failed
+  rm -f __NAME__.utrrates.succeed
+else
+  touch __NAME__.utrrates.succeed
+  rm -f __NAME__.utrrates.failed
+fi
+",
+    docker_prefix         => "slamdunk_",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "",
+    source_ref            => $slamdunk_filter_task,
+    source_arg            => "",
+    source_join_delimiter => "",
+    no_output => 1,
+    output_to_same_folder => 1,
+    output_arg            => "",
+    output_to_folder      => 0,
+    output_file_prefix    => "",
+    output_file_ext       => "_filtered_mutationrates_utr.csv",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=$max_thread",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+  push (@$tasks, $alleyoop_utrrates_task);
+
+  my $alleyoop_tcperreadpos_task = "T08_alleyoop_tcperreadpos";
+  $config->{$alleyoop_tcperreadpos_task} = {
+    class                 => "CQS::ProgramWrapperOneToOne",
+    perform               => 1,
+    target_dir            => "$target_dir/$alleyoop_tcperreadpos_task",
+    init_command          => "",
+    option                => "
+alleyoop tcperreadpos \\
+  -o . \\
+  -r $reference_fasta \\
+  -v __FILE2__ \\
+  -l $read_length \\
+  -t $max_thread \\
+  __FILE__
+
+status=\$?
+if [ \$status -ne 0 ]; then
+  touch __NAME__.tcperreadpos.failed
+  rm -f __NAME__.tcperreadpos.succeed
+else
+  touch __NAME__.tcperreadpos.succeed
+  rm -f __NAME__.tcperreadpos.failed
+fi
+",
+    docker_prefix         => "slamdunk_",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "",
+    source_ref            => $slamdunk_filter_task,
+    source_arg            => "",
+    source_join_delimiter => "",
+    parameterSampleFile2_ref => $slamdunk_snp_task,
+    no_output => 1,
+    output_to_same_folder => 1,
     output_arg            => "",
     output_to_folder      => 0,
     output_file_prefix    => "",
@@ -244,7 +422,128 @@ fi
       "mem"       => "40gb"
     },
   };
-  push (@$tasks, $slamdunk_count_task);
+  push (@$tasks, $alleyoop_tcperreadpos_task);
+
+  my $alleyoop_tcperutrpos_task = "T09_alleyoop_tcperutrpos";
+  $config->{$alleyoop_tcperutrpos_task} = {
+    class                 => "CQS::ProgramWrapperOneToOne",
+    perform               => 1,
+    target_dir            => "$target_dir/$alleyoop_tcperutrpos_task",
+    init_command          => "",
+    option                => "
+alleyoop tcperutrpos \\
+  -o . \\
+  -r $reference_fasta \\
+  -b $utr3_bed \\
+  -v __FILE2__ \\
+  -l $read_length \\
+  -t $max_thread \\
+  __FILE__
+
+status=\$?
+if [ \$status -ne 0 ]; then
+  touch __NAME__.tcperutrpos.failed
+  rm -f __NAME__.tcperutrpos.succeed
+else
+  touch __NAME__.tcperutrpos.succeed
+  rm -f __NAME__.tcperutrpos.failed
+fi
+",
+    docker_prefix         => "slamdunk_",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "",
+    source_ref            => $slamdunk_filter_task,
+    source_arg            => "",
+    source_join_delimiter => "",
+    parameterSampleFile2_ref => $slamdunk_snp_task,
+    no_output => 1,
+    output_to_same_folder => 1,
+    output_arg            => "",
+    output_to_folder      => 0,
+    output_file_prefix    => "",
+    output_file_ext       => ".txt",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=$max_thread",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+  push (@$tasks, $alleyoop_tcperutrpos_task);
+
+  my $alleyoop_summary_task = "T10_alleyoop_summary";
+  my $count_folder = $config->{$slamdunk_count_task}{target_dir} . "/result";
+  $config->{$alleyoop_summary_task} = {
+    class                 => "CQS::ProgramWrapper",
+    perform               => 1,
+    target_dir            => "$target_dir/$alleyoop_summary_task",
+    init_command          => "",
+    option                => "
+alleyoop summary \\
+  -o __NAME__.summary.txt \\
+  -t $count_folder \\
+  __FILE__
+
+status=\$?
+if [ \$status -ne 0 ]; then
+  touch __NAME__.summary.failed
+  rm -f __NAME__.summary.succeed
+else
+  touch __NAME__.summary.succeed
+  rm -f __NAME__.summary.failed
+fi
+",
+    docker_prefix         => "slamdunk_",
+    interpretor           => "",
+    check_program         => 0,
+    program               => "",
+    source_ref            => $slamdunk_filter_task,
+    source_arg            => "",
+    source_type           => "array",
+    source_join_delimiter => " ",
+    parameterSampleFile2_ref => $slamdunk_count_task,
+    no_output => 1,
+    output_to_same_folder => 1,
+    output_arg            => "",
+    output_to_folder      => 0,
+    output_file_prefix    => "",
+    output_file_ext => ".summary.txt",
+    output_other_ext => ".summary_PCA.txt",
+    sh_direct             => 0,
+    pbs                   => {
+      "nodes"     => "1:ppn=$max_thread",
+      "walltime"  => "10",
+      "mem"       => "40gb"
+    },
+  };
+  push (@$tasks, $alleyoop_summary_task);
+
+  $config->{report} = {
+    class                      => "CQS::BuildReport",
+    perform                    => 1,
+    target_dir                 => $target_dir . "/" . getNextFolderIndex($def) . "report",
+    report_rmd_file            => "../Pipeline/SLAMseq.Rmd",
+    additional_rmd_files       => "../Pipeline/Pipeline.R;reportFunctions.R",
+    parameterSampleFile1 => {
+      task_name => $task_name,
+      email     => $email,
+      affiliation => getValue($def, "affiliation", ""),
+    },
+    parameterSampleFile2 => $def->{groups},
+    parameterSampleFile4_ref => $alleyoop_summary_task,
+    parameterSampleFile5_ref => $alleyoop_rates_task,
+    parameterSampleFile6_ref => $alleyoop_utrrates_task,
+    parameterSampleFile7_ref => $alleyoop_tcperreadpos_task,
+    parameterSampleFile8_ref => $alleyoop_tcperutrpos_task,
+    sh_direct => 1,
+    pbs => {
+      "nodes"     => "1:ppn=1",
+      "walltime"  => "1",
+      "mem"       => "10gb"
+    },
+  };
+  push( @$tasks, "report" );
 
   $config->{sequencetask} = {
     class      => getSequenceTaskClassname($cluster),
