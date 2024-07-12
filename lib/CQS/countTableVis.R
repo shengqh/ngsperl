10455d7b81f74f80687bffe94dae5a1fdf2b6f60
Quanhu Sheng <shengqh@gmail.com>
2024-07-12 10:41:11 -0500

    refine vis with customized text size

--- lib/CQS/countTableVis.R
+++ lib/CQS/countTableVis.R
@@ -1,3 +1,18 @@
+rm(list=ls()) 
+outFile='DM_7369.fungus_group4Mapping.Result'
+parSampleFile1='fileList1.txt'
+parSampleFile2=''
+parSampleFile3='fileList3.txt'
+parFile1='/nobackup/vickers_lab/projects/20240613_7369_DM_smRNA_human_byMars_rerun/nonhost_genome/bowtie1_fungus_group4_pm_table/result/fungus_group4_pm_DM_7369.Species.count'
+parFile2=''
+parFile3='/nobackup/vickers_lab/projects/20240613_7369_DM_smRNA_human_byMars_rerun/preprocessing/fastqc_post_trim_summary/result/DM_7369.countInFastQcVis.Result.Reads.csv'
+maxCategory=4;
+
+setwd('/nobackup/vickers_lab/projects/20240613_7369_DM_smRNA_human_byMars_rerun/data_visualization/nonhost_genome_fungus_group4_vis/result')
+
+### Parameter setting end ###
+
+source("countTableVisFunctions.R")
 options(bitmapType='cairo')
 
 #############################
@@ -12,6 +27,12 @@ totalCountFile<-parFile3
 groupFileList<-parSampleFile1
 groupVisLayoutFileList<-parSampleFile2
 
+myoptions=read_file_map(parSampleFile3, do_unlist=FALSE)
+barplot_width_px=as.integer(myoptions$barplot_width_px)
+barplot_height_px=as.integer(myoptions$barplot_height_px)
+textSize=as.integer(myoptions$textSize)
+groupTextSize=as.integer(myoptions$groupTextSize)
+
 facetColCount=getFacetColCount(groupFileList)
 
 mappingResult<-read.delim(mappingResultFile,header=T,row.names=1, check.names=F)
@@ -23,14 +44,40 @@ if (max(nchar(row.names(mappingResult2Species)))>stringLengthCut) {
 }
 
 #Pie chart for all samples
-ggpieToFile(mappingResult2Species,fileName=paste0(resultFile,".Piechart.png"),maxCategory=maxCategory,textSize=textSize,facetColCount=facetColCount)
+ggpieToFile(mappingResult2Species,
+            fileName=paste0(resultFile,".Piechart.png"),
+            maxCategory=maxCategory,
+            textSize=textSize,
+            facetColCount=facetColCount)
 
 #Barplot for all samples
-tableBarplotToFile(dat=mappingResult2Species,fileName=paste0(resultFile,".Barplot.png"),totalCountFile=totalCountFile,maxCategory=maxCategory,textSize=textSize)
+tableBarplotToFile( dat=mappingResult2Species, 
+                    fileName=paste0(resultFile,".Barplot.png"),
+                    totalCountFile=totalCountFile,
+                    maxCategory=maxCategory,
+                    textSize=textSize,
+                    xlab="",
+                    width=barplot_width_px, 
+                    height=barplot_height_px)
 
 #Barplot for Group samples
-tableBarplotToFile(dat=mappingResult2Species,fileName=paste0(resultFile,".Group.Barplot.png"),groupFileList=groupFileList,outFileName=paste0(resultFile,".ReadsPerMillionGroups.csv"),totalCountFile=totalCountFile,maxCategory=maxCategory,textSize=textSize)
+tableBarplotToFile( dat=mappingResult2Species,
+                    fileName=paste0(resultFile,".Group.Barplot.png"),
+                    groupFileList=groupFileList,
+                    outFileName=paste0(resultFile,".ReadsPerMillionGroups.csv"),
+                    totalCountFile=totalCountFile,
+                    maxCategory=maxCategory,
+                    textSize=textSize,
+                    xlab="",
+                    height=barplot_height_px)
 
 #Group Pie chart
-ggpieGroupToFile(dat=mappingResult2Species,fileName=paste0(resultFile,".Group.Piechart.png"),groupFileList=groupFileList,
-		outFileName=paste0(resultFile,".PercentGroups.csv"),maxCategory=maxCategory,textSize=groupTextSize,visLayoutFileList=groupVisLayoutFileList)
+ggpieGroupToFile( dat=mappingResult2Species,
+                  fileName=paste0(resultFile,".Group.Piechart.png"),
+                  groupFileList=groupFileList,
+		              outFileName=paste0(resultFile,".PercentGroups.csv"),
+                  maxCategory=maxCategory,
+                  textSize=groupTextSize,
+                  visLayoutFileList=groupVisLayoutFileList)
+
+writeLines(capture.output(sessionInfo()), 'sessionInfo.txt')
