971cebec60c8483c67e09a6baf1ac5c0e20f4e98
Quanhu Sheng <shengqh@gmail.com>
2025-02-17 14:40:54 -0600

    refine scDblFinder

--- lib/scRNA/scDblFinder.r
+++ lib/scRNA/scDblFinder.r
@@ -1,14 +1,15 @@
-rm(list=ls()) 
-outFile='P9061'
+rm(list=ls())
+sample_name='KA_0001'
+outFile='KA_0001'
 parSampleFile1='fileList1.txt'
 parSampleFile2=''
 parSampleFile3=''
-parFile1='/scratch/vickers_lab/projects/20221201_scRNA_9061_mouse/seurat_rawdata/result/P9061.rawobj.rds'
+parFile1=''
 parFile2=''
 parFile3=''
 
 
-setwd('/scratch/vickers_lab/projects/20221201_scRNA_9061_mouse/seurat_rawdata_scDblFinder/result')
+setwd('/nobackup/shah_lab/shengq2/20241030_Kaushik_Amancherla_snRNAseq/20241030_T01_cellbender/scDblFinder/result/KA_0001')
 
 ### Parameter setting end ###
 
@@ -17,16 +18,20 @@ library(Seurat)
 library(scDblFinder)
 library(plyr)
 
-obj <- read_object(parFile1)
-sce <- as.SingleCellExperiment(obj)
-sce <- scDblFinder(sce, samples = "sample")
+h5_file=read.table(parSampleFile1, sep="\t", header=F, stringsAsFactors = F)$V1[1]
 
-obj <- as.Seurat(sce)
+obj <- read_object(h5_file)
+sce <- as.SingleCellExperiment(obj)
+sce <- scDblFinder(sce)
+
+obj <- as.Seurat(sce, data = NULL)
 rm(sce)
 
-saveRDS(obj@meta.data, paste0(outFile, ".scDblFinder_meta.rds"))
+saveRDS(obj@meta.data, paste0(outFile, ".scDblFinder.meta.rds"))
 
-write.csv(table(obj$scDblFinder.class, obj$scDblFinder.sample), paste0(outFile, ".scDblFinder.csv"))
+obj$orig.ident = sample_name
+saveRDS(obj, paste0(outFile, ".scDblFinder.object.rds"))
+write.csv(table(obj$scDblFinder.class, obj$orig.ident), paste0(outFile, ".scDblFinder.class.csv"))
 
 singlet <- subset(obj, subset = scDblFinder.class == "singlet")
-saveRDS(singlet, paste0(outFile, ".scDblFinder.rds"))
+saveRDS(singlet, paste0(outFile, ".scDblFinder.singlet_object.rds"))
