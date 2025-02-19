8f2b6d67059b438f1ded087a61046d19b90a1412
Quanhu Sheng <shengqh@gmail.com>
2025-02-18 21:56:56 -0600

    Can use cell type from dynamic qc as clusters

--- lib/scRNA/scDblFinder.r
+++ lib/scRNA/scDblFinder.r
@@ -1,28 +1,49 @@
-rm(list=ls())
+rm(list=ls()) 
 sample_name='KA_0001'
 outFile='KA_0001'
 parSampleFile1='fileList1.txt'
-parSampleFile2=''
-parSampleFile3=''
+parSampleFile2='fileList2.txt'
+parSampleFile3='fileList3.txt'
 parFile1=''
 parFile2=''
 parFile3=''
 
 
-setwd('/nobackup/shah_lab/shengq2/20241030_Kaushik_Amancherla_snRNAseq/20241030_T01_cellbender/scDblFinder/result/KA_0001')
+setwd('/nobackup/shah_lab/shengq2/20241030_Kaushik_Amancherla_snRNAseq/20241030_T01_cellbender/cellbender_raw_dynamic_qc_sct2_scDblFinder/result/KA_0001')
 
 ### Parameter setting end ###
 
 source("scRNA_func.r")
+source("countTableVisFunctions.R")
 library(Seurat)
 library(scDblFinder)
-library(plyr)
 
 h5_file=read.table(parSampleFile1, sep="\t", header=F, stringsAsFactors = F)$V1[1]
 
 obj <- read_object(h5_file)
+
+if(file.exists(parSampleFile2)){
+  meta_file = read.table(parSampleFile2, sep="\t", header=F, stringsAsFactors = F) |>
+    dplyr::filter(V2==sample_name) |>
+    dplyr::pull(V1)
+
+  meta.data = readRDS(meta_file)
+  obj = subset(obj, cells=rownames(meta.data))
+  stopifnot(all(colnames(obj) == rownames(meta.data)))
+
+  obj@meta.data = meta.data
+
+  clusters = fread(parSampleFile3, header=F, stringsAsFactors = F) |>
+    dplyr::filter(V2=="ct_column") |>
+    dplyr::pull(V1)
+  cat("clusters=", clusters, "\n")
+}else{
+    clusters = NULL
+}
+
 sce <- as.SingleCellExperiment(obj)
-sce <- scDblFinder(sce)
+
+sce <- scDblFinder(sce, clusters=clusters)
 
 obj <- as.Seurat(sce, data = NULL)
 rm(sce)
@@ -35,3 +56,4 @@ write.csv(table(obj$scDblFinder.class, obj$orig.ident), paste0(outFile, ".scDblF
 
 singlet <- subset(obj, subset = scDblFinder.class == "singlet")
 saveRDS(singlet, paste0(outFile, ".scDblFinder.singlet_object.rds"))
+writeLines(capture.output(sessionInfo()), 'sessionInfo.txt')
