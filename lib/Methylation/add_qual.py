2a6ed698cec5257716d95f1096845a2c9b928b48
Quanhu Sheng <shengqh@gmail.com>
2025-10-09 02:01:10 -0500

    speed up

--- lib/Methylation/add_qual.py
+++ lib/Methylation/add_qual.py
@@ -13,19 +13,19 @@ def change_quality_scores(bam_file, output_file):
 
   # Open input and output BAM files
   with pysam.AlignmentFile(bam_file, "rb") as input_samfile:
-    with pysam.AlignmentFile(output_file, "wb", template=input_samfile) as output_samfile:
+    # Get header and remove lines containing ID:DNMTOOLS
+    header = input_samfile.header.to_dict()
+    if 'PG' in header:
+      idx = 0
+      # Modify DNMTOOLS ID if present
+      for pg in header['PG']:
+        if 'ID' in pg and pg['ID'] == 'DNMTOOLS':
+          idx = idx + 1
+          pg['ID'] = 'DNMTOOLS.' + str(idx)
+    with pysam.AlignmentFile(output_file, "wb", header=header) as output_samfile:
       # Iterate over reads in the input BAM file
       for read in input_samfile:
-        # Get the read length
-        read_length = len(read.query_sequence)
-
-        # Create an array of F characters with the same length as the read
-        new_quality_scores = pysam.qualitystring_to_array("F" * read_length)
-
-        # Update the read's quality scores with the new scores
-        read.query_qualities  = new_quality_scores
-
-        # Write the modified read to the output BAM file
+        read.query_qualities  = pysam.qualitystring_to_array("F" * read.query_length)
         output_samfile.write(read)
 
 parser = argparse.ArgumentParser(description='add quality columns to bam files')
