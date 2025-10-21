0b207c7813beedb623ffd9015a3dda44bb87656a
Quanhu Sheng <shengqh@gmail.com>
2025-10-20 23:05:05 -0500

    check index of gene name

--- lib/Methylation/get_gene_names.pl
+++ lib/Methylation/get_gene_names.pl
@@ -1,19 +1,38 @@
 open IN,"$ARGV[0]";
 open OUT,">$ARGV[1]";
 
+$header_found = 0;
+$gene_col_index = -1;
+
 while(<IN>){
   chomp;
+  
+  # Find header line and get Gene.refGene column index
+  if(!$header_found && /^Chr|^chr\s+/) {
+    @header = split/\t/,$_;
+    for my $i (0..$#header) {
+      if($header[$i] eq "Gene.refGene") {
+        $gene_col_index = $i;
+        last;
+      }
+    }
+    $header_found = 1;
+    next;
+  }
+  
   next if(/^Chr/);
   next if(/^chr\s+/);
+  next if($gene_col_index == -1);
+  
   @a = split/\t/,$_;
-  if($a[6] =~/[,;]+/){
-    @b = split/[,;]+/,$a[6];
+  if($a[$gene_col_index] =~/[,;]+/){
+    @b = split/[,;]+/,$a[$gene_col_index];
     foreach $key(@b){
       $genes{$key}++;
     }
   }
   else{
-    $genes{$a[6]}++;
+    $genes{$a[$gene_col_index]}++;
   }
 }
 
