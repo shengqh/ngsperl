0d31d7f3d432d56d5f6e550a6ea1f99cef2781a5
Quanhu Sheng <shengqh@gmail.com>
2025-10-30 20:05:29 -0500

    format the code

--- lib/CQS/ProgramWrapper.pm
+++ lib/CQS/ProgramWrapper.pm
@@ -16,6 +16,7 @@ use Data::Dumper;
 
 our @ISA = qw(CQS::UniqueWrapper);
 
+
 sub new {
   my ($class) = @_;
   my $self = $class->SUPER::new();
@@ -23,61 +24,63 @@ sub new {
   $self->{_suffix} = "_pw";
   bless $self, $class;
   return $self;
-}
+} ## end sub new
+
 
 sub replace_tag {
-  my ($config, $section, $task_name, $result_dir, $option, $source_key, $sample_name, $final_prefix) = @_;
+  my ( $config, $section, $task_name, $result_dir, $option, $source_key, $sample_name, $final_prefix ) = @_;
 
   my $task_suffix = get_option( $config, $section, "suffix", "" );
 
   my $cur_option = $option;
-  if ($cur_option =~ /__NAME__/){
+  if ( $cur_option =~ /__NAME__/ ) {
     $cur_option =~ s/__NAME__/$sample_name/g;
   }
   my $input;
-  ($cur_option, $input) = process_parameter_sample_file($config, $section, $result_dir, $task_name, $task_suffix, $cur_option, $source_key, 1);
+  ( $cur_option, $input ) = process_parameter_sample_file( $config, $section, $result_dir, $task_name, $task_suffix, $cur_option, $source_key, 1 );
 
-  my $output_arg            = get_option( $config, $section, "output_arg", "" );
-  my $no_output            = get_option( $config, $section, "no_output", 0 );
+  my $output_arg = get_option( $config, $section, "output_arg", "" );
+  my $no_output  = get_option( $config, $section, "no_output",  0 );
 
   my $output_option = "$output_arg $final_prefix";
-  if ($cur_option =~ /__OUTPUT__/){
+  if ( $cur_option =~ /__OUTPUT__/ ) {
     $cur_option =~ s/__OUTPUT__/$final_prefix/g;
     $output_option = "";
   }
 
-  if (option_contains_arg($cur_option, $output_arg)) {
+  if ( option_contains_arg( $cur_option, $output_arg ) ) {
     $output_option = "";
   }
 
-  if ($no_output){
+  if ($no_output) {
     $output_option = "";
   }
 
   my $cur_init_command = get_option( $config, $section, "init_command", "" );
-  if ($cur_init_command =~ /__NAME__/){
+  if ( $cur_init_command =~ /__NAME__/ ) {
     $cur_init_command =~ s/__NAME__/$sample_name/g;
   }
 
-  if ($cur_init_command =~ /__FILE__/){
+  if ( $cur_init_command =~ /__FILE__/ ) {
     $cur_init_command =~ s/__FILE__/$input/g;
   }
 
   my $cur_post_command = get_option( $config, $section, "post_command", "" );
-  if ($cur_post_command =~ /__NAME__/){
+  if ( $cur_post_command =~ /__NAME__/ ) {
     $cur_post_command =~ s/__NAME__/$sample_name/g;
   }
 
-  if ($cur_post_command =~ /__FILE__/){
+  if ( $cur_post_command =~ /__FILE__/ ) {
     $cur_post_command =~ s/__FILE__/$input/g;
   }
 
-  if ($cur_post_command =~ /__OUTPUT__/){
+  if ( $cur_post_command =~ /__OUTPUT__/ ) {
     $cur_post_command =~ s/__OUTPUT__/$final_prefix/g;
   }
 
-  return($cur_option, $output_option, $cur_init_command, $cur_post_command);
-}
+  return ( $cur_option, $output_option, $cur_init_command, $cur_post_command );
+} ## end sub replace_tag
+
 
 sub perform {
   my ( $self, $config, $section ) = @_;
@@ -88,88 +91,91 @@ sub perform {
   my $task_suffix = get_option( $config, $section, "suffix", "" );
   $self->{_task_suffix} = $task_suffix;
 
-  my $check_file_ext = get_option($config, $section, "check_file_ext", "");
+  my $check_file_ext = get_option( $config, $section, "check_file_ext", "" );
 
   my $no_prefix = get_option( $config, $section, "no_prefix", 0 );
 
   my $interpretor = get_option( $config, $section, "interpretor", "" );
-  my $program = get_option( $config, $section, "program" );
+  my $program     = get_option( $config, $section, "program" );
 
-  if (get_option($config, $section, "check_program", 1)){
+  if ( get_option( $config, $section, "check_program", 1 ) ) {
     if ( !File::Spec->file_name_is_absolute($program) ) {
       $program = dirname(__FILE__) . "/$program";
     }
     if ( !( -e $program ) ) {
       die("program $program defined but not exists!");
     }
-  }
+  } ## end if ( get_option( $config...))
 
   my $source_key = "source";
-  if ((not defined $config->{$section}{"source"} ) && (not defined $config->{$section}{"source_ref"} )) {
+  if ( ( not defined $config->{$section}{"source"} ) && ( not defined $config->{$section}{"source_ref"} ) ) {
     $source_key = "parameterSampleFile1";
   }
 
-  for my $index (2..10){
+  for my $index ( 2 .. 10 ) {
     my $key = "parameterSampleFile" . $index;
-    if (not has_raw_files($config, $section, $key)){
+    if ( not has_raw_files( $config, $section, $key ) ) {
       next;
     }
 
     my ( $parameterSampleFile, $parameterSampleFilearg, $parameterSampleFileJoinDelimiter ) = get_parameter_sample_files( $config, $section, $key );
 
     my $filelist_name;
-    if($no_prefix){
+    if ($no_prefix) {
       $filelist_name = "fileList${index}.list";
-    }else{
+    }
+    else {
       $filelist_name = "${task_name}_${task_suffix}_fileList${index}.list";
     }
     my $listfile = save_parameter_sample_file( $config, $section, $key, "${result_dir}/${filelist_name}" );
     if ( $listfile ne "" ) {
       $listfile = basename($listfile);
-      $option = $option . " " . $parameterSampleFilearg . " " . $listfile;
+      $option   = $option . " " . $parameterSampleFilearg . " " . $listfile;
     }
-  }
+  } ## end for my $index ( 2 .. 10)
 
-  $option = get_parameter_file_option($config, $section, $option);
+  $option = get_parameter_file_option( $config, $section, $option );
 
-  my $pbs_file   = $self->get_pbs_filename( $pbs_dir, $task_name, ".pbs" );
-  my $pbs_name   = basename($pbs_file);
-  my $log        = $self->get_log_filename( $log_dir, $task_name, ".log" );
-  my $log_desc   = $cluster->get_log_description($log);
+  my $pbs_file = $self->get_pbs_filename( $pbs_dir, $task_name, ".pbs" );
+  my $pbs_name = basename($pbs_file);
+  my $log      = $self->get_log_filename( $log_dir, $task_name, ".log" );
+  my $log_desc = $cluster->get_log_description($log);
 
-  my $results = $self->result( $config, $section );
+  my $results      = $self->result( $config, $section );
   my $result_files = $results->{$task_name};
 
   #print(Dumper($result_files));
-  
+
   my $pbs;
   my $final_file;
-  if (defined $result_files){
+  if ( defined $result_files ) {
     $final_file = $result_files->[0];
 
-    if ($final_file ne $result_dir) {
-      if (rindex($final_file, $result_dir, 0) == 0){
-        $final_file=substr($final_file, length($result_dir)+1);
+    if ( $final_file ne $result_dir ) {
+      if ( rindex( $final_file, $result_dir, 0 ) == 0 ) {
+        $final_file = substr( $final_file, length($result_dir) + 1 );
       }
     }
-    
+
     my $checkFile = $result_files->[-1] . $check_file_ext;
     $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $checkFile );
-  }else{
+  } ## end if ( defined $result_files)
+  else {
     $final_file = $target_dir . "/";
-    if(defined $results){
-      my @sampleKeys = reverse sort(keys (%$results));
-      my $firstKey = $sampleKeys[0];
-      my $checkFile = $results->{$firstKey}[-1] . $check_file_ext;
-      $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $checkFile);
-    }else{
-      $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir);
+    if ( defined $results ) {
+      my @sampleKeys = reverse sort( keys(%$results) );
+      my $firstKey   = $sampleKeys[0];
+      my $checkFile  = $results->{$firstKey}[-1] . $check_file_ext;
+      $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir, $checkFile );
+    } ## end if ( defined $results )
+    else {
+      $pbs = $self->open_pbs( $pbs_file, $pbs_desc, $log_desc, $path_file, $result_dir );
     }
-  }
+  } ## end else [ if ( defined $result_files)]
 
   #print($final_file);
 
-  my ($cur_option, $output_option, $cur_init_command, $cur_post_command) = replace_tag( $config, $section, $task_name, $result_dir, $option, $source_key, $task_name, $final_file);
+  my ( $cur_option, $output_option, $cur_init_command, $cur_post_command ) = replace_tag( $config, $section, $task_name, $result_dir, $option, $source_key, $task_name, $final_file );
 
   print $pbs "
 $cur_init_command
@@ -182,6 +188,6 @@ $cur_post_command
   $self->close_pbs( $pbs, $pbs_file );
 
   print "!!!pbs file $pbs_file created, you can run this pbs file to submit to cluster.\n";
-}
+} ## end sub perform
 
 1;
