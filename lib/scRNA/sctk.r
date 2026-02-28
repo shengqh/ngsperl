0953db8b136c113a46cbc7c6f1505dd591f518ee
Quanhu Sheng <shengqh@gmail.com>
2026-02-27 22:00:22 -0500

    only meta will be saved

--- lib/scRNA/sctk.r
+++ lib/scRNA/sctk.r
@@ -24,44 +24,21 @@ myoptions = read_file_map("fileList2.txt", do_unlist=FALSE)
 nFeature_cutoff_min=max(as.numeric(myoptions$nFeature_cutoff_min) - 50, 100)
 nCount_cutoff=max(as.numeric(myoptions$nCount_cutoff) - 50, 200)
 
-overwrite=FALSE
+cat("sample_name\n")
+countfile = sample_map[sample_name]
 
-sce_list <- list()
-sample_name=names(sample_map)[1]
-for (sample_name in names(sample_map)){
-  cat("sample_name\n")
-  countfile = sample_map[sample_name]
+cat("  read_scrna_data\n")
+lst = read_scrna_data(countfile)
+counts<-lst$counts
+rawobj = CreateSeuratObject(counts = counts, project = sample_name)
+rawobj<-subset(rawobj, subset = nFeature_RNA >= nFeature_cutoff_min & nCount_RNA >= nCount_cutoff)
 
-  sctk_rds = paste0(sample_name, ".sctk.rds")
+sce <- as.SingleCellExperiment(rawobj)
+rm(rawobj)
 
-  redo = !file.exists(sctk_rds) | overwrite
-  if(!redo){
-    cat("  read", sctk_rds, "\n")
-    sce = readRDS(sctk_rds)
-  }else{
-    cat("  read_scrna_data\n")
-    lst = read_scrna_data(countfile)
-    counts<-lst$counts
-    rawobj = CreateSeuratObject(counts = counts, project = sample_name)
-    rawobj<-subset(rawobj, subset = nFeature_RNA >= nFeature_cutoff_min & nCount_RNA >= nCount_cutoff)
+cat("  runCellQC\n")
+#sce = runCellQC(sce, algorithms= c("QCMetrics", "scrublet", "doubletFinder", "scDblFinder", "cxds", "bcds", "cxds_bcds_hybrid", "decontX"))
+#There is error to run scrublet, so ignore it.
+sce = runCellQC(sce, algorithms= c("QCMetrics", "doubletFinder", "scDblFinder", "cxds", "bcds", "cxds_bcds_hybrid", "decontX"))
 
-    sce <- as.SingleCellExperiment(rawobj)
-    rm(rawobj)
-
-    cat("  runCellQC\n")
-    #sce = runCellQC(sce, algorithms= c("QCMetrics", "scrublet", "doubletFinder", "scDblFinder", "cxds", "bcds", "cxds_bcds_hybrid", "decontX"))
-    #There is error to run scrublet, so ignore it.
-    sce = runCellQC(sce, algorithms= c("QCMetrics", "doubletFinder", "scDblFinder", "cxds", "bcds", "cxds_bcds_hybrid", "decontX"))
-
-    saveRDS(sce, sctk_rds)
-  }
-
-  saveRDS(colData(sce), paste0(sample_name, ".meta.rds"))
-
-  #cat("  reportCellQC\n")
-  #there is a lot of error to run reportCellQC, so ignore it.
-  #reportCellQC(inSCE = sce, output_file = paste0(sample_name, "_reportCellQC"))
-
-  #cat("  exportSCE\n")
-  #exportSCE(inSCE = sce, samplename = sample_name, type = "Cells", format = c("Seurat"))
-}
+saveRDS(colData(sce), paste0(sample_name, ".meta.rds"))
