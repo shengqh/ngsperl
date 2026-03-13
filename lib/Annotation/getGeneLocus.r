93c0625581a197ee3bafd09f03f9e36db734be55
Quanhu Sheng <shengqh@gmail.com>
2026-03-13 11:18:17 -0500

    remove duplicated get_gene_locus.r

--- lib/Annotation/getGeneLocus.r
+++ lib/Annotation/getGeneLocus.r
@@ -1,3 +1,16 @@
+rm(list=ls()) 
+outFile='AutoGVP'
+parSampleFile1='fileList1.txt'
+parSampleFile2=''
+parSampleFile3=''
+parFile1=''
+parFile2=''
+parFile3=''
+
+
+setwd('/nobackup/h_cqs/cindy_chen_projects/20260311_AutoGVP/genes_locus/result')
+
+### Parameter setting end ###
 
 require(biomaRt)
 require(stringr)
@@ -8,26 +21,31 @@ params<-split(params_def$V1, params_def$V2)
 host=params$host
 dataset=params$dataset
 symbolKey=params$symbolKey
-genesStr<-params$genesStr
-addChr<-params$add_chr=="1"
+genesStr=params$genesStr
+addChr=params$add_chr=="1"
+gene_shift=as.numeric(params$gene_shift)
+output_gff=params$output_gff=="1"
 
 if(!file.exists(genesStr)){
+  genes = str_trim(unlist(strsplit(genesStr, ",")))
   genesStr = gsub(",", " ", genesStr)
   genesStr = gsub(";", " ", genesStr)
-  genes = unlist(strsplit(genesStr, "\\s+"))
+  genes = strsplit(genesStr, "\\s+")
 }else{
   geneTable = read.table(genesStr, sep="\t", stringsAsFactor=F)
   if (grepl(".bed$", genesStr)){
-    genes = unique(geneTable$V4)
+    genes = geneTable$V4
     if (all(grepl("(.+)", genes))) {
       genes = gsub(".+?\\(", "", genes)
       genes = gsub("\\).+", "", genes)
-      genes = unique(genes)
     }
   }else{
     genes = geneTable$V1
   }
 }
+genes = genes |> unlist() |> str_trim() |> unique() |> sort()
+
+cat("genes:", paste0(genes, collapse=", "), "\n")
 
 ensembl <- useMart("ensembl", host=host, dataset=dataset)
 
@@ -48,12 +66,23 @@ geneLocus<-geneLocus[order(geneLocus$chromosome_name, geneLocus$start_position),
 geneLocus$strand[geneLocus$strand == 1]<-"+"
 geneLocus$strand[geneLocus$strand == -1]<-"-"
 
+if(gene_shift != 0){
+  cat("shift gene ", gene_shift, " bases\n")
+  geneLocus$start_position = geneLocus$start_position - gene_shift
+  geneLocus$start_position[geneLocus$start_position < 1] = 1
+  geneLocus$end_position = geneLocus$end_position + gene_shift
+}
+
 if(addChr & (!any(grepl("chr", geneLocus$chromosome_name)))){
   geneLocus$chromosome_name = paste0("chr", geneLocus$chromosome_name)
 }
 
+geneLocus$chromosome_name = gsub("^chrMT$", "chrM", geneLocus$chromosome_name)
+geneLocus$chromosome_name = gsub("^MT$", "M", geneLocus$chromosome_name)
+
 bedFile<-paste0(outFile, ".bed")
-write.table(geneLocus, file=bedFile, row.names=F, col.names = F, sep="\t", quote=F)
+geneLocusBed = geneLocus |> dplyr::mutate(start_position = start_position-1)
+write.table(geneLocusBed, file=bedFile, row.names=F, col.names = F, sep="\t", quote=F)
 
 missing<-genes[!(genes %in% geneLocus[,symbolKey])]
 writeLines(missing, paste0(outFile, ".missing"))
@@ -68,3 +97,11 @@ exonLocus<-getBM(attributes=c("exon_chrom_start", "exon_chrom_end", "ensembl_gen
 exonLocus<-exonLocus[,c("ensembl_gene_id", "exon_chrom_start", "exon_chrom_end")]
 
 write.table(exonLocus, file=paste0(bedFile, ".exon"), row.names=F, col.names = F, sep="\t", quote=F)
+
+if(output_gff){
+  geneLocus$unknown1="GENE"
+  geneLocus$unknown2='.'
+  geneLocus$unknown3='.'
+  geneLocus=geneLocus[,c(1,4,6,2,3,7,5,8)]
+  write.table(geneLocus, file=paste0(outFile, ".gff"), row.names = F,col.names = FALSE, quote = F, sep="\t")
+}
